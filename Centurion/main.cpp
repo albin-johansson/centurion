d8bb351d4fac02a4f9667ad517434f493e5872ba
Albin Johansson <albinoscar99@hotmail.com>
2019-03-20 10:50:44 +0100

    Update main.cpp

--- Centurion/main.cpp
+++ Centurion/main.cpp
@@ -1,31 +1,49 @@
-#include <stdio.h>
 #include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_mixer.h>
-#include <SDL_ttf.h>
+#include <stdio.h>
+#include "Centurion.h"
+#include "Window.h"
+#include "Image.h"
+#include "Screen.h"
 
-static void initSDL()
-{
-	SDL_Init(SDL_INIT_EVERYTHING);
-	IMG_Init(IMG_InitFlags::IMG_INIT_PNG | IMG_InitFlags::IMG_INIT_JPG);
-	Mix_Init(MIX_InitFlags::MIX_INIT_MP3 | MIX_InitFlags::MIX_INIT_OGG | MIX_InitFlags::MIX_INIT_FLAC);
-	TTF_Init();
-}
+using namespace centurion;
 
-static void closeSDL()
-{
-	IMG_Quit();
-	TTF_Quit();
-	Mix_Quit();
-	SDL_Quit();
-}
-
+//TODO remove main.c from the library
 int main(int argc, char** argv)
 {
-	initSDL();
+	Centurion::init();
 
-	printf("Hello world!\n");
+	std::string s = "HelloWorld";
+	Window window = Window(s, 800, 600);
 
-	closeSDL();
+	Image* img = window.createImage("player_ph.png");
+	window.setResizable(false);
+	window.show();
+
+	bool running = true;
+	SDL_Event event;
+	while (running) {
+
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_EventType::SDL_QUIT) {
+				running = false;
+			} else if (event.type == SDL_EventType::SDL_KEYUP) {
+				if (event.key.keysym.sym == SDLK_ESCAPE) {
+					running = false;
+				}
+			}
+		}
+
+		window.render(*img, 300, 200, 190, 300);
+		window.update();
+		SDL_Delay(2);
+	}
+
+	printf("Screen width: %i\n", Screen::getWidth());
+	printf("Screen height: %i\n", Screen::getHeight());
+	printf("Window width: %i\n", window.getWidth());
+	printf("Window height: %i\n", window.getHeight());
+
+	delete img;
+	Centurion::close();
 	return 0;
 }

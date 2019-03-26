eaf163ee0ad0acb07aa46025f010bdc0de33cac3
Albin Johansson <albinoscar99@hotmail.com>
2019-03-26 22:21:46 +0100

    Documentend Screen and Texture

--- Centurion/include/screen.h
+++ Centurion/include/screen.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "centurion.h"
 
+/**
+\brief The Screen class provides information about the system display.
+*/
 class centurion::Screen final {
 
 private:
@@ -9,7 +12,13 @@ private:
 public:
 	~Screen() = default;
 
+	/**
+	\brief Returns the width of the screen.
+	*/
 	static int GetWidth();
 
+	/**
+	\brief Returns the height of the screen.
+	*/
 	static int GetHeight();
 };
--- Centurion/include/texture.h
+++ Centurion/include/texture.h
@@ -4,6 +4,9 @@
 #include <SDL_image.h>
 #include <string>
 
+/**
+\brief The Texture class represents textures such as images.
+*/
 class centurion::visuals::Texture {
 
 private:
@@ -11,22 +14,48 @@ private:
 	int height;
 	SDL_Texture* texture = nullptr;
 
-	Texture(const std::string& path, centurion::visuals::Renderer& graphics);
-	
+	Texture(const std::string& path, centurion::visuals::Renderer& renderer);
+
 public:
 	~Texture();
 
+	/**
+	\brief Returns the width of this texture.
+	*/
 	int GetWidth();
 
+	/**
+	\brief Returns the height of this texture.
+	*/
 	int GetHeight();
 
-	SDL_Texture* GetTexture();
+	/**
+	\brief Returns a pointer to the internal SDL_Texture representation of this Texture.
+	*/
+	SDL_Texture* GetTexture(); //TODO avoid
 
+	/**
+	\brief Returns a pointer to an SDL_Texture.
+	\param surface - the SDL_Surface to base the texture on.
+	\param renderer - the SDL_Renderer used to create the texture.
+	*/
 	static SDL_Texture* CreateTexture(SDL_Surface* surface, SDL_Renderer* renderer);
 
-	static centurion::visuals::Texture* Create(const std::string& path, centurion::visuals::Renderer& graphics);
+	/**
+	\brief Returns a pointer to a Texture, based on the image at the specified path.
+	\param path - the path to the image file which the texture will be based on.
+	\param renderer - the Renderer used to create the texture.
+	*/
+	static centurion::visuals::Texture* Create(const std::string& path, centurion::visuals::Renderer& renderer);
 
+	/**
+	\brief Returns a pointer to a Texture, based on the supplied SDL_Texture.
+	\param texture - the SDL_Texture to base the Texture on.
+	*/
 	static centurion::visuals::Texture* Create(SDL_Texture* texture);
 
+	/**
+	\brief Destroys a Texture.
+	*/
 	static void Destroy(centurion::visuals::Texture* img);
 };

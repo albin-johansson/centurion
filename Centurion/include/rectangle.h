d816d6895745d8ef7acac98f4dbc4538e4cf3735
Albin Johansson <albinoscar99@hotmail.com>
2019-03-26 21:45:35 +0100

    Documented rectangle

--- Centurion/include/rectangle.h
+++ Centurion/include/rectangle.h
@@ -2,37 +2,94 @@
 #include "centurion.h"
 #include <SDL_rect.h>
 
+/**
+\brief The Rectangle class quite simply represents a rectangle.
+*/
 class centurion::geo::Rectangle {
 
 private:
 	SDL_Rect rect;
 
 public:
+	/**
+	\param x - the initial x-coordinate of the rectangle.
+	\param y - the initial y-coordinate of the rectangle.
+	\param w - the width of the rectangle.
+	\param h - the height of the rectangle.
+	*/
 	Rectangle(int x, int y, int w, int h);
 
+	/**
+	\param w - the width of the rectangle.
+	\param h - the height of the rectangle.
+	*/
 	Rectangle(int w, int h);
 
 	~Rectangle() = default;
 
+	/**
+	\brief Assigns the location of this rectangle.
+	\param x - the new x-coordinate.
+	\param y - the new y-coordinate.
+	*/
 	void SetLocation(int x, int y);
 
+	/**
+	\brief Assigns the x-coordinate of this rectangle.
+	\param x - the new x-coordinate.
+	*/
 	void SetX(int x);
 
+	/**
+	\brief Assigns the y-coordinate of this rectangle.
+	\param y - the new y-coordinate.
+	*/
 	void SetY(int y);
 
+	/**
+	\brief Indicates whether or not this rectangle intersects another rectangle. Returns true if
+	 the supplied rectangle intersects this rectangle, returns false otherwise.
+	\param rect - the rectangle that will be checked with this rectangle.
+	*/
 	bool Intersects(centurion::geo::Rectangle& rect);
 
+	/**
+	\brief Indicates whether or not this rectangle contains the specified point. Returns true if
+	 the point is contained in this rectangle, returns false otherwise.
+	\param x - the x-coordinate of the point to check.
+	\param y - the y-coordinate of the point to check.
+	*/
 	bool Contains(int x, int y);
 
+	/**
+	\brief Indicates whether or not this rectangle contains the specified point. Returns true if
+	 the point is contained in this rectangle, returns false otherwise.
+	\param point - the point that will be checked.
+	*/
 	bool Contains(Point& point);
 
+	/**
+	\brief Returns the x-coordinate of this rectangle.
+	*/
 	int GetX();
 
+	/**
+	\brief Returns the y-coordinate of this rectangle.
+	*/
 	int GetY();
 
+	/**
+	\brief Returns the width of this rectangle.
+	*/
 	int GetWidth();
 
+	/**
+	\brief Returns the height of this rectangle.
+	*/
 	int GetHeight();
 
+	/**
+	\brief Creates and returns an SDL_Rect that represents this rectangle.
+	*/
 	SDL_Rect CreateSDLRect();
 };

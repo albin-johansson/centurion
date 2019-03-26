fe5f694950cdc2f20159d2618a08b62f448f46ee
Albin Johansson <albinoscar99@hotmail.com>
2019-03-26 21:31:38 +0100

    Documented Point

--- Centurion/include/point.h
+++ Centurion/include/point.h
@@ -2,23 +2,49 @@
 #include "centurion.h"
 #include <SDL_rect.h>
 
+/**
+\brief The Point class represents a two-dimensional point.
+*/
 class centurion::geo::Point {
 
 private:
 	SDL_Point point;
 
 public:
+	/**
+	\param x - the desired initial x-coordinate.
+	\param y - the desired initial y-coordinate.
+	*/
 	Point(int x, int y);
 
 	~Point() = default;
 
+	/**
+	\brief Assigns the location of this point.
+	\param x - the new x-coordinate.
+	\param y - the new y-coordinate.
+	*/
 	void SetLocation(int x, int y);
 
+	/**
+	\brief Assigns the x-coordinate of this point.
+	\param x - the new x-coordinate.
+	*/
 	void SetX(int x);
 
+	/**
+	\brief Assigns the y-coordinate of this point.
+	\param y - the new y-coordinate.
+	*/
 	void SetY(int y);
 
+	/**
+	\brief Returns the x-coordinate of this point.
+	*/
 	int GetX();
 
+	/**
+	\brief Returns the y-coordinate of this point.
+	*/
 	int GetY();
 };

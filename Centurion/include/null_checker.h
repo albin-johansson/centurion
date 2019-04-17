18da6e5337bbaeadaf6c42891df2462616b6acce
Albin Johansson <albinoscar99@hotmail.com>
2019-04-17 13:35:33 +0200

    Marked NullChecker as deprecated

--- Centurion/include/null_checker.h
+++ Centurion/include/null_checker.h
@@ -5,6 +5,7 @@ namespace centurion {
 namespace tools {
 
 /**
+*DEPRECATED*
 \brief The NullChecker class is a simple class that checks whether or not
 pointers are null pointers.
 \since 1.0.0
@@ -12,6 +13,7 @@ pointers are null pointers.
 class NullChecker {
  public:
   /**
+  *DEPRECATED*
   \brief Indicates whether the supplied pointer is a null pointer.
   \since 1.0.0
   */

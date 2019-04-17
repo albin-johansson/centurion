dbc612689f005b8c1f333a219e0703c1ebcad5e1
Albin Johansson <albinoscar99@hotmail.com>
2019-04-17 13:33:38 +0200

    Added MouseState

---
+++ Centurion/include/mouse_state.h
@@ -0,0 +1,106 @@
+#pragma once
+#include <SDL_mouse.h>
+#include <SDL_stdinc.h>
+#include <memory>
+#include "point.h"
+
+namespace centurion {
+namespace input {
+
+/**
+\brief The MouseState class holds information about the mouse state.
+\since 1.1.0
+*/
+class MouseState {
+ private:
+  int x;
+  int y;
+  Uint32 prevMask;
+  Uint32 currentMask;
+
+  inline bool IsValidMask(const Uint32 mask) const noexcept {
+    return mask | COMPLETE_MASK;
+  }
+
+ public:
+  /**
+  \brief A bit mask for all of the possible ::SDL_BUTTON_xyz values.
+  \since 1.1.0
+  */
+  const static Uint32 COMPLETE_MASK = SDL_BUTTON_LMASK | SDL_BUTTON_MMASK |
+                                      SDL_BUTTON_RMASK | SDL_BUTTON_X1MASK |
+                                      SDL_BUTTON_X2MASK;
+
+  MouseState();
+
+  ~MouseState();
+
+  void Update() noexcept;
+
+  /**
+  \brief Returns true if the button associated with the mask is pressed, returns
+  false otherwise.
+  \param buttonMask - the mask associated with the desired button. The possible
+  values are: SDL_BUTTON_LMASK, SDL_BUTTON_MMASK, SDL_BUTTON_RMASK,
+  SDL_BUTTON_X1MASK and SDL_BUTTON_X2MASK;
+  \since 1.1.0
+  */
+  bool IsButtonPressed(Uint32 buttonMask) const noexcept;
+
+  /**
+  \brief Returns true if the button associated with the mask was released,
+  returns false otherwise.
+  \param buttonMask - the mask associated with the desired button. The possible
+  values are: SDL_BUTTON_LMASK, SDL_BUTTON_MMASK, SDL_BUTTON_RMASK,
+  SDL_BUTTON_X1MASK and SDL_BUTTON_X2MASK;
+  \since 1.1.0
+  */
+  bool WasButtonReleased(Uint32 buttonMask) const noexcept;
+
+  /**
+  \brief Returns true if the left button is being pressed, returns false
+  otherwise.
+  \since 1.1.0
+  */
+  bool IsLeftButtonPressed() const noexcept;
+
+  /**
+  \brief Returns true if the right button is being pressed, returns false
+  otherwise.
+  \since 1.1.0
+  */
+  bool IsRightButtonPressed() const noexcept;
+
+  /**
+  \brief Returns true if the left button was released, returns false otherwise.
+  \since 1.1.0
+  */
+  bool WasLeftButtonReleased() const noexcept;
+
+  /**
+  \brief Returns true if the right button was released, returns false otherwise.
+  \since 1.1.0
+  */
+  bool WasRightButtonReleased() const noexcept;
+
+  /**
+  \brief Returns the x-coordinate of the mouse cursor.
+  \since 1.1.0
+  */
+  inline int GetX() const noexcept { return x; }
+
+  /**
+  \brief Returns the y-coordinate of the mouse cursor.
+  \since 1.1.0
+  */
+  inline int GetY() const noexcept { return y; }
+
+  centurion::geo::Point GetPoint() const noexcept;
+};
+
+typedef std::shared_ptr<centurion::input::MouseState> MouseState_sptr;
+typedef std::unique_ptr<centurion::input::MouseState> MouseState_uptr;
+typedef std::weak_ptr<centurion::input::MouseState> MouseState_wptr;
+
+}  // namespace input
+}  // namespace centurion
---
+++ Centurion/src/mouse_state.cpp
@@ -0,0 +1,58 @@
+#include "mouse_state.h"
+
+using centurion::geo::Point;
+
+namespace centurion {
+namespace input {
+
+MouseState::MouseState() {
+  x = 0;
+  y = 0;
+  prevMask = 0;
+  currentMask = 0;
+}
+
+MouseState::~MouseState() = default;
+
+void MouseState::Update() noexcept {
+  prevMask = currentMask;
+  currentMask = SDL_GetMouseState(&x, &y);
+}
+
+bool MouseState::IsButtonPressed(Uint32 buttonMask) const noexcept {
+  if (!IsValidMask(buttonMask)) {
+    return false;
+  } else {
+    return (currentMask & buttonMask);
+  }
+}
+
+bool MouseState::WasButtonReleased(Uint32 buttonMask) const noexcept {
+  if (!IsValidMask(buttonMask)) {
+    return false;
+  } else {
+    return !(currentMask & buttonMask) && (prevMask & buttonMask);
+  }
+}
+
+bool MouseState::IsLeftButtonPressed() const noexcept {
+  return (currentMask & SDL_BUTTON_LMASK);
+}
+
+bool MouseState::IsRightButtonPressed() const noexcept {
+  return (currentMask & SDL_BUTTON_RMASK);
+}
+
+bool MouseState::WasLeftButtonReleased() const noexcept {
+  return !(currentMask & SDL_BUTTON_LMASK) && (prevMask | SDL_BUTTON_LMASK);
+}
+
+bool MouseState::WasRightButtonReleased() const noexcept {
+  return !(currentMask & SDL_BUTTON_RMASK) && (prevMask | SDL_BUTTON_RMASK);
+}
+
+// TODO does this work?
+Point MouseState::GetPoint() const noexcept { return Point(x, y); }
+
+}  // namespace input
+}  // namespace centurion

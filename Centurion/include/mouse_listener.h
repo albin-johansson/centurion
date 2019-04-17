af89c14231100e1a8f30a8792b2070e72c9a340e
Albin Johansson <albinoscar99@hotmail.com>
2019-04-17 13:34:05 +0200

    Added MouseListener

---
+++ Centurion/include/mouse_listener.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <memory>
+#include "mouse_state.h"
+
+namespace centurion {
+namespace input {
+
+/**
+\brief The MouseListener class is an interface for objects that responds to
+mouse changes.
+\since 1.1.0
+*/
+class MouseListener {
+ protected:
+  MouseListener() = default;
+
+ public:
+  virtual ~MouseListener() = default;
+
+  // TODO MouseMoved
+
+  /**
+  \brief Invoked when a mouse button has been pressed.
+  \param mouse - a reference to the Mouse instance holding information about the
+  mouse state.
+  \since 1.1.0
+  */
+  virtual void MousePressed(const centurion::input::MouseState& mouse) = 0;
+
+  /**
+  \brief Invoked when a mouse button has been released.
+  \param mouse - a reference to the Mouse instance holding information about the
+  mouse state.
+  \since 1.1.0
+  */
+  virtual void MouseReleased(const centurion::input::MouseState& mouse) = 0;
+};
+
+typedef std::shared_ptr<centurion::input::MouseListener> MouseListener_sptr;
+typedef std::unique_ptr<centurion::input::MouseListener> MouseListener_uptr;
+typedef std::weak_ptr<centurion::input::MouseListener> MouseListener_wptr;
+
+}  // namespace input
+}  // namespace centurion

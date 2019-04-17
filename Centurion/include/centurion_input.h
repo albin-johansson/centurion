633ae16f4be0cab9bfc82797026e82089e0002b5
Albin Johansson <albinoscar99@hotmail.com>
2019-04-17 13:37:11 +0200

    Tweaked the convenience header for the input namespace

--- Centurion/include/centurion_input.h
+++ Centurion/include/centurion_input.h
@@ -2,16 +2,14 @@
 
 /*
   This header file includes all of the components located in the
-  centurion::input namespace. Last updated: 2019-04-15.
+  centurion::input namespace. Last updated: 2019-04-17.
   \since 1.1.0
 */
 
 #include "input_dispatcher.h"
-
-#include "key_state.h"
 #include "key_listener.h"
 #include "key_listener_composite.h"
-
-#include "mouse_state.h"
+#include "key_state.h"
 #include "mouse_listener.h"
 #include "mouse_listener_composite.h"
+#include "mouse_state.h"

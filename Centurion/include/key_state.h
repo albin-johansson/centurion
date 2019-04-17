64b450f9a090e8c98c66e9157942f13c8d3b4116
Albin Johansson <albinoscar99@hotmail.com>
2019-04-17 13:33:24 +0200

    Added KeyState

---
+++ Centurion/include/key_state.h
@@ -0,0 +1,76 @@
+#pragma once
+#include <SDL_scancode.h>
+#include <SDL_stdinc.h>
+#include <memory>
+#include <vector>
+
+namespace centurion {
+namespace input {
+
+/**
+\brief The KeyState class provides information about the keyboard state.
+\since 1.1.0
+*/
+class KeyState final {
+ private:
+  int nKeys;
+  const Uint8* stateArr;
+  std::vector<Uint8> prevStates;
+
+  void CopyStates();
+
+ public:
+  KeyState();
+
+  ~KeyState();
+
+  /**
+  \brief Refreshes the information about the key state. This method does NOT
+  call SDL_PumpEvents().
+  \since 1.1.0
+  */
+  void Update();
+
+  /**
+  \brief Returns true if the key associated with the supplied SDL_Scancode is
+  being pressed.
+  \param code - the SDL_Scancode that corresponds to the desired key to check.
+  \since 1.1.0
+  */
+  bool IsPressed(const SDL_Scancode code) const;
+
+  /**
+  \brief Returns true if the key associated with the supplied SDL_Scancode is
+  currently held down.
+  \param code - the SDL_Scancode that corresponds to the
+  desired key to check.
+  \since 1.1.0
+  */
+  bool IsHeldDown(const SDL_Scancode code) const;
+
+  /**
+  \brief Returns true if the key associated with the supplied SDL_Scancode was
+  released.
+  \param code - the SDL_Scancode that corresponds to the desired key to check.
+  \since 1.1.0
+  */
+  bool WasReleased(const SDL_Scancode code) const;
+
+  /**
+  \brief Returns true if the key associated with the supplied SDL_Scancode is
+  being pressed and triggered for the first time, since it was pressed. Returns
+  false otherwise.
+  \param code - the SDL_Scancode that corresponds to the desired key to check.
+  \since 1.1.0
+  */
+  bool IsFirstTrigger(const SDL_Scancode code) const;
+
+  inline int GetNumberOfKeys() const noexcept { return nKeys; }
+};
+
+typedef std::shared_ptr<centurion::input::KeyState> KeyState_sptr;
+typedef std::unique_ptr<centurion::input::KeyState> KeyState_uptr;
+typedef std::weak_ptr<centurion::input::KeyState> KeyState_wptr;
+
+}  // namespace input
+}  // namespace centurion
---
+++ Centurion/src/key_state.cpp
@@ -0,0 +1,40 @@
+#include "key_state.h"
+#include <SDL_keyboard.h>
+
+namespace centurion {
+namespace input {
+
+KeyState::KeyState() {
+  nKeys = 0;
+  stateArr = SDL_GetKeyboardState(&nKeys);
+  prevStates = std::vector<Uint8>(nKeys);
+}
+
+KeyState::~KeyState() {}
+
+void KeyState::CopyStates() {
+  for (int i = 0; i < nKeys; i++) {
+    prevStates.at(i) = stateArr[i];
+  }
+}
+
+void KeyState::Update() { CopyStates(); }
+
+bool KeyState::IsPressed(const SDL_Scancode code) const {
+  return stateArr[code];
+}
+
+bool KeyState::IsHeldDown(const SDL_Scancode code) const {
+  return stateArr[code] && prevStates.at(code);
+}
+
+bool KeyState::WasReleased(const SDL_Scancode code) const {
+  return !stateArr[code] && prevStates.at(code);
+}
+
+bool KeyState::IsFirstTrigger(const SDL_Scancode code) const {
+  return stateArr[code] && !prevStates.at(code);
+}
+
+}  // namespace input
+}  // namespace centurion

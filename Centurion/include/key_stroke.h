29e83172d824719122a3d70b3fd5f4ea5d6c6d4d
Albin Johansson <albinoscar99@hotmail.com>
2019-04-18 21:22:36 +0200

    Added create methods to KeyStroke

--- Centurion/include/key_stroke.h
+++ Centurion/include/key_stroke.h
@@ -9,6 +9,11 @@
 namespace centurion {
 namespace events {
 
+class KeyStroke;
+typedef std::shared_ptr<KeyStroke> KeyStroke_sptr;
+typedef std::unique_ptr<KeyStroke> KeyStroke_uptr;
+typedef std::weak_ptr<KeyStroke> KeyStroke_wptr;
+
 /**
 \brief The KeyStroke class represents an key controlled action.
 \since 1.0.0
@@ -37,18 +42,6 @@ class KeyStroke {
   ~KeyStroke();
 
   /**
-  \brief Creates and returns a heap allocated KeyStroke instance.
-  \param keycode - the key code of the key that will trigger the KeyStroke.
-  \param action - the Action that will be executed when the related key
-  is pressed/released.
-  \param trigger - the KeyTrigger value which specifies the
-  preferred moment of activation.
-  */
-  static std::shared_ptr<centurion::events::KeyStroke> Create(
-      SDL_Keycode, std::shared_ptr<centurion::events::Action> action,
-      centurion::events::KeyTrigger trigger);
-
-  /**
   \brief Updates this KeyStroke by comparing it to the supplied event.
   \param event - The event that will be checked.
   */
@@ -73,11 +66,44 @@ class KeyStroke {
   \brief Indicates whether or not this KeyStroke is repeatable.
   */
   inline bool IsRepeatable() const noexcept { return isRepeatable; }
-};
 
-typedef std::shared_ptr<centurion::events::KeyStroke> KeyStroke_sptr;
-typedef std::unique_ptr<centurion::events::KeyStroke> KeyStroke_uptr;
-typedef std::weak_ptr<centurion::events::KeyStroke> KeyStroke_wptr;
+  /**
+  \brief Creates and returns a shared pointer that points to a KeyStroke
+  instance.
+  \param keycode - the key code of the key that will trigger the KeyStroke.
+  \param action - the Action that will be executed when the related key is
+  pressed/released.
+  \param trigger - the KeyTrigger value which specifies the preferred moment of
+  activation.
+  */
+  static KeyStroke_sptr CreateShared(SDL_Keycode keycode,
+                                     centurion::events::Action_sptr action,
+                                     centurion::events::KeyTrigger trigger);
+  /**
+  \brief Creates and returns a unique pointer that points to a KeyStroke
+  instance.
+  \param keycode - the key code of the key that will trigger the KeyStroke.
+  \param action - the Action that will be executed when the related key is
+  pressed/released.
+  \param trigger - the KeyTrigger value which specifies the preferred moment of
+  activation.
+  */
+  static KeyStroke_uptr CreateUnique(SDL_Keycode keycode,
+                                     centurion::events::Action_sptr action,
+                                     centurion::events::KeyTrigger trigger);
+  /**
+  \brief Creates and returns a weak pointer that points to a KeyStroke
+  instance.
+  \param keycode - the key code of the key that will trigger the KeyStroke.
+  \param action - the Action that will be executed when the related key is
+  pressed/released.
+  \param trigger - the KeyTrigger value which specifies the preferred moment of
+  activation.
+  */
+  static KeyStroke_wptr CreateWeak(SDL_Keycode keycode,
+                                   centurion::events::Action_sptr action,
+                                   centurion::events::KeyTrigger trigger);
+};
 
 }  // namespace events
 }  // namespace centurion

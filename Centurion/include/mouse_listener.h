17f147b677478512b2ac0742c945b194ad7011b6
Albin Johansson <albinoscar99@hotmail.com>
2019-04-18 21:23:25 +0200

    Tweaked typedefs in MouseListener

--- Centurion/include/mouse_listener.h
+++ Centurion/include/mouse_listener.h
@@ -17,8 +17,6 @@ class MouseListener {
  public:
   virtual ~MouseListener() = default;
 
-  // TODO MouseMoved
-
   /**
   \brief Invoked when a mouse button has been pressed.
   \param mouse - a reference to the Mouse instance holding information about the
@@ -36,9 +34,9 @@ class MouseListener {
   virtual void MouseReleased(const centurion::input::MouseState& mouse) = 0;
 };
 
-typedef std::shared_ptr<centurion::input::MouseListener> MouseListener_sptr;
-typedef std::unique_ptr<centurion::input::MouseListener> MouseListener_uptr;
-typedef std::weak_ptr<centurion::input::MouseListener> MouseListener_wptr;
+typedef std::shared_ptr<MouseListener> MouseListener_sptr;
+typedef std::unique_ptr<MouseListener> MouseListener_uptr;
+typedef std::weak_ptr<MouseListener> MouseListener_wptr;
 
 }  // namespace input
 }  // namespace centurion

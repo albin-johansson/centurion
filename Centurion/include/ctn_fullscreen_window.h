fea49cc8fb8c85aa542d5c73ffa4caba3dfd26d5
Albin Johansson <albinoscar99@hotmail.com>
2019-05-06 16:26:27 +0200

    Documented FullscreenWindow

--- Centurion/include/ctn_fullscreen_window.h
+++ Centurion/include/ctn_fullscreen_window.h
@@ -10,7 +10,11 @@ using FullscreenWindow_sptr = std::shared_ptr<FullscreenWindow>;
 using FullscreenWindow_uptr = std::unique_ptr<FullscreenWindow>;
 using FullscreenWindow_wptr = std::weak_ptr<FullscreenWindow>;
 
-class FullscreenWindow final : public AbstractWindow {
+/**
+\brief The FullscreenWindow class represents a fullscreen top-level frame.
+\since 2.0.0
+*/
+class FullscreenWindow final : public AbstractWindow {  // FIXME document
  private:
   SDL_Window* CreateWindow();
 
@@ -19,10 +23,22 @@ class FullscreenWindow final : public AbstractWindow {
 
   ~FullscreenWindow();
 
+  /**
+  \brief Creates and returns a shared pointer to a FullscreenWindow instance.
+  \since 2.0.0
+  */
   static FullscreenWindow_sptr CreateShared();
 
+  /**
+  \brief Creates and returns a unique pointer to a FullscreenWindow instance.
+  \since 2.0.0
+  */
   static FullscreenWindow_uptr CreateUnique();
 
+  /**
+  \brief Creates and returns a weak pointer to a FullscreenWindow instance.
+  \since 2.0.0
+  */
   static FullscreenWindow_wptr CreateWeak();
 };
 

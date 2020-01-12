5e7aacbe7b350f82a9f562cb911e066a7e652edc
Albin Johansson <albinoscar99@hotmail.com>
2020-01-12 20:46:31 +0100

    Add demo executable

---
+++ demo/demo_main.cpp
@@ -0,0 +1,48 @@
+#include "centurion.h"
+
+#include "window.h"
+#include "window_listener.h"
+#include "renderer.h"
+#include "colors.h"
+
+#include <SDL.h>
+
+using namespace centurion;
+
+static void do_stuff() {
+  Window window;
+  Renderer renderer{window};
+
+  window.show();
+
+  bool running = true;
+  SDL_Event event;
+
+  while (running) {
+
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT || event.type == SDL_KEYUP) {
+        running = false;
+      }
+    }
+
+    static auto x = 0;
+
+    renderer.set_color(Colors::indian_red);
+    renderer.clear();
+
+    renderer.set_color(Colors::snow);
+    renderer.fill_rect(100 + (x++), 100, 100, 100);
+
+    renderer.present();
+  }
+
+  window.hide();
+}
+
+int main(int, char**) {
+  Centurion::init();
+  do_stuff();
+  Centurion::quit();
+  return 0;
+}

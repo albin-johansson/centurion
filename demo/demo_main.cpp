67af4e02575f63c4d80bf04d89fbdabad84d2b70
Albin Johansson <albinoscar99@hotmail.com>
2020-01-13 16:27:41 +0100

    Update demo_main.cpp

--- demo/demo_main.cpp
+++ demo/demo_main.cpp
@@ -4,17 +4,37 @@
 #include "window_listener.h"
 #include "renderer.h"
 #include "image.h"
+#include "image_generator.h"
 #include "colors.h"
+#include "paths.h"
 
 #include <SDL.h>
 
 using namespace centurion;
 
 static void do_stuff() {
-  Window window;
-  Renderer renderer{window};
+  SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengl");
 
-  Image image{renderer, "resources/grass.png"};
+  Window window;
+  auto renderer = Renderer::shared(window);
+
+  ImageGenerator imageGenerator{renderer};
+
+  auto image = imageGenerator.unique_img("resources/grass.png");
+
+  AppPath appPath;
+  if (appPath) {
+    SDL_Log("Application path: %s", appPath.get());
+  }
+
+  PrefPath prefPath{"albinjohansson", "centurion"};
+  if (prefPath) {
+    SDL_Log("Preferred path: %s", prefPath.get());
+  }
+
+  const auto info = renderer->get_info();
+
+  SDL_Log("Renderer driver: %s", info.name);
 
   window.show();
 
@@ -31,15 +51,15 @@ static void do_stuff() {
 
     static auto x = 0;
 
-    renderer.set_color(Colors::indian_red);
-    renderer.clear();
+    renderer->set_color(Colors::hot_pink);
+    renderer->clear();
 
-    renderer.draw_image(image, 200, 200, 200, 200);
+    renderer->draw_image(*image, 200, 200, 200, 200);
 
-    renderer.set_color(Colors::snow);
-    renderer.fill_rect(100 + x++, 100, 100, 100);
+    renderer->set_color(Colors::snow);
+    renderer->fill_rect(100 + x++, 100, 100, 100);
 
-    renderer.present();
+    renderer->present();
   }
 
   window.hide();

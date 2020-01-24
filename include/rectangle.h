6c8be644c198554f7ba54b88f7ef6fa55381bed1
Albin Johansson <albinoscar99@hotmail.com>
2020-01-24 13:28:04 +0100

    Implement FRect and test rectangles

--- include/rectangle.h
+++ include/rectangle.h
@@ -6,11 +6,11 @@
 namespace centurion {
 
 /**
- * The Rectangle class is a wrapper around an SDL_Rect struct.
+ * The Rect class is a wrapper around an SDL_Rect struct.
  *
  * @since 3.0.0
  */
-class CENTURION_API Rectangle final {
+class CENTURION_API Rect final {
  private:
   SDL_Rect rect{0, 0, 0, 0};
 
@@ -20,7 +20,7 @@ class CENTURION_API Rectangle final {
    *
    * @since 3.0.0
    */
-  CENTURION_API Rectangle() noexcept;
+  CENTURION_API Rect() noexcept;
 
   /**
    * @param x the x-coordinate of the rectangle.
@@ -29,7 +29,7 @@ class CENTURION_API Rectangle final {
    * @param height the height of the rectangle.
    * @since 3.0.0
    */
-  CENTURION_API Rectangle(int x, int y, int width, int height) noexcept;
+  CENTURION_API Rect(int x, int y, int width, int height) noexcept;
 
   /**
    * Sets the x-coordinate of the rectangle.
@@ -71,7 +71,7 @@ class CENTURION_API Rectangle final {
    * @since 3.0.0
    */
   [[nodiscard]]
-  CENTURION_API bool intersects(const Rectangle& other) noexcept;
+  CENTURION_API bool intersects(const Rect& other) const noexcept;
 
   /**
    * Indicates whether or not the rectangle contains the supplied point.
@@ -82,7 +82,7 @@ class CENTURION_API Rectangle final {
    * @since 3.0.0
    */
   [[nodiscard]]
-  CENTURION_API bool contains(int px, int py) noexcept;
+  CENTURION_API bool contains(int px, int py) const noexcept;
 
   /**
    * Indicates whether or not the rectangle contains the supplied point.
@@ -92,7 +92,7 @@ class CENTURION_API Rectangle final {
    * @since 3.0.0
    */
   [[nodiscard]]
-  CENTURION_API bool contains(Point point) noexcept;
+  CENTURION_API bool contains(Point point) const noexcept;
 
   /**
    * Indicates whether or not the rectangle has an area. A rectangle has an area if the width and
@@ -102,7 +102,7 @@ class CENTURION_API Rectangle final {
    * @since 3.0.0
    */
   [[nodiscard]]
-  CENTURION_API bool has_no_area() const noexcept;
+  CENTURION_API bool has_area() const noexcept;
 
   /**
    * Returns the x-coordinate of the rectangle.
@@ -183,13 +183,186 @@ class CENTURION_API Rectangle final {
 
 };
 
-class FRectangle final {
+/**
+ * The FRect class is a wrapper around the SDL_FRect struct.
+ *
+ * @since 3.0.0
+ */
+class FRect final {
  private:
-  float x = 0;
-  float y = 0;
-  float width = 0;
-  float height = 0;
+  SDL_FRect rect{0, 0, 0, 0};
 
+ public:
+  /**
+   * Creates a rectangle with all of the components set to 0.
+   *
+   * @since 3.0.0
+   */
+  CENTURION_API FRect() noexcept;
+
+  /**
+   * @param x the x-coordinate of the rectangle.
+   * @param y the y-coordinate of the rectangle.
+   * @param width the width of the rectangle.
+   * @param height the height of the rectangle.
+   * @since 3.0.0
+   */
+  CENTURION_API FRect(float x, float y, float width, float height) noexcept;
+
+  /**
+   * Sets the x-coordinate of the rectangle.
+   *
+   * @param x the new x-coordinate of the rectangle.
+   * @since 3.0.0
+   */
+  CENTURION_API void set_x(float x) noexcept;
+
+  /**
+   * Sets the y-coordinate of the rectangle.
+   *
+   * @param y the new y-coordinate of the rectangle.
+   * @since 3.0.0
+   */
+  CENTURION_API void set_y(float y) noexcept;
+
+  /**
+   * Sets the width of the rectangle.
+   *
+   * @param width the new width of the rectangle.
+   * @since 3.0.0
+   */
+  CENTURION_API void set_width(float width) noexcept;
+
+  /**
+   * Sets the height of the rectangle.
+   *
+   * @param height the new height of the rectangle.
+   * @since 3.0.0
+   */
+  CENTURION_API void set_height(float height) noexcept;
+
+  /**
+   * Indicates whether or not the rectangle intersects the supplied rectangle.
+   *
+   * @param other the rectangle to check for an intersection with.
+   * @return true if the rectangles intersect; false otherwise.
+   * @since 3.0.0
+   */
+  [[nodiscard]]
+  CENTURION_API bool intersects(const FRect& other) const noexcept;
+
+  /**
+   * Indicates whether or not the rectangle contains the specified point.
+   *
+   * @param px the x-coordinate of the point.
+   * @param py the y-coordinate of the point.
+   * @return true if the point is contained within the rectangle; false otherwise.
+   * @since 3.0.0
+   */
+  [[nodiscard]]
+  CENTURION_API bool contains(float px, float py) const noexcept;
+
+  /**
+   * Indicates whether or not the rectangle contains the specified point.
+   *
+   * @param point the point that will be checked.
+   * @return true if the point is contained within the rectangle; false otherwise.
+   * @since 3.0.0
+   */
+  [[nodiscard]]
+  CENTURION_API bool contains(FPoint point) const noexcept;
+
+  /**
+   * Indicates whether or not the rectangle has an area. A rectangle has an area if its width and
+   * height are both greater than zero.
+   *
+   * @return true if the rectangle has an area; false otherwise.
+   * @since 3.0.0
+   */
+  [[nodiscard]]
+  CENTURION_API bool has_area() const noexcept;
+
+  /**
+   * Returns the x-coordinate of the rectangle.
+   *
+   * @return the x-coordinate of the rectangle.
+   * @since 3.0.0
+   */
+  [[nodiscard]]
+  CENTURION_API float get_x() const noexcept;
+
+  /**
+   * Returns the y-coordinate of the rectangle.
+   *
+   * @return the y-coordinate of the rectangle.
+   * @since 3.0.0
+   */
+  [[nodiscard]]
+  CENTURION_API float get_y() const noexcept;
+
+  /**
+   * Returns the x-coordinate of the center point of the rectangle.
+   *
+   * @return the x-coordinate of the center point of the rectangle.
+   * @since 3.0.0
+   */
+  CENTURION_API float get_center_x() const noexcept;
+
+  /**
+   * Returns the y-coordinate of the center point of the rectangle.
+   *
+   * @return the y-coordinate of the center point of the rectangle.
+   * @since 3.0.0
+   */
+  [[nodiscard]]
+  CENTURION_API float get_center_y() const noexcept;
+
+  /**
+   * Returns the maximum x-coordinate of the rectangle, which is equivalent to the sum
+   * of the x-coordinate and width of the rectangle.
+   *
+   * @return the maximum x-coordinate of the rectangle.
+   * @since 3.0.0
+   */
+  [[nodiscard]]
+  CENTURION_API float get_max_x() const noexcept;
+
+  /**
+   * Returns the maximum y-coordinate of the rectangle, which is equivalent to the sum
+   * of the y-coordinate and height of the rectangle.
+   *
+   * @return the maximum y-coordinate of the rectangle.
+   * @since 3.0.0
+   */
+  [[nodiscard]]
+  CENTURION_API float get_max_y() const noexcept;
+
+  /**
+   * Returns the width of the rectangle.
+   *
+   * @return the width of the rectangle.
+   * @since 3.0.0
+   */
+  [[nodiscard]]
+  CENTURION_API float get_width() const noexcept;
+
+  /**
+   * Returns the height of the rectangle.
+   *
+   * @return the height of the rectangle.
+   * @since 3.0.0
+   */
+  [[nodiscard]]
+  CENTURION_API float get_height() const noexcept;
+
+  /**
+   * Returns a reference to the internal SDL_FRect instance.
+   *
+   * @return a reference to the internal SDL_FRect instance.
+   * @since 3.0.0
+   */
+  [[nodiscard]]
+  CENTURION_API /*implicit*/ operator const SDL_FRect&() const noexcept;
 };
 
 }
--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -4,89 +4,181 @@
 
 namespace centurion {
 
-static_assert(sizeof(Rectangle) == sizeof(SDL_Rect));
-static_assert(std::is_final_v<Rectangle>);
-static_assert(std::is_convertible_v<Rectangle, const SDL_Rect&>);
+static_assert(sizeof(Rect) == sizeof(SDL_Rect));
+static_assert(std::is_final_v<Rect>);
+static_assert(std::is_convertible_v<Rect, const SDL_Rect&>);
 
-static_assert(std::is_nothrow_copy_constructible_v<Rectangle>);
-static_assert(std::is_nothrow_copy_assignable_v<Rectangle>);
+static_assert(std::is_nothrow_copy_constructible_v<Rect>);
+static_assert(std::is_nothrow_copy_assignable_v<Rect>);
 
-static_assert(std::is_nothrow_move_constructible_v<Rectangle>);
-static_assert(std::is_nothrow_move_assignable_v<Rectangle>);
+static_assert(std::is_nothrow_move_constructible_v<Rect>);
+static_assert(std::is_nothrow_move_assignable_v<Rect>);
 
-Rectangle::Rectangle() noexcept = default;
+static_assert(std::is_nothrow_default_constructible_v<Rect>);
+static_assert(std::is_nothrow_destructible_v<Rect>);
 
-Rectangle::Rectangle(int x, int y, int width, int height) noexcept
+Rect::Rect() noexcept = default;
+
+Rect::Rect(int x, int y, int width, int height) noexcept
     : rect{x, y, width, height} {}
 
-void Rectangle::set_x(int x) noexcept {
+void Rect::set_x(int x) noexcept {
   rect.x = x;
 }
 
-void Rectangle::set_y(int y) noexcept {
+void Rect::set_y(int y) noexcept {
   rect.y = y;
 }
 
-void Rectangle::set_width(int width) noexcept {
+void Rect::set_width(int width) noexcept {
   rect.w = width;
 }
 
-void Rectangle::set_height(int height) noexcept {
+void Rect::set_height(int height) noexcept {
   rect.h = height;
 }
 
-bool Rectangle::intersects(const Rectangle& r) noexcept {
-  return !(get_x() >= r.get_max_x()
-      || get_max_x() <= r.get_x()
-      || get_y() >= r.get_max_y()
-      || get_max_y() <= r.get_y());
+bool Rect::intersects(const Rect& other) const noexcept {
+  return !(get_x() >= other.get_max_x()
+      || get_max_x() <= other.get_x()
+      || get_y() >= other.get_max_y()
+      || get_max_y() <= other.get_y());
 }
 
-bool Rectangle::contains(int px, int py) noexcept {
+bool Rect::contains(int px, int py) const noexcept {
   return !(px < get_x() || py < get_y() || px > get_max_x() || py > get_max_y());
 }
 
-bool Rectangle::contains(Point point) noexcept {
+bool Rect::contains(Point point) const noexcept {
   return contains(point.get_x(), point.get_y());
 }
 
-bool Rectangle::has_no_area() const noexcept {
-  return get_width() <= 0 || get_height() <= 0;
+bool Rect::has_area() const noexcept {
+  return get_width() > 0 && get_height() > 0;
 }
 
-int Rectangle::get_x() const noexcept {
+int Rect::get_x() const noexcept {
   return rect.x;
 }
 
-int Rectangle::get_y() const noexcept {
+int Rect::get_y() const noexcept {
   return rect.y;
 }
 
-int Rectangle::get_max_x() const noexcept {
+int Rect::get_max_x() const noexcept {
   return get_x() + get_width();
 }
 
-int Rectangle::get_max_y() const noexcept {
+int Rect::get_max_y() const noexcept {
   return get_y() + get_height();
 }
 
-int Rectangle::get_width() const noexcept {
+int Rect::get_width() const noexcept {
   return rect.w;
 }
 
-int Rectangle::get_height() const noexcept {
+int Rect::get_height() const noexcept {
   return rect.h;
 }
 
-int Rectangle::get_center_x() const noexcept {
+int Rect::get_center_x() const noexcept {
   return get_x() + (get_width() / 2);
 }
 
-int Rectangle::get_center_y() const noexcept {
+int Rect::get_center_y() const noexcept {
   return get_y() + (get_height() / 2);
 }
 
-Rectangle::operator const SDL_Rect&() const noexcept {
+Rect::operator const SDL_Rect&() const noexcept {
+  return rect;
+}
+
+static_assert(sizeof(FRect) == sizeof(SDL_FRect));
+static_assert(std::is_final_v<FRect>);
+static_assert(std::is_convertible_v<FRect, const SDL_FRect&>);
+
+static_assert(std::is_nothrow_copy_constructible_v<FRect>);
+static_assert(std::is_nothrow_copy_assignable_v<FRect>);
+
+static_assert(std::is_nothrow_move_constructible_v<FRect>);
+static_assert(std::is_nothrow_move_assignable_v<FRect>);
+
+static_assert(std::is_nothrow_default_constructible_v<FRect>);
+static_assert(std::is_nothrow_destructible_v<FRect>);
+
+FRect::FRect() noexcept = default;
+
+FRect::FRect(float x, float y, float width, float height) noexcept
+    : rect{x, y, width, height} {}
+
+void FRect::set_x(float x) noexcept {
+  rect.x = x;
+}
+
+void FRect::set_y(float y) noexcept {
+  rect.y = y;
+}
+
+void FRect::set_width(float width) noexcept {
+  rect.w = width;
+}
+
+void FRect::set_height(float height) noexcept {
+  rect.h = height;
+}
+
+bool FRect::intersects(const FRect& other) const noexcept {
+  return !(get_x() >= other.get_max_x()
+      || get_max_x() <= other.get_x()
+      || get_y() >= other.get_max_y()
+      || get_max_y() <= other.get_y());
+}
+
+bool FRect::contains(float px, float py) const noexcept {
+  return !(px < get_x() || py < get_y() || px > get_max_x() || py > get_max_y());
+}
+
+bool FRect::contains(FPoint point) const noexcept {
+  return contains(point.get_x(), point.get_y());
+}
+
+bool FRect::has_area() const noexcept {
+  return get_width() > 0 && get_height() > 0;
+}
+
+float FRect::get_x() const noexcept {
+  return rect.x;
+}
+
+float FRect::get_y() const noexcept {
+  return rect.y;
+}
+
+float FRect::get_center_x() const noexcept {
+  return rect.x + (rect.w / 2.0f);
+}
+
+float FRect::get_center_y() const noexcept {
+  return rect.y + (rect.h / 2.0f);
+}
+
+float FRect::get_max_x() const noexcept {
+  return rect.x + rect.w;
+}
+
+float FRect::get_max_y() const noexcept {
+  return rect.y + rect.h;
+}
+
+float FRect::get_width() const noexcept {
+  return rect.w;
+}
+
+float FRect::get_height() const noexcept {
+  return rect.h;
+}
+
+FRect::operator const SDL_FRect&() const noexcept {
   return rect;
 }
 

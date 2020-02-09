1bf6f82f6cd118066653e4eae0599132d5cde431
Albin Johansson <albinoscar99@hotmail.com>
2020-02-09 13:57:09 +0100

    Tweak Timer tests

--- test/unittests/timer_test.cpp
+++ test/unittests/timer_test.cpp
@@ -14,14 +14,14 @@ TEST_CASE("Timer::sleep", "[Timer]") {
   const int after = Timer::millis();
 
   const int diff = after - before;
-  CHECK(std::abs(millis - diff) < 2); // allow slight difference
+  CHECK(std::abs(millis - diff) < 10); // allow slight difference
 }
 
 TEST_CASE("Timer::high_res", "[Timer]") {
   const int64_t time = Timer::high_res();
   const int64_t sdlTime = SDL_GetPerformanceCounter();
   const int64_t diff = std::abs(time - sdlTime);
-  CHECK(diff < 5);
+  CHECK(diff < 25);
 }
 
 TEST_CASE("Timer::high_res_freq", "[Timer]") {

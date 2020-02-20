7d97b60a3dd340a352ac4555520a742363ad4752
Albin Johansson <albinoscar99@hotmail.com>
2020-02-20 08:29:46 +0100

    Update message_box_test.cpp

--- test/unittests/message_box_test.cpp
+++ test/unittests/message_box_test.cpp
@@ -1,19 +1,14 @@
 #include "message_box.h"
 
-#include "catch.hpp"
+#include <catch.hpp>
+#include <cstdint>
+#include <string>
 
+#include "colors.h"
+
+using namespace centurion;
 using namespace centurion::messagebox;
-
-TEST_CASE("MessageBox::set_type", "[MessageBox]")
-{
-  MessageBox mb;
-  CHECK(MessageBoxID::Info == mb.get_type());
-
-  const auto type = MessageBoxID::Error;
-  mb.set_type(type);
-
-  CHECK(type == mb.get_type());
-}
+using namespace centurion::video;
 
 TEST_CASE("MessageBoxID enum values", "[MessageBox]")
 {
@@ -24,18 +19,24 @@ TEST_CASE("MessageBoxID enum values", "[MessageBox]")
   CHECK(SDL_MESSAGEBOX_INFORMATION == MessageBoxID::Info);
   CHECK(SDL_MESSAGEBOX_ERROR == MessageBoxID::Error);
   CHECK(SDL_MESSAGEBOX_WARNING == MessageBoxID::Warning);
+
+  CHECK(MessageBoxID::Info != SDL_MESSAGEBOX_ERROR);
+  CHECK(SDL_MESSAGEBOX_WARNING != MessageBoxID::Error);
 }
 
-TEST_CASE("ButtonData enum values", "[MessageBox]")
+TEST_CASE("ButtonDataHint enum values", "[MessageBox]")
 {
   CHECK(ButtonDataHint::ReturnKey == SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT);
   CHECK(ButtonDataHint::EscapeKey == SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT);
 
   CHECK(SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT == ButtonDataHint::ReturnKey);
   CHECK(SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT == ButtonDataHint::EscapeKey);
+
+  CHECK(ButtonDataHint::None != SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT);
+  CHECK(SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT != ButtonDataHint::EscapeKey);
 }
 
-TEST_CASE("ColorSchemeType enum values", "[MessageBox]")
+TEST_CASE("ColorType enum values", "[MessageBox]")
 {
   CHECK(ColorType::ButtonSelected == SDL_MESSAGEBOX_COLOR_BUTTON_SELECTED);
   CHECK(ColorType::ButtonBackground == SDL_MESSAGEBOX_COLOR_BUTTON_BACKGROUND);
@@ -46,4 +47,53 @@ TEST_CASE("ColorSchemeType enum values", "[MessageBox]")
   CHECK(SDL_MESSAGEBOX_COLOR_BUTTON_BACKGROUND == ColorType::ButtonBackground);
   CHECK(SDL_MESSAGEBOX_COLOR_BUTTON_BORDER == ColorType::ButtonBorder);
   CHECK(SDL_MESSAGEBOX_COLOR_BACKGROUND == ColorType::Background);
+
+  CHECK(ColorType::Background != SDL_MESSAGEBOX_COLOR_BUTTON_BACKGROUND);
+  CHECK(SDL_MESSAGEBOX_COLOR_BUTTON_BORDER != ColorType::ButtonSelected);
+}
+
+TEST_CASE("ButtonData general test", "[MessageBox]")
+{
+  const auto hint = ButtonDataHint::ReturnKey;
+  const auto id = 7;
+  const std::string text{"foo"};
+
+  const ButtonData buttonData{hint, id, text};
+  const SDL_MessageBoxButtonData data = buttonData;
+
+  CHECK(id == data.buttonid);
+  CHECK(static_cast<uint32_t>(hint) == data.flags);
+  CHECK_THAT(text, Catch::Equals(data.text));
+}
+
+TEST_CASE("ColorScheme general test", "[MessageBox]")
+{
+  ColorScheme scheme;
+
+  scheme.set_color(ColorType::Background, video::aquamarine);
+  scheme.set_color(ColorType::ButtonBackground, video::azure);
+  scheme.set_color(ColorType::ButtonBorder, video::tomato);
+  scheme.set_color(ColorType::ButtonSelected, video::cornsilk);
+
+  const auto sdlScheme = static_cast<SDL_MessageBoxColorScheme>(scheme);
+
+  const auto index = [](ColorType type) noexcept {
+    return static_cast<int>(type);
+  };
+
+  CHECK(sdlScheme.colors[index(ColorType::Background)] == aquamarine);
+  CHECK(sdlScheme.colors[index(ColorType::ButtonBackground)] == azure);
+  CHECK(sdlScheme.colors[index(ColorType::ButtonBorder)] == tomato);
+  CHECK(sdlScheme.colors[index(ColorType::ButtonSelected)] == cornsilk);
+}
+
+TEST_CASE("MessageBox::set_type", "[MessageBox]")
+{
+  MessageBox mb;
+  CHECK(MessageBoxID::Info == mb.get_type());
+
+  const auto type = MessageBoxID::Error;
+  mb.set_type(type);
+
+  CHECK(type == mb.get_type());
 }

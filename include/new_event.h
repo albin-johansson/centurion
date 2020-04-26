f230409b6a5436f8e2bd9e5efb44db0cf7ee6596
Albin Johansson <albinoscar99@hotmail.com>
2020-04-26 13:41:29 +0200

    Document QuitEvent

--- include/new_event.h
+++ include/new_event.h
@@ -128,12 +128,36 @@ class MouseWheelEvent : public TEvent<SDL_MouseWheelEvent> {
 class MultiGestureEvent : public TEvent<SDL_MultiGestureEvent> {
 };
 
+/**
+ * The QuitEvent class represents the event of the user wanting to close the
+ * application, usually by pressing the "X"-button on the window frame.
+ *
+ * @see SDL_QuitEvent
+ * @since 4.0.0
+ */
 class QuitEvent : public TEvent<SDL_QuitEvent> {
  public:
+  /**
+   * Creates a default-initialized quit event.
+   *
+   * @since 4.0.0
+   */
   CENTURION_API QuitEvent() noexcept;
 
+  /**
+   * Creates a quit event based on the supplied SDL event.
+   *
+   * @param event the SDL event that will be copied.
+   * @since 4.0.0
+   */
   CENTURION_API explicit QuitEvent(const SDL_QuitEvent& event) noexcept;
 
+  /**
+   * Creates a quit event based on the supplied SDL event.
+   *
+   * @param event the SDL event that will be moved.
+   * @since 4.0.0
+   */
   CENTURION_API explicit QuitEvent(SDL_QuitEvent&& event) noexcept;
 };
 

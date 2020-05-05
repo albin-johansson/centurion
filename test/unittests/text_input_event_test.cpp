9727b30bd2354d6a60b00e13e09f110eece6ccd8
Albin Johansson <albinoscar99@hotmail.com>
2020-05-05 22:57:27 +0200

    Add TextInputEvent

---
+++ test/unittests/text_input_event_test.cpp
@@ -0,0 +1,30 @@
+#include "text_input_event.h"
+
+#include <catch.hpp>
+
+using namespace centurion::event;
+
+TEST_CASE("TextInputEvent::set_window_id", "[TextInputEvent]")
+{
+  TextInputEvent event;
+
+  const auto id = 23;
+  event.set_window_id(id);
+
+  CHECK(event.window_id() == id);
+}
+
+TEST_CASE("TextInputEvent::window_id", "[TextInputEvent]")
+{
+  SDL_TextInputEvent sdlEvent{.windowID = 8};
+  TextInputEvent event{sdlEvent};
+
+  CHECK(event.window_id() == sdlEvent.windowID);
+}
+
+TEST_CASE("TextInputEvent::text", "[TextInputEvent]")
+{
+  SDL_TextInputEvent sdlEvent{SDL_TEXTINPUT, 1, 1, "hello"};
+  TextInputEvent event{sdlEvent};
+  CHECK_THAT(event.text(), Catch::Equals("hello"));
+}

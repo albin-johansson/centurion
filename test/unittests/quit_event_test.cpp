96ab56cd248bef29ff77ad4c5aeae4fe5c580c66
Albin Johansson <albinoscar99@hotmail.com>
2020-05-16 14:08:03 +0200

    Add quit_event_test.cpp

---
+++ test/unittests/quit_event_test.cpp
@@ -0,0 +1,21 @@
+#include "quit_event.h"
+
+#include <catch.hpp>
+
+using namespace centurion::event;
+
+TEST_CASE("QuitEvent()", "[QuitEvent]")
+{
+  CHECK_NOTHROW(QuitEvent{});
+}
+
+TEST_CASE("QuitEvent(const SDL_QuitEvent&)", "[QuitEvent]")
+{
+  SDL_QuitEvent qEvent;
+  CHECK_NOTHROW(QuitEvent{qEvent});
+}
+
+TEST_CASE("QuitEvent(SDL_QuitEvent&&)", "[QuitEvent]")
+{
+  CHECK_NOTHROW(QuitEvent{{}});
+}

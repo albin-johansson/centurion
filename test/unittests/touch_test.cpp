5614b84f7555207fafd0315f364f5cf33de6dd05
Albin Johansson <albinoscar99@hotmail.com>
2020-07-06 12:31:47 +0200

    Update touch_test.cpp

--- test/unittests/touch_test.cpp
+++ test/unittests/touch_test.cpp
@@ -19,8 +19,7 @@ TEST_CASE("DeviceType enum", "[Touch]")
     CHECK(SDL_TOUCH_DEVICE_INDIRECT_ABSOLUTE == DeviceType::IndirectAbsolute);
     CHECK(SDL_TOUCH_DEVICE_INDIRECT_RELATIVE == DeviceType::IndirectRelative);
 
-    CHECK(
-        !(DeviceType::IndirectAbsolute == SDL_TOUCH_DEVICE_DIRECT));  // NOLINT
+    CHECK(!(DeviceType::IndirectAbsolute == SDL_TOUCH_DEVICE_DIRECT));
   }
 
   SECTION("Operator !=")
@@ -30,40 +29,40 @@ TEST_CASE("DeviceType enum", "[Touch]")
   }
 }
 
-TEST_CASE("num_devices", "[Touch]")
+TEST_CASE("touch::num_devices", "[Touch]")
 {
   CHECK(num_devices() == SDL_GetNumTouchDevices());
 }
 
-TEST_CASE("get_device", "[Touch]")
+TEST_CASE("touch::get_device", "[Touch]")
 {
   const auto device = get_device(0);
   CHECK(!device.has_value());
 }
 
-TEST_CASE("type_of", "[Touch]")
+TEST_CASE("touch::type_of", "[Touch]")
 {
   CHECK(type_of(0) == SDL_GetTouchDeviceType(0));
 }
 
-TEST_CASE("num_fingers", "[Touch]")
+TEST_CASE("touch::num_fingers", "[Touch]")
 {
   CHECK(num_fingers(0) == SDL_GetNumTouchFingers(0));
 }
 
-TEST_CASE("get_finger", "[Touch]")
+TEST_CASE("touch::get_finger", "[Touch]")
 {
   const auto finger = get_finger(0, 0);
   CHECK(!finger.has_value());
 }
 
-TEST_CASE("touch_mouse_id", "[Touch]")
+TEST_CASE("touch::touch_mouse_id", "[Touch]")
 {
   CHECK(touch_mouse_id() == SDL_TOUCH_MOUSEID);
   CHECK(touch_mouse_id() != SDL_MOUSE_TOUCHID);
 }
 
-TEST_CASE("mouse_touch_id", "[Touch]")
+TEST_CASE("touch::mouse_touch_id", "[Touch]")
 {
   CHECK(mouse_touch_id() == SDL_MOUSE_TOUCHID);
   CHECK(mouse_touch_id() != SDL_TOUCH_MOUSEID);

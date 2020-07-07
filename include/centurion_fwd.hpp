be828edecb3da864c7d18a9a1834bc915f416168
Albin Johansson <albinoscar99@hotmail.com>
2020-07-07 13:51:34 +0200

    Add forward declaration header

---
+++ include/centurion_fwd.hpp
@@ -0,0 +1,17 @@
+#ifndef CENTURION_FWD_HEADER
+#define CENTURION_FWD_HEADER
+
+namespace centurion {
+
+template <typename FontKey>
+class Renderer;
+
+class Texture;
+
+class Window;
+
+class Font;
+
+}
+
+#endif  // CENTURION_FWD_HEADER

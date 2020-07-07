d19d6f90674435aee114bc2af0a31bad0f2bacf7
Albin Johansson <albinoscar99@hotmail.com>
2020-07-07 23:58:49 +0200

    Update centurion_fwd.hpp

--- include/centurion_fwd.hpp
+++ include/centurion_fwd.hpp
@@ -1,6 +1,19 @@
 #ifndef CENTURION_FWD_HEADER
 #define CENTURION_FWD_HEADER
 
+/**
+ * @file centurion_fwd.hpp
+ *
+ * @brief Provides forward declarations.
+ *
+ * @todo Provide more forward declarations.
+ *
+ * @author Albin Johansson
+ * @date 2019-2020
+ * @copyright MIT License
+ * @since 5.0.0
+ */
+
 namespace centurion {
 
 template <typename FontKey>

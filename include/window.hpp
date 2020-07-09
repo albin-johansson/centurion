0a104d9d82e58ca3b76d8d132e64a6c0003ff9f1
Albin Johansson <albinoscar99@hotmail.com>
2020-07-09 16:13:53 +0200

    Update window.hpp

--- include/window.hpp
+++ include/window.hpp
@@ -828,7 +828,6 @@ static_assert(std::is_nothrow_move_assignable_v<Window>);
 static_assert(std::is_nothrow_move_constructible_v<Window>);
 static_assert(!std::is_copy_assignable_v<Window>);
 static_assert(!std::is_copy_constructible_v<Window>);
-static_assert(std::is_convertible_v<Window, SDL_Window*>);
 static_assert(std::is_default_constructible_v<Window>);
 static_assert(std::is_nothrow_destructible_v<Window>);
 

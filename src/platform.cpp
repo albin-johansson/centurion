eb4a1724d78c9edf1c90e842c464dd0bdbb5cfba
Albin Johansson <albinoscar99@hotmail.com>
2020-07-10 23:29:22 +0200

    Update platform.cpp

--- src/platform.cpp
+++ src/platform.cpp
@@ -10,16 +10,16 @@ namespace centurion::platform {
 CENTURION_DEF
 auto id() noexcept -> platform_id
 {
-  const auto platform = name();
-  if (platform == "Windows") {
+  const auto platformName = name();
+  if (platformName == "Windows") {
     return platform_id::windows;
-  } else if (platform == "Mac OS X") {
+  } else if (platformName == "Mac OS X") {
     return platform_id::mac_osx;
-  } else if (platform == "Linux") {
+  } else if (platformName == "Linux") {
     return platform_id::linux;
-  } else if (platform == "iOS") {
+  } else if (platformName == "iOS") {
     return platform_id::ios;
-  } else if (platform == "Android") {
+  } else if (platformName == "Android") {
     return platform_id::android;
   } else {
     return platform_id::unknown;

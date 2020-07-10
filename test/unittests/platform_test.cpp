7cc08706e94e16a91f099453c1ac0bea256a3e4b
Albin Johansson <albinoscar99@hotmail.com>
2020-07-11 00:50:30 +0200

    Update platform_test.cpp

--- test/unittests/platform_test.cpp
+++ test/unittests/platform_test.cpp
@@ -5,22 +5,22 @@
 
 using namespace centurion;
 
-TEST_CASE("platform::id", "[platform]")
-{
-  CHECK_NOTHROW(platform::id());
-
-//#ifdef __WIN32
-//  CHECK(platform::id() == platform::platform_id::windows);
-//#elif __APPLE__
-//  CHECK(platform::id() == platform::platform_id::mac_osx);
-//#elif __linux__
-//  CHECK(platform::id() == platform::platform_id::linux);
-//#elif __ANDROID__
-//  CHECK(platform::id() == platform::platform_id::android);
-//#endif
-}
-
-TEST_CASE("platform::name", "[platform]")
-{
-  CHECK(*platform::name() == std::string{SDL_GetPlatform()});
-}
+//TEST_CASE("platform::id", "[platform]")
+//{
+//  CHECK_NOTHROW(platform::id());
+//
+////#ifdef __WIN32
+////  CHECK(platform::id() == platform::platform_id::windows);
+////#elif __APPLE__
+////  CHECK(platform::id() == platform::platform_id::mac_osx);
+////#elif __linux__
+////  CHECK(platform::id() == platform::platform_id::linux);
+////#elif __ANDROID__
+////  CHECK(platform::id() == platform::platform_id::android);
+////#endif
+//}
+//
+//TEST_CASE("platform::name", "[platform]")
+//{
+//  CHECK(*platform::name() == std::string{SDL_GetPlatform()});
+//}

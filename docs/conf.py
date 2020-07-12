035b48c502528dfacfd1aebae2c6631b6181a0d0
Albin Johansson <albinoscar99@hotmail.com>
2020-07-12 22:43:34 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import subprocess, os
 
 # -- Project information -----------------------------------------------------
 
@@ -57,3 +58,10 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Build doxygen docs
+
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+if read_the_docs_build:
+  subprocess.call('cd ..; doxygen', shell = True)

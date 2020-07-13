2f81e911354022ad316962049be823e48e6faf27
Albin Johansson <albinoscar99@hotmail.com>
2020-07-14 00:53:44 +0200

    Update area_test.cpp

--- test/unittests/area_test.cpp
+++ test/unittests/area_test.cpp
@@ -2,20 +2,20 @@
 
 #include <catch.hpp>
 
-using namespace centurion;
+#include "centurion_as_ctn.hpp"
 
 TEST_CASE("Construction", "[area]")
 {
   SECTION("Default values")
   {
-    const area_i area;
+    const ctn::area_i area;
     CHECK(area.width == 0);
     CHECK(area.height == 0);
   }
 
   const auto w = 7353;
   const auto h = 8395;
-  const area_i area{w, h};
+  const ctn::area_i area{w, h};
 
   CHECK(area.width == w);
   CHECK(area.height == h);
@@ -25,22 +25,22 @@ TEST_CASE("Area operator==", "[area]")
 {
   SECTION("Reflexivity")
   {
-    const area_i area{234, 12};
+    const ctn::area_i area{234, 12};
     CHECK(area == area);
   }
 
   SECTION("Equal")
   {
-    const area_i first{47, 9123};
-    const area_i second{first};
+    const ctn::area_i first{47, 9123};
+    const ctn::area_i second{first};
     CHECK(first == second);
     CHECK(second == first);
   }
 
   SECTION("Different")
   {
-    const area_i first{1238, 594};
-    const area_i second{8882, 123};
+    const ctn::area_i first{1238, 594};
+    const ctn::area_i second{8882, 123};
     CHECK_FALSE(first == second);
     CHECK_FALSE(second == first);
   }
@@ -50,22 +50,22 @@ TEST_CASE("Area operator!=", "[area]")
 {
   SECTION("Self")
   {
-    const area_i area{234, 12};
+    const ctn::area_i area{234, 12};
     CHECK_FALSE(area != area);
   }
 
   SECTION("Equal")
   {
-    const area_i first{47, 9123};
-    const area_i second{first};
+    const ctn::area_i first{47, 9123};
+    const ctn::area_i second{first};
     CHECK_FALSE(first != second);
     CHECK_FALSE(second != first);
   }
 
   SECTION("Different")
   {
-    const area_i first{1238, 594};
-    const area_i second{8882, 123};
+    const ctn::area_i first{1238, 594};
+    const ctn::area_i second{8882, 123};
     CHECK(first != second);
     CHECK(second != first);
   }

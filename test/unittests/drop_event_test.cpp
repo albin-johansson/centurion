76930a708b2a71467cd50ea8f4a0e059216d4390
Albin Johansson <albinoscar99@hotmail.com>
2020-07-14 01:42:26 +0200

    Update drop_event_test.cpp

--- test/unittests/drop_event_test.cpp
+++ test/unittests/drop_event_test.cpp
@@ -1,17 +1,16 @@
 #include <catch.hpp>
 
 #include "event.hpp"
+#include "centurion_as_ctn.hpp"
 
-using namespace centurion;
-
-TEST_CASE("DropEvent move constructor", "[DropEvent]")
+TEST_CASE("drop_event move constructor", "[drop_event]")
 {
-  CHECK_NOTHROW(drop_event{{}});
+  CHECK_NOTHROW(ctn::drop_event{{}});
 }
 
-TEST_CASE("DropEvent::set_will_free_file", "[DropEvent]")
+TEST_CASE("drop_event::set_will_free_file", "[drop_event]")
 {
-  drop_event event;
+  ctn::drop_event event;
 
   event.set_will_free_file(true);
   CHECK(event.will_free_file());
@@ -20,21 +19,21 @@ TEST_CASE("DropEvent::set_will_free_file", "[DropEvent]")
   CHECK(!event.will_free_file());
 }
 
-TEST_CASE("DropEvent::set_file", "[DropEvent]")
+TEST_CASE("drop_event::set_file", "[drop_event]")
 {
-  drop_event event;
+  ctn::drop_event event;
   CHECK_NOTHROW(event.set_file(nullptr));
 
-  // This is the only time in the tests that a DropEvent should free the file,
+  // This is the only time in the tests that a drop_event should free the file,
   // check the code coverage reports in order to see if it's freed.
   auto* file = static_cast<char*>(SDL_malloc(sizeof(char)));
   event.set_file(file);
   event.set_will_free_file(true);
 }
 
-TEST_CASE("DropEvent::set_window_id", "[DropEvent]")
+TEST_CASE("drop_event::set_window_id", "[drop_event]")
 {
-  drop_event event;
+  ctn::drop_event event;
 
   const auto id = 84;
   event.set_window_id(id);
@@ -42,31 +41,31 @@ TEST_CASE("DropEvent::set_window_id", "[DropEvent]")
   CHECK(event.window_id() == id);
 }
 
-TEST_CASE("DropEvent::will_free_file", "[DropEvent]")
+TEST_CASE("drop_event::will_free_file", "[drop_event]")
 {
-  drop_event event;
+  ctn::drop_event event;
   CHECK(!event.will_free_file());
 }
 
-TEST_CASE("DropEvent::file", "[DropEvent]")
+TEST_CASE("drop_event::file", "[drop_event]")
 {
   char file = '1';  // pretend this is some raw data
   SDL_DropEvent sdlEvent;
   sdlEvent.file = &file;  // shouldn't be deleted, otherwise we're in trouble
 
-  drop_event event{sdlEvent};
+  ctn::drop_event event{sdlEvent};
 
   CHECK(event.file());
   CHECK(*event.file() == file);
 }
 
-TEST_CASE("DropEvent::window_id", "[DropEvent]")
+TEST_CASE("drop_event::window_id", "[drop_event]")
 {
   SDL_DropEvent sdlEvent{};
   sdlEvent.windowID = 32;
   sdlEvent.file = nullptr;
 
-  drop_event event{sdlEvent};
+  ctn::drop_event event{sdlEvent};
 
   CHECK(event.window_id() == sdlEvent.windowID);
 }

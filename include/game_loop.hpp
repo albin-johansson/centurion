069001bd07718bdf11489c65dcf192a3326f46d9
Albin Johansson <albinoscar99@hotmail.com>
2020-07-22 18:35:24 +0200

    Update game_loop.hpp

--- include/game_loop.hpp
+++ include/game_loop.hpp
@@ -1,15 +1,57 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019-2020 Albin Johansson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * @file game_loop.hpp
+ *
+ * @brief Provides various different game loop implementations.
+ *
+ * @since 5.0.0
+ *
+ * @see `variable_timestep_loop`
+ * @see `semi_fixed_timestep_loop`
+ * @see `fixed_timestep_loop`
+ *
+ * @author Albin Johansson
+ *
+ * @date 2019-2020
+ *
+ * @copyright MIT License
+ */
+
 #ifndef CENTURION_GAME_LOOP_HEADER
 #define CENTURION_GAME_LOOP_HEADER
 
 #include <cassert>
 #include <entt.hpp>
-#include <type_traits>
 
 #include "centurion_api.hpp"
 #include "counter.hpp"
-#include "renderer.hpp"
-#include "screen.hpp"
-#include "window.hpp"
+
+#ifdef CENTURION_USE_PRAGMA_ONCE
+#pragma once
+#endif  // CENTURION_USE_PRAGMA_ONCE
 
 namespace centurion::experimental {
 

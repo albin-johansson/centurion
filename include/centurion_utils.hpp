9b1c473d8a7630ba30c88aaa45a7fae8ee7a3b3c
Albin Johansson <albinoscar99@hotmail.com>
2020-07-26 16:07:11 +0200

    Update centurion_utils.hpp

--- include/centurion_utils.hpp
+++ include/centurion_utils.hpp
@@ -58,6 +58,35 @@
 #pragma once
 #endif  // CENTURION_USE_PRAGMA_ONCE
 
+namespace centurion {
+
+/**
+ * @brief Casts a value to a value of another type.
+ *
+ * @ingroup misc
+ *
+ * @details This is the default implementation, which simply attempts to use
+ * `static_cast`. The idea is that this function will be specialized for
+ * various Centurion and SDL types. This is useful because it isn't always
+ * possible to implement conversion operators as members.
+ *
+ * @tparam To the type of the value that will be converted.
+ * @tparam From the type that the value will be casted to.
+ *
+ * @param from the value that will be converted.
+ *
+ * @return the result of casting the supplied value to the specified type.
+ *
+ * @since 5.0.0
+ */
+template <typename To, typename From>
+[[nodiscard]] constexpr auto cast(const From& from) noexcept -> To
+{
+  return static_cast<To>(from);
+}
+
+/// @cond FALSE
+
 /**
  * @namespace centurion::detail
  *
@@ -66,7 +95,7 @@
  * @warning Do not use anything that resides in this namespace, the contents
  * may change at any time without warning.
  */
-namespace centurion::detail {
+namespace detail {
 
 /**
  * @brief Returns the corresponding `SDL_bool` value for the supplied boolean
@@ -128,6 +157,10 @@ template <typename T>
   }
 }
 
-}  // namespace centurion::detail
+}  // namespace detail
+
+/// @endcond
+
+}  // namespace centurion
 
 #endif  // CENTURION_CENTURION_UTILS_HEADER

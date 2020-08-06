d0f3a611d6b93e7a90e056d69d66b6408ec79bcd
Albin Johansson <albinoscar99@hotmail.com>
2020-08-06 22:23:54 +0200

    Update sharing_texture_test.cpp

--- test/unittests/sharing_texture_test.cpp
+++ test/unittests/sharing_texture_test.cpp
@@ -6,23 +6,30 @@
 #include "renderer.hpp"
 #include "window.hpp"
 
-TEST_CASE("sharing_texture::", "[sharing_texture]")
+TEST_CASE("sharing_texture implicit sharing semantics", "[sharing_texture]")
 {
   ctn::window window;
   ctn::renderer renderer{window};
 
-  // todo enable creating sharing_texture from rvalue texture
+  ctn::sharing_texture fst{renderer, "resources/panda.png"};
+  ctn::sharing_texture snd{fst};
+
+  CHECK(fst.get() == snd.get());
+
+  const auto alpha = 0x34;
+  fst.set_alpha(alpha);
+
+  CHECK(fst.alpha() == alpha);
+  CHECK(snd.alpha() == alpha);
+}
+
+TEST_CASE("sharing_texture from texture", "[sharing_texture]")
+{
+  ctn::window window;
+  ctn::renderer renderer{window};
 
   ctn::texture fst{renderer, "resources/panda.png"};
   ctn::sharing_texture snd{std::move(fst)};
 
   CHECK(!fst.get());
-
-//  CHECK(fst.get() == snd.get());
-
-//  const auto alpha = 0x34;
-//  fst.set_alpha(alpha);
-//
-//  CHECK(fst.alpha() == alpha);
-//  CHECK(snd.alpha() == alpha);
 }

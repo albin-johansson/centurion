ae6c3fd5dd099bf022fcdc6e9253809909c65ee0
Albin Johansson <albinoscar99@hotmail.com>
2020-08-30 13:15:25 +0200

    Update platform_test.cpp

--- test/unittests/platform_test.cpp
+++ test/unittests/platform_test.cpp
@@ -1,28 +1,26 @@
-//#include "platform.hpp"
-
-// FIXME investigate
+#include "platform.hpp"
 
 #include <catch.hpp>
 #include <string>
 
-// using namespace centurion;
+#include "centurion_as_ctn.hpp"
 
-// TEST_CASE("platform::id", "[platform]")
-//{
-//  CHECK_NOTHROW(platform::id());
-//
-////#ifdef __WIN32
-////  CHECK(platform::id() == platform::platform_id::windows);
-////#elif __APPLE__
-////  CHECK(platform::id() == platform::platform_id::mac_osx);
-////#elif __linux__
-////  CHECK(platform::id() == platform::platform_id::linux);
-////#elif __ANDROID__
-////  CHECK(platform::id() == platform::platform_id::android);
-////#endif
-//}
-//
-// TEST_CASE("platform::name", "[platform]")
-//{
-//  CHECK(*platform::name() == std::string{SDL_GetPlatform()});
-//}
+TEST_CASE("platform::id", "[platform]")
+{
+  CHECK_NOTHROW(ctn::platform::id());
+
+#ifdef __WIN32
+  CHECK(ctn::platform::id() == ctn::platform::platform_id::windows);
+#elif __APPLE__
+  CHECK(ctn::platform::id() == ctn::platform::platform_id::mac_osx);
+#elif __linux__
+  CHECK(ctn::platform::id() == ctn::platform::platform_id::linux);
+#elif __ANDROID__
+  CHECK(ctn::platform::id() == ctn::platform::platform_id::android);
+#endif
+}
+
+TEST_CASE("platform::name", "[platform]")
+{
+  CHECK(*ctn::platform::name() == std::string{SDL_GetPlatform()});
+}

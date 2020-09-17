0680e6ba1f20952390157d6ce84001447d2798a7
Albin Johansson <albinoscar99@hotmail.com>
2020-09-17 22:06:17 +0200

    Remove renderer.ipp

--- include/renderer.hpp
+++ include/renderer.hpp
@@ -575,6 +575,100 @@ class renderer final : public basic_renderer<renderer>
       -> basic_rect<T>;
 };
 
+template <typename T>
+auto renderer::translate(const basic_point<T>& point) const noexcept
+    -> basic_point<T>
+{
+  const auto rect = viewport();
+
+  using value_type = typename basic_point<T>::value_type;
+
+  const auto x = point.x() - static_cast<value_type>(rect.x());
+  const auto y = point.y() - static_cast<value_type>(rect.y());
+
+  return basic_point<T>{x, y};
+}
+
+template <typename T>
+auto renderer::translate(const basic_rect<T>& rect) const noexcept
+    -> basic_rect<T>
+{
+  return basic_rect<T>{translate(rect.position()), rect.size()};
+}
+
+template <typename T>
+void renderer::draw_rect_t(const basic_rect<T>& rect) noexcept
+{
+  draw_rect(translate(rect));
+}
+
+template <typename T>
+void renderer::fill_rect_t(const basic_rect<T>& rect) noexcept
+{
+  fill_rect(translate(rect));
+}
+
+template <typename T>
+void renderer::render_t(const texture& texture,
+                        const basic_point<T>& position) noexcept
+{
+  render(texture, translate(position));
+}
+
+template <typename T>
+void renderer::render_t(const texture& texture,
+                        const basic_rect<T>& destination) noexcept
+{
+  render(texture, translate(destination));
+}
+
+template <typename T>
+void renderer::render_t(const texture& texture,
+                        const irect& source,
+                        const basic_rect<T>& destination) noexcept
+{
+  render(texture, source, translate(destination));
+}
+
+template <typename T>
+void renderer::render_t(const texture& texture,
+                        const irect& source,
+                        const basic_rect<T>& destination,
+                        double angle) noexcept
+{
+  render(texture, source, translate(destination), angle);
+}
+
+template <typename R, typename P>
+void renderer::render_t(const texture& texture,
+                        const irect& source,
+                        const basic_rect<R>& destination,
+                        double angle,
+                        const basic_point<P>& center) noexcept
+{
+  render(texture, source, translate(destination), angle, center);
+}
+
+template <typename R, typename P>
+void renderer::render_t(const texture& texture,
+                        const irect& source,
+                        const basic_rect<R>& destination,
+                        double angle,
+                        const basic_point<P>& center,
+                        SDL_RendererFlip flip) noexcept
+{
+  render(texture, source, translate(destination), angle, center, flip);
+}
+
+template <typename... Args>
+void renderer::emplace_font(hash_id id, Args&&... args)
+{
+  if (m_fonts.find(id) != m_fonts.end()) {
+    remove_font(id);
+  }
+  m_fonts.emplace(id, font{std::forward<Args>(args)...});
+}
+
 static_assert(std::is_final_v<renderer>);
 static_assert(std::is_nothrow_destructible_v<renderer>);
 
@@ -615,7 +709,5 @@ auto operator<<(std::ostream& stream, const renderer& renderer)
     -> std::ostream&;
 
 }  // namespace centurion
-
-#include "renderer.ipp"
 
 #endif  // CENTURION_RENDERER_HEADER

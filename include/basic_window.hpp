32a805509930e6f6c2427ee984e6d08bc20a5b28
Albin Johansson <albinoscar99@hotmail.com>
2020-09-18 17:59:46 +0200

    Update basic_window.hpp

--- include/basic_window.hpp
+++ include/basic_window.hpp
@@ -50,7 +50,7 @@ namespace centurion {
  *
  * @brief Provides the base implementation for windows.
  *
- * @tparam Derived the type of the derived window.
+ * @tparam T the type of the derived window.
  *
  * @since 5.0.0
  *
@@ -59,7 +59,7 @@ namespace centurion {
  *
  * @headerfile basic_window.hpp
  */
-template <class Derived>
+template <class T>
 class basic_window
 {
  public:
@@ -619,50 +619,50 @@ class basic_window
       -> renderer_handle;
 };
 
-template <class Derived>
-auto basic_window<Derived>::ptr() const noexcept -> SDL_Window*
+template <class T>
+auto basic_window<T>::ptr() const noexcept -> SDL_Window*
 {
-  return static_cast<const Derived*>(this)->get();
+  return static_cast<const T*>(this)->get();
 }
 
-template <class Derived>
-void basic_window<Derived>::show() noexcept
+template <class T>
+void basic_window<T>::show() noexcept
 {
   SDL_ShowWindow(ptr());
 }
 
-template <class Derived>
-void basic_window<Derived>::hide() noexcept
+template <class T>
+void basic_window<T>::hide() noexcept
 {
   SDL_HideWindow(ptr());
 }
 
-template <class Derived>
-void basic_window<Derived>::raise() noexcept
+template <class T>
+void basic_window<T>::raise() noexcept
 {
   SDL_RaiseWindow(ptr());
 }
 
-template <class Derived>
-void basic_window<Derived>::center() noexcept
+template <class T>
+void basic_window<T>::center() noexcept
 {
   set_position({SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED});
 }
 
-template <class Derived>
-void basic_window<Derived>::maximize() noexcept
+template <class T>
+void basic_window<T>::maximize() noexcept
 {
   SDL_MaximizeWindow(ptr());
 }
 
-template <class Derived>
-void basic_window<Derived>::minimize() noexcept
+template <class T>
+void basic_window<T>::minimize() noexcept
 {
   SDL_MinimizeWindow(ptr());
 }
 
-template <class Derived>
-void basic_window<Derived>::set_fullscreen(bool fullscreen) noexcept
+template <class T>
+void basic_window<T>::set_fullscreen(bool fullscreen) noexcept
 {
   const auto flag = static_cast<unsigned>(SDL_WINDOW_FULLSCREEN);
   SDL_SetWindowFullscreen(ptr(), fullscreen ? flag : 0);
@@ -672,240 +672,239 @@ void basic_window<Derived>::set_fullscreen(bool fullscreen) noexcept
   }
 }
 
-template <class Derived>
-void basic_window<Derived>::set_fullscreen_desktop(bool fullscreen) noexcept
+template <class T>
+void basic_window<T>::set_fullscreen_desktop(bool fullscreen) noexcept
 {
   const auto flag = static_cast<unsigned>(SDL_WINDOW_FULLSCREEN_DESKTOP);
   SDL_SetWindowFullscreen(ptr(), fullscreen ? flag : 0);
 }
 
-template <class Derived>
-void basic_window<Derived>::set_decorated(bool decorated) noexcept
+template <class T>
+void basic_window<T>::set_decorated(bool decorated) noexcept
 {
   SDL_SetWindowBordered(ptr(), detail::convert_bool(decorated));
 }
 
-template <class Derived>
-void basic_window<Derived>::set_resizable(bool resizable) noexcept
+template <class T>
+void basic_window<T>::set_resizable(bool resizable) noexcept
 {
   SDL_SetWindowResizable(ptr(), detail::convert_bool(resizable));
 }
 
-template <class Derived>
-void basic_window<Derived>::set_width(int width) noexcept
+template <class T>
+void basic_window<T>::set_width(int width) noexcept
 {
   if (width > 0) {
     SDL_SetWindowSize(ptr(), width, height());
   }
 }
 
-template <class Derived>
-void basic_window<Derived>::set_height(int height) noexcept
+template <class T>
+void basic_window<T>::set_height(int height) noexcept
 {
   if (height > 0) {
     SDL_SetWindowSize(ptr(), width(), height);
   }
 }
 
-template <class Derived>
-void basic_window<Derived>::set_size(const iarea& size) noexcept
+template <class T>
+void basic_window<T>::set_size(const iarea& size) noexcept
 {
   if ((size.width > 0) && (size.height > 0)) {
     SDL_SetWindowSize(ptr(), size.width, size.height);
   }
 }
 
-template <class Derived>
-void basic_window<Derived>::set_icon(const surface& icon) noexcept
+template <class T>
+void basic_window<T>::set_icon(const surface& icon) noexcept
 {
   SDL_SetWindowIcon(ptr(), icon.get());
 }
 
-template <class Derived>
-void basic_window<Derived>::set_title(nn_czstring title) noexcept
+template <class T>
+void basic_window<T>::set_title(nn_czstring title) noexcept
 {
   SDL_SetWindowTitle(ptr(), title);
 }
 
-template <class Derived>
-void basic_window<Derived>::set_opacity(float opacity) noexcept
+template <class T>
+void basic_window<T>::set_opacity(float opacity) noexcept
 {
   SDL_SetWindowOpacity(ptr(), opacity);
 }
 
-template <class Derived>
-void basic_window<Derived>::set_min_size(const iarea& size) noexcept
+template <class T>
+void basic_window<T>::set_min_size(const iarea& size) noexcept
 {
   SDL_SetWindowMinimumSize(ptr(), size.width, size.height);
 }
 
-template <class Derived>
-void basic_window<Derived>::set_max_size(const iarea& size) noexcept
+template <class T>
+void basic_window<T>::set_max_size(const iarea& size) noexcept
 {
   SDL_SetWindowMaximumSize(ptr(), size.width, size.height);
 }
 
-template <class Derived>
-void basic_window<Derived>::set_position(const ipoint& position) noexcept
+template <class T>
+void basic_window<T>::set_position(const ipoint& position) noexcept
 {
   SDL_SetWindowPosition(ptr(), position.x(), position.y());
 }
 
-template <class Derived>
-void basic_window<Derived>::set_grab_mouse(bool grabMouse) noexcept
+template <class T>
+void basic_window<T>::set_grab_mouse(bool grabMouse) noexcept
 {
   SDL_SetWindowGrab(ptr(), detail::convert_bool(grabMouse));
 }
 
-template <class Derived>
-void basic_window<Derived>::set_brightness(float brightness) noexcept
+template <class T>
+void basic_window<T>::set_brightness(float brightness) noexcept
 {
   if (is_fullscreen()) {
     SDL_SetWindowBrightness(ptr(), std::clamp(brightness, 0.0f, 1.0f));
   }
 }
 
-template <class Derived>
-void basic_window<Derived>::set_capturing_mouse(bool capturingMouse) noexcept
+template <class T>
+void basic_window<T>::set_capturing_mouse(bool capturingMouse) noexcept
 {
   SDL_CaptureMouse(detail::convert_bool(capturingMouse));
 }
 
-template <class Derived>
-auto basic_window<Derived>::is_decorated() const noexcept -> bool
+template <class T>
+auto basic_window<T>::is_decorated() const noexcept -> bool
 {
   return !(flags() & SDL_WINDOW_BORDERLESS);
 }
 
-template <class Derived>
-auto basic_window<Derived>::grabbing_mouse() const noexcept -> bool
+template <class T>
+auto basic_window<T>::grabbing_mouse() const noexcept -> bool
 {
   return SDL_GetWindowGrab(ptr());
 }
 
-template <class Derived>
-auto basic_window<Derived>::is_resizable() const noexcept -> bool
+template <class T>
+auto basic_window<T>::is_resizable() const noexcept -> bool
 {
   return static_cast<bool>(flags() & SDL_WINDOW_RESIZABLE);
 }
 
-template <class Derived>
-auto basic_window<Derived>::is_fullscreen() const noexcept -> bool
+template <class T>
+auto basic_window<T>::is_fullscreen() const noexcept -> bool
 {
   return static_cast<bool>(flags() & SDL_WINDOW_FULLSCREEN);
 }
 
-template <class Derived>
-auto basic_window<Derived>::is_fullscreen_desktop() const noexcept -> bool
+template <class T>
+auto basic_window<T>::is_fullscreen_desktop() const noexcept -> bool
 {
   return static_cast<bool>(flags() & SDL_WINDOW_FULLSCREEN_DESKTOP);
 }
 
-template <class Derived>
-auto basic_window<Derived>::is_visible() const noexcept -> bool
+template <class T>
+auto basic_window<T>::is_visible() const noexcept -> bool
 {
   return static_cast<bool>(flags() & SDL_WINDOW_SHOWN);
 }
 
-template <class Derived>
-auto basic_window<Derived>::is_opengl() const noexcept -> bool
+template <class T>
+auto basic_window<T>::is_opengl() const noexcept -> bool
 {
   return static_cast<bool>(flags() & SDL_WINDOW_OPENGL);
 }
 
-template <class Derived>
-auto basic_window<Derived>::is_vulkan() const noexcept -> bool
+template <class T>
+auto basic_window<T>::is_vulkan() const noexcept -> bool
 {
   return static_cast<bool>(flags() & SDL_WINDOW_VULKAN);
 }
 
-template <class Derived>
-auto basic_window<Derived>::has_input_focus() const noexcept -> bool
+template <class T>
+auto basic_window<T>::has_input_focus() const noexcept -> bool
 {
   return static_cast<bool>(flags() & SDL_WINDOW_INPUT_FOCUS);
 }
 
-template <class Derived>
-auto basic_window<Derived>::has_mouse_focus() const noexcept -> bool
+template <class T>
+auto basic_window<T>::has_mouse_focus() const noexcept -> bool
 {
   return static_cast<bool>(flags() & SDL_WINDOW_MOUSE_FOCUS);
 }
 
-template <class Derived>
-auto basic_window<Derived>::is_foreign() const noexcept -> bool
+template <class T>
+auto basic_window<T>::is_foreign() const noexcept -> bool
 {
   return static_cast<bool>(flags() & SDL_WINDOW_FOREIGN);
 }
 
-template <class Derived>
-auto basic_window<Derived>::is_capturing_mouse() const noexcept -> bool
+template <class T>
+auto basic_window<T>::is_capturing_mouse() const noexcept -> bool
 {
   return static_cast<bool>(flags() & SDL_WINDOW_MOUSE_CAPTURE);
 }
 
-template <class Derived>
-auto basic_window<Derived>::always_on_top() const noexcept -> bool
+template <class T>
+auto basic_window<T>::always_on_top() const noexcept -> bool
 {
   return static_cast<bool>(flags() & SDL_WINDOW_ALWAYS_ON_TOP);
 }
 
-template <class Derived>
-auto basic_window<Derived>::is_minimized() const noexcept -> bool
+template <class T>
+auto basic_window<T>::is_minimized() const noexcept -> bool
 {
   return static_cast<bool>(flags() & SDL_WINDOW_MINIMIZED);
 }
 
-template <class Derived>
-auto basic_window<Derived>::is_maximized() const noexcept -> bool
+template <class T>
+auto basic_window<T>::is_maximized() const noexcept -> bool
 {
   return static_cast<bool>(flags() & SDL_WINDOW_MAXIMIZED);
 }
 
-template <class Derived>
-auto basic_window<Derived>::check_flag(SDL_WindowFlags flag) const noexcept
-    -> bool
+template <class T>
+auto basic_window<T>::check_flag(SDL_WindowFlags flag) const noexcept -> bool
 {
   return static_cast<bool>(flags() & flag);
 }
 
-template <class Derived>
-auto basic_window<Derived>::flags() const noexcept -> u32
+template <class T>
+auto basic_window<T>::flags() const noexcept -> u32
 {
   return SDL_GetWindowFlags(ptr());
 }
 
-template <class Derived>
-auto basic_window<Derived>::brightness() const noexcept -> float
+template <class T>
+auto basic_window<T>::brightness() const noexcept -> float
 {
   return SDL_GetWindowBrightness(ptr());
 }
 
-template <class Derived>
-auto basic_window<Derived>::opacity() const noexcept -> float
+template <class T>
+auto basic_window<T>::opacity() const noexcept -> float
 {
   float opacity{1};
   SDL_GetWindowOpacity(ptr(), &opacity);
   return opacity;
 }
 
-template <class Derived>
-auto basic_window<Derived>::x() const noexcept -> int
+template <class T>
+auto basic_window<T>::x() const noexcept -> int
 {
   int x{};
   SDL_GetWindowPosition(ptr(), &x, nullptr);
   return x;
 }
 
-template <class Derived>
-auto basic_window<Derived>::y() const noexcept -> int
+template <class T>
+auto basic_window<T>::y() const noexcept -> int
 {
   int y{};
   SDL_GetWindowPosition(ptr(), nullptr, &y);
   return y;
 }
 
-template <class Derived>
-auto basic_window<Derived>::position() const noexcept -> ipoint
+template <class T>
+auto basic_window<T>::position() const noexcept -> ipoint
 {
   int x{};
   int y{};
@@ -913,14 +912,14 @@ auto basic_window<Derived>::position() const noexcept -> ipoint
   return {x, y};
 }
 
-template <class Derived>
-auto basic_window<Derived>::id() const noexcept -> u32
+template <class T>
+auto basic_window<T>::id() const noexcept -> u32
 {
   return SDL_GetWindowID(ptr());
 }
 
-template <class Derived>
-auto basic_window<Derived>::display_index() const noexcept -> std::optional<int>
+template <class T>
+auto basic_window<T>::display_index() const noexcept -> std::optional<int>
 {
   const auto index = SDL_GetWindowDisplayIndex(ptr());
   if (index != -1) {
@@ -930,8 +929,8 @@ auto basic_window<Derived>::display_index() const noexcept -> std::optional<int>
   }
 }
 
-template <class Derived>
-auto basic_window<Derived>::min_size() const noexcept -> iarea
+template <class T>
+auto basic_window<T>::min_size() const noexcept -> iarea
 {
   int width{};
   int height{};
@@ -939,8 +938,8 @@ auto basic_window<Derived>::min_size() const noexcept -> iarea
   return {width, height};
 }
 
-template <class Derived>
-auto basic_window<Derived>::max_size() const noexcept -> iarea
+template <class T>
+auto basic_window<T>::max_size() const noexcept -> iarea
 {
   int width{};
   int height{};
@@ -948,38 +947,38 @@ auto basic_window<Derived>::max_size() const noexcept -> iarea
   return {width, height};
 }
 
-template <class Derived>
-auto basic_window<Derived>::width() const noexcept -> int
+template <class T>
+auto basic_window<T>::width() const noexcept -> int
 {
   int width{};
   SDL_GetWindowSize(ptr(), &width, nullptr);
   return width;
 }
 
-template <class Derived>
-auto basic_window<Derived>::height() const noexcept -> int
+template <class T>
+auto basic_window<T>::height() const noexcept -> int
 {
   int height{};
   SDL_GetWindowSize(ptr(), nullptr, &height);
   return height;
 }
 
-template <class Derived>
-auto basic_window<Derived>::size() const noexcept -> iarea
+template <class T>
+auto basic_window<T>::size() const noexcept -> iarea
 {
   iarea size{};
   SDL_GetWindowSize(ptr(), &size.width, &size.height);
   return size;
 }
 
-template <class Derived>
-auto basic_window<Derived>::get_pixel_format() const noexcept -> pixel_format
+template <class T>
+auto basic_window<T>::get_pixel_format() const noexcept -> pixel_format
 {
   return static_cast<pixel_format>(SDL_GetWindowPixelFormat(ptr()));
 }
 
-template <class Derived>
-auto basic_window<Derived>::title() const -> std::string
+template <class T>
+auto basic_window<T>::title() const -> std::string
 {
   return SDL_GetWindowTitle(ptr());
 }

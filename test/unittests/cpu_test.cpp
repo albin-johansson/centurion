15581a1afc06a6b12f738e0e75a88f7892127076
Albin Johansson <albinoscar99@hotmail.com>
2020-09-19 12:49:29 +0200

    Update cpu_test.cpp

--- test/unittests/cpu_test.cpp
+++ test/unittests/cpu_test.cpp
@@ -24,72 +24,72 @@ TEST_CASE("cpu::is_big_endian", "[cpu]")
 
 TEST_CASE("cpu::has_rdtsc", "[cpu]")
 {
-  CHECK(cen::cpu::has_rdtsc() == SDL_HasRDTSC());
+  CHECK(cen::cpu::has_rdtsc() == static_cast<bool>(SDL_HasRDTSC()));
 }
 
 TEST_CASE("cpu::has_altivec", "[cpu]")
 {
-  CHECK(cen::cpu::has_altivec() == SDL_HasAltiVec());
+  CHECK(cen::cpu::has_altivec() == static_cast<bool>(SDL_HasAltiVec()));
 }
 
 TEST_CASE("cpu::has_mmx", "[cpu]")
 {
-  CHECK(cen::cpu::has_mmx() == SDL_HasMMX());
+  CHECK(cen::cpu::has_mmx() == static_cast<bool>(SDL_HasMMX()));
 }
 
 TEST_CASE("cpu::has_3dnow", "[cpu]")
 {
-  CHECK(cen::cpu::has_3dnow() == SDL_Has3DNow());
+  CHECK(cen::cpu::has_3dnow() == static_cast<bool>(SDL_Has3DNow()));
 }
 
 TEST_CASE("cpu::has_sse", "[cpu]")
 {
-  CHECK(cen::cpu::has_sse() == SDL_HasSSE());
+  CHECK(cen::cpu::has_sse() == static_cast<bool>(SDL_HasSSE()));
 }
 
 TEST_CASE("cpu::has_sse2", "[cpu]")
 {
-  CHECK(cen::cpu::has_sse2() == SDL_HasSSE2());
+  CHECK(cen::cpu::has_sse2() == static_cast<bool>(SDL_HasSSE2()));
 }
 
 TEST_CASE("cpu::has_sse3", "[cpu]")
 {
-  CHECK(cen::cpu::has_sse3() == SDL_HasSSE3());
+  CHECK(cen::cpu::has_sse3() == static_cast<bool>(SDL_HasSSE3()));
 }
 
 TEST_CASE("cpu::has_sse41", "[cpu]")
 {
-  CHECK(cen::cpu::has_sse41() == SDL_HasSSE41());
+  CHECK(cen::cpu::has_sse41() == static_cast<bool>(SDL_HasSSE41()));
 }
 
 TEST_CASE("cpu::has_sse42", "[cpu]")
 {
-  CHECK(cen::cpu::has_sse42() == SDL_HasSSE42());
+  CHECK(cen::cpu::has_sse42() == static_cast<bool>(SDL_HasSSE42()));
 }
 
 TEST_CASE("cpu::has_avx", "[cpu]")
 {
-  CHECK(cen::cpu::has_avx() == SDL_HasAVX());
+  CHECK(cen::cpu::has_avx() == static_cast<bool>(SDL_HasAVX()));
 }
 
 TEST_CASE("cpu::has_avx2", "[cpu]")
 {
-  CHECK(cen::cpu::has_avx2() == SDL_HasAVX2());
+  CHECK(cen::cpu::has_avx2() == static_cast<bool>(SDL_HasAVX2()));
 }
 
 TEST_CASE("cpu::has_avx512f", "[cpu]")
 {
-  CHECK(cen::cpu::has_avx512f() == SDL_HasAVX512F());
+  CHECK(cen::cpu::has_avx512f() == static_cast<bool>(SDL_HasAVX512F()));
 }
 
 TEST_CASE("cpu::has_neon", "[cpu]")
 {
-  CHECK(cen::cpu::has_neon() == SDL_HasNEON());
+  CHECK(cen::cpu::has_neon() == static_cast<bool>(SDL_HasNEON()));
 }
 
 TEST_CASE("cpu::has_arm_simd", "[cpu]")
 {
-  CHECK(cen::cpu::has_arm_simd() == SDL_HasARMSIMD());
+  CHECK(cen::cpu::has_arm_simd() == static_cast<bool>(SDL_HasARMSIMD()));
 }
 
 TEST_CASE("cpu::simd_alignment", "[cpu]")

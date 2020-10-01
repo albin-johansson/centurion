8b3cfbfe0a356c04f437e0dc6179ff9fd2cd2384
Albin Johansson <albinoscar99@hotmail.com>
2020-10-02 00:16:03 +0200

    Update cursor.hpp

--- include/cursor.hpp
+++ include/cursor.hpp
@@ -147,10 +147,10 @@ enum class system_cursor {
 }
 
 template <typename T>
-using is_owning = std::enable_if_t<std::is_same_v<T, std::true_type>>;
+using is_cursor_owning = std::enable_if_t<std::is_same_v<T, std::true_type>>;
 
 template <typename T>
-using is_handle = std::enable_if_t<std::is_same_v<T, std::false_type>>;
+using is_cursor_handle = std::enable_if_t<std::is_same_v<T, std::false_type>>;
 
 /**
  * @class basic_cursor
@@ -203,7 +203,7 @@ class basic_cursor final
    *
    * @since 4.0.0
    */
-  template <typename U = T, typename = is_owning<U>>
+  template <typename U = T, typename = is_cursor_owning<U>>
   explicit basic_cursor(system_cursor cursor)
       : m_cursor{SDL_CreateSystemCursor(static_cast<SDL_SystemCursor>(cursor))}
   {
@@ -225,7 +225,7 @@ class basic_cursor final
    *
    * @since 4.0.0
    */
-  template <typename U = T, typename = is_owning<U>>
+  template <typename U = T, typename = is_cursor_owning<U>>
   basic_cursor(const surface& surface, const ipoint& hotspot)
       : m_cursor{SDL_CreateColorCursor(surface.get(), hotspot.x(), hotspot.y())}
   {
@@ -247,7 +247,7 @@ class basic_cursor final
    *
    * @since 5.0.0
    */
-  template <typename U = T, typename = is_handle<U>>
+  template <typename U = T, typename = is_cursor_handle<U>>
   explicit basic_cursor(SDL_Cursor* cursor) noexcept : m_cursor{cursor}
   {}
 
@@ -260,7 +260,7 @@ class basic_cursor final
    *
    * @since 5.0.0
    */
-  template <typename U = T, typename = is_handle<U>>
+  template <typename U = T, typename = is_cursor_handle<U>>
   explicit basic_cursor(const basic_cursor<std::true_type>& cursor) noexcept
       : m_cursor{cursor.get()}
   {}
@@ -369,7 +369,7 @@ class basic_cursor final
    *
    * @since 5.0.0
    */
-  template <typename U = T, typename = is_handle<U>>
+  template <typename U = T, typename = is_cursor_handle<U>>
   explicit operator bool() const noexcept
   {
     return m_cursor != nullptr;

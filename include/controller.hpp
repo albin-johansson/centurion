c5ac1c5b3c6abb2079e4e1fe219c902a40a4a047
Albin Johansson <albinoscar99@hotmail.com>
2020-10-01 21:30:03 +0200

    Update controller enums

--- include/controller.hpp
+++ include/controller.hpp
@@ -72,7 +72,7 @@ namespace cen {
  *
  * @headerfile controller.hpp
  */
-enum class controller_type {
+enum class controller_type : std::underlying_type_t<SDL_GameControllerType> {
   unknown = SDL_CONTROLLER_TYPE_UNKNOWN,   ///< An unknown controller.
   xbox_360 = SDL_CONTROLLER_TYPE_XBOX360,  ///< An Xbox 360 controller.
   xbox_one = SDL_CONTROLLER_TYPE_XBOXONE,  ///< An Xbox One controller.
@@ -94,7 +94,7 @@ enum class controller_type {
  *
  * @headerfile controller.hpp
  */
-enum class controller_axis {
+enum class controller_axis : std::underlying_type_t<SDL_GameControllerAxis> {
   invalid = SDL_CONTROLLER_AXIS_INVALID,
   left_x = SDL_CONTROLLER_AXIS_LEFTX,
   left_y = SDL_CONTROLLER_AXIS_LEFTY,
@@ -114,7 +114,8 @@ enum class controller_axis {
  *
  * @headerfile controller.hpp
  */
-enum class controller_button {
+enum class controller_button : std::underlying_type_t<
+    SDL_GameControllerButton> {
   invalid = SDL_CONTROLLER_BUTTON_INVALID,
   a = SDL_CONTROLLER_BUTTON_A,
   b = SDL_CONTROLLER_BUTTON_B,
@@ -143,7 +144,8 @@ enum class controller_button {
  *
  * @headerfile controller.hpp
  */
-enum class controller_bind_type {
+enum class controller_bind_type : std::underlying_type_t<
+    SDL_GameControllerBindType> {
   none = SDL_CONTROLLER_BINDTYPE_NONE,
   button = SDL_CONTROLLER_BINDTYPE_BUTTON,
   axis = SDL_CONTROLLER_BINDTYPE_AXIS,

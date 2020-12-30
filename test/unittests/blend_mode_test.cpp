41a35b51db2072fd1d32296da7339638a37765f2
Albin Johansson <albinoscar99@hotmail.com>
2020-12-30 18:18:48 +0100

    Reintroduce blend mode tests

--- test/unittests/blend_mode_test.cpp
+++ test/unittests/blend_mode_test.cpp
@@ -1,29 +1,29 @@
 #include "blend_mode.hpp"
 
-#include <catch.hpp>
+#include <gtest/gtest.h>
 
-TEST_CASE("blend_mode enum values", "[blend_mode]")
+TEST(BlendMode, EqualityOperator)
 {
-  CHECK(cen::blend_mode::none == SDL_BLENDMODE_NONE);
-  CHECK(cen::blend_mode::blend == SDL_BLENDMODE_BLEND);
-  CHECK(cen::blend_mode::add == SDL_BLENDMODE_ADD);
-  CHECK(cen::blend_mode::mod == SDL_BLENDMODE_MOD);
-  CHECK(cen::blend_mode::mul == SDL_BLENDMODE_MUL);
-  CHECK(cen::blend_mode::invalid == SDL_BLENDMODE_INVALID);
+  EXPECT_EQ(cen::blend_mode::none, SDL_BLENDMODE_NONE);
+  EXPECT_EQ(cen::blend_mode::blend, SDL_BLENDMODE_BLEND);
+  EXPECT_EQ(cen::blend_mode::add, SDL_BLENDMODE_ADD);
+  EXPECT_EQ(cen::blend_mode::mod, SDL_BLENDMODE_MOD);
+  EXPECT_EQ(cen::blend_mode::mul, SDL_BLENDMODE_MUL);
+  EXPECT_EQ(cen::blend_mode::invalid, SDL_BLENDMODE_INVALID);
 
-  CHECK(SDL_BLENDMODE_NONE == cen::blend_mode::none);
-  CHECK(SDL_BLENDMODE_BLEND == cen::blend_mode::blend);
-  CHECK(SDL_BLENDMODE_ADD == cen::blend_mode::add);
-  CHECK(SDL_BLENDMODE_MOD == cen::blend_mode::mod);
-  CHECK(SDL_BLENDMODE_MUL == cen::blend_mode::mul);
-  CHECK(SDL_BLENDMODE_INVALID == cen::blend_mode::invalid);
+  EXPECT_EQ(SDL_BLENDMODE_NONE, cen::blend_mode::none);
+  EXPECT_EQ(SDL_BLENDMODE_BLEND, cen::blend_mode::blend);
+  EXPECT_EQ(SDL_BLENDMODE_ADD, cen::blend_mode::add);
+  EXPECT_EQ(SDL_BLENDMODE_MOD, cen::blend_mode::mod);
+  EXPECT_EQ(SDL_BLENDMODE_MUL, cen::blend_mode::mul);
+  EXPECT_EQ(SDL_BLENDMODE_INVALID, cen::blend_mode::invalid);
 }
 
-TEST_CASE("blend_mode operator!=", "[blend_mode]")
+TEST(BlendMode, InequalityOperator)
 {
-  CHECK(cen::blend_mode::none != SDL_BLENDMODE_MOD);
-  CHECK(cen::blend_mode::blend != SDL_BLENDMODE_INVALID);
+  EXPECT_NE(cen::blend_mode::none, SDL_BLENDMODE_MOD);
+  EXPECT_NE(cen::blend_mode::blend, SDL_BLENDMODE_INVALID);
 
-  CHECK(SDL_BLENDMODE_ADD != cen::blend_mode::blend);
-  CHECK(SDL_BLENDMODE_MOD != cen::blend_mode::add);
+  EXPECT_NE(SDL_BLENDMODE_ADD, cen::blend_mode::blend);
+  EXPECT_NE(SDL_BLENDMODE_MOD, cen::blend_mode::add);
 }

700ca28603b6f7e4404e13fb7fb717930ada5c66
Albin Johansson <albinoscar99@hotmail.com>
2020-12-30 18:15:08 +0100

    Reintroduce bool conversion tests

--- test/unittests/bool_converter_test.cpp
+++ test/unittests/bool_converter_test.cpp
@@ -1,9 +1,9 @@
-#include <catch.hpp>
+#include <gtest/gtest.h>
 
 #include "detail/utils.hpp"
 
-TEST_CASE("Convert C++ bool to SDL bool", "[utilities]")
+TEST(ConvertBool, Conversion)
 {
-  CHECK(cen::detail::convert_bool(true) == SDL_TRUE);
-  CHECK(cen::detail::convert_bool(false) == SDL_FALSE);
+  EXPECT_EQ(cen::detail::convert_bool(true), SDL_TRUE);
+  EXPECT_EQ(cen::detail::convert_bool(false), SDL_FALSE);
 }

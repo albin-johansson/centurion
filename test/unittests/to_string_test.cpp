f9998535251a141f080bed704ebe3854a3e0f34e
Albin Johansson <albinoscar99@hotmail.com>
2020-12-30 13:17:18 +0100

    Begin switch to Google test

--- test/unittests/to_string_test.cpp
+++ test/unittests/to_string_test.cpp
@@ -1,59 +1,55 @@
 #include "detail/to_string.hpp"
 
-#include <array>
-#include <catch.hpp>
-#include <charconv>
+#include <gtest/gtest.h>
+
 #include <cstdint>  // int32_t, int64_t, uint32_t, uint64_t
 #include <limits>   // numeric_limits
-#include <string>
+#include <string>   // to_string
 
-TEST_CASE("to_string", "[to_string]")
+TEST(ToString, ToStringFloat)
 {
-  SECTION("float")
-  {
-    constexpr auto min = std::numeric_limits<float>::min();
-    constexpr auto max = std::numeric_limits<float>::max();
-    CHECK(cen::detail::to_string(min).has_value());
-    CHECK(cen::detail::to_string(max).has_value());
-  }
+  constexpr auto min = std::numeric_limits<float>::min();
+  constexpr auto max = std::numeric_limits<float>::max();
+  EXPECT_TRUE(cen::detail::to_string(min).has_value());
+  EXPECT_TRUE(cen::detail::to_string(max).has_value());
+}
 
-  SECTION("double")
-  {
-    constexpr auto min = std::numeric_limits<double>::min();
-    constexpr auto max = std::numeric_limits<double>::max();
-    CHECK(cen::detail::to_string<32>(min).has_value());
-    CHECK(cen::detail::to_string<32>(max).has_value());
-  }
+TEST(ToString, ToStringDouble)
+{
+  constexpr auto min = std::numeric_limits<double>::min();
+  constexpr auto max = std::numeric_limits<double>::max();
+  EXPECT_TRUE(cen::detail::to_string<32>(min).has_value());
+  EXPECT_TRUE(cen::detail::to_string<32>(max).has_value());
+}
 
-  SECTION("32-bit int")
-  {
-    constexpr auto min = std::numeric_limits<int32_t>::min();
-    constexpr auto max = std::numeric_limits<int32_t>::max();
-    CHECK(cen::detail::to_string(min) == std::to_string(min));
-    CHECK(cen::detail::to_string(max) == std::to_string(max));
-  }
+TEST(ToString, ToStringInt32)
+{
+  constexpr auto min = std::numeric_limits<int32_t>::min();
+  constexpr auto max = std::numeric_limits<int32_t>::max();
+  EXPECT_EQ(cen::detail::to_string(min), std::to_string(min));
+  EXPECT_EQ(cen::detail::to_string(max), std::to_string(max));
+}
 
-  SECTION("32-bit unsigned int")
-  {
-    constexpr auto min = std::numeric_limits<uint32_t>::min();
-    constexpr auto max = std::numeric_limits<uint32_t>::max();
-    CHECK(cen::detail::to_string(min) == std::to_string(min));
-    CHECK(cen::detail::to_string(max) == std::to_string(max));
-  }
+TEST(ToString, ToStringUnsigned32)
+{
+  constexpr auto min = std::numeric_limits<uint32_t>::min();
+  constexpr auto max = std::numeric_limits<uint32_t>::max();
+  EXPECT_EQ(cen::detail::to_string(min), std::to_string(min));
+  EXPECT_EQ(cen::detail::to_string(max), std::to_string(max));
+}
 
-  SECTION("64-bit int")
-  {
-    constexpr auto min = std::numeric_limits<int64_t>::min();
-    constexpr auto max = std::numeric_limits<int64_t>::max();
-    CHECK(cen::detail::to_string<32>(min) == std::to_string(min));
-    CHECK(cen::detail::to_string<32>(max) == std::to_string(max));
-  }
+TEST(ToString, ToStringInt64)
+{
+  constexpr auto min = std::numeric_limits<int64_t>::min();
+  constexpr auto max = std::numeric_limits<int64_t>::max();
+  EXPECT_EQ(cen::detail::to_string<32>(min), std::to_string(min));
+  EXPECT_EQ(cen::detail::to_string<32>(max), std::to_string(max));
+}
 
-  SECTION("64-bit unsigned int")
-  {
-    constexpr auto min = std::numeric_limits<uint64_t>::min();
-    constexpr auto max = std::numeric_limits<uint64_t>::max();
-    CHECK(cen::detail::to_string<32>(min) == std::to_string(min));
-    CHECK(cen::detail::to_string<32>(max) == std::to_string(max));
-  }
+TEST(ToString, ToStringUnsigned64)
+{
+  constexpr auto min = std::numeric_limits<uint64_t>::min();
+  constexpr auto max = std::numeric_limits<uint64_t>::max();
+  EXPECT_EQ(cen::detail::to_string<32>(min), std::to_string(min));
+  EXPECT_EQ(cen::detail::to_string<32>(max), std::to_string(max));
 }

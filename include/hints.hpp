ca545c05bebd13c3a1b50d18f0fa6127c0b26a4e
Albin Johansson <albinoscar99@hotmail.com>
2020-12-31 19:27:17 +0100

    Update license comments

--- include/hints.hpp
+++ include/hints.hpp
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2019-2020 Albin Johansson
+ * Copyright (c) 2019-2021 Albin Johansson
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -46,7 +46,7 @@
  *
  * \author Albin Johansson
  *
- * \date 2019-2020
+ * \date 2019-2021
  *
  * \copyright MIT License
  */

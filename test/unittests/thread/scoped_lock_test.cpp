96745d8b026948d43fb91952df9898e2be9b7ca4
Albin Johansson <albinoscar99@hotmail.com>
2020-12-31 23:33:30 +0100

    Inline scoped_lock class

--- test/unittests/thread/scoped_lock_test.cpp
+++ test/unittests/thread/scoped_lock_test.cpp
@@ -2,6 +2,11 @@
 
 #include <gtest/gtest.h>
 
+#include <type_traits>
+
+static_assert(!std::is_copy_constructible_v<cen::scoped_lock>);
+static_assert(!std::is_copy_assignable_v<cen::scoped_lock>);
+
 TEST(ScopedLock, Construction)
 {
   cen::mutex mutex;

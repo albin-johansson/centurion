510e3a5530b03fb1bf6439cd02c9dbe9ea3e0633
Albin Johansson <albinoscar99@hotmail.com>
2020-12-31 23:37:12 +0100

    Reintroduce counter tests

--- test/unittests/system/counter_test.cpp
+++ test/unittests/system/counter_test.cpp
@@ -1,23 +1,23 @@
 #include "counter.hpp"
 
-#include <catch.hpp>
+#include <gtest/gtest.h>
 
-TEST_CASE("counter::high_res_freq", "[counter]")
+TEST(Counter, HighResFreq)
 {
-  CHECK(cen::counter::high_res_freq() == SDL_GetPerformanceFrequency());
+  EXPECT_EQ(SDL_GetPerformanceFrequency(), cen::counter::high_res_freq());
 }
 
-TEST_CASE("counter::now", "[counter]")
+TEST(Counter, Now)
 {
-  CHECK_NOTHROW(cen::counter::now());
+  EXPECT_NO_THROW(cen::counter::now());
 }
 
-TEST_CASE("counter::now_sec", "[counter]")
+TEST(Counter, NowSec)
 {
-  CHECK_NOTHROW(cen::counter::now_sec<double>());
+  EXPECT_NO_THROW(cen::counter::now_sec<double>());
 }
 
-TEST_CASE("counter::ticks", "[counter]")
+TEST(Counter, Ticks)
 {
-  CHECK_NOTHROW(cen::counter::ticks());
+  EXPECT_NO_THROW(cen::counter::ticks());
 }

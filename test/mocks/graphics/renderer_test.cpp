9462e2d0f51e1662a461326cd41972c340b11678
Albin Johansson <albinoscar99@hotmail.com>
2021-01-01 16:39:12 +0100

    Update renderer_test.cpp

--- test/mocks/graphics/renderer_test.cpp
+++ test/mocks/graphics/renderer_test.cpp
@@ -9,6 +9,8 @@ FAKE_VALUE_FUNC(int, SDL_RenderClear, SDL_Renderer*)
 FAKE_VALUE_FUNC(int, SDL_SetRenderDrawColor, SDL_Renderer*, Uint8, Uint8, Uint8, Uint8)
 FAKE_VALUE_FUNC(int, SDL_GetRenderDrawColor, SDL_Renderer*, Uint8*, Uint8*, Uint8*, Uint8*)
 FAKE_VOID_FUNC(SDL_RenderPresent, SDL_Renderer*)
+FAKE_VALUE_FUNC(int, SDL_RenderDrawRect, SDL_Renderer*, const SDL_Rect*)
+FAKE_VALUE_FUNC(int, SDL_RenderDrawRectF, SDL_Renderer*, const SDL_FRect*)
 }
 // clang-format on
 
@@ -21,6 +23,8 @@ class RendererTest : public testing::Test
     RESET_FAKE(SDL_SetRenderDrawColor);
     RESET_FAKE(SDL_GetRenderDrawColor);
     RESET_FAKE(SDL_RenderPresent);
+    RESET_FAKE(SDL_RenderDrawRect);
+    RESET_FAKE(SDL_RenderDrawRectF);
   }
 
   cen::renderer_handle m_renderer{nullptr};
@@ -50,4 +54,17 @@ TEST_F(RendererTest, Present)
 {
   m_renderer.present();
   EXPECT_EQ(1, SDL_RenderPresent_fake.call_count);
+}
+
+TEST_F(RendererTest, DrawRect)
+{
+  constexpr cen::irect irect;
+  m_renderer.draw_rect(irect);
+  EXPECT_EQ(1, SDL_RenderDrawRect_fake.call_count);
+  EXPECT_EQ(0, SDL_RenderDrawRectF_fake.call_count);
+
+  constexpr cen::frect frect;
+  m_renderer.draw_rect(frect);
+  EXPECT_EQ(1, SDL_RenderDrawRect_fake.call_count);
+  EXPECT_EQ(1, SDL_RenderDrawRectF_fake.call_count);
 }

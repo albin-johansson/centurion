ea2854fa008f01408f8b4a5151c0e70850417cf2
Albin Johansson <albinoscar99@hotmail.com>
2021-01-01 16:33:43 +0100

    Update renderer_test.cpp

--- test/mocks/graphics/renderer_test.cpp
+++ test/mocks/graphics/renderer_test.cpp
@@ -6,6 +6,8 @@
 // clang-format off
 extern "C" {
 FAKE_VALUE_FUNC(int, SDL_RenderClear, SDL_Renderer*)
+FAKE_VALUE_FUNC(int, SDL_SetRenderDrawColor, SDL_Renderer*, Uint8, Uint8, Uint8, Uint8)
+FAKE_VALUE_FUNC(int, SDL_GetRenderDrawColor, SDL_Renderer*, Uint8*, Uint8*, Uint8*, Uint8*)
 FAKE_VOID_FUNC(SDL_RenderPresent, SDL_Renderer*)
 }
 // clang-format on
@@ -16,6 +18,8 @@ class RendererTest : public testing::Test
   void SetUp() override
   {
     RESET_FAKE(SDL_RenderClear);
+    RESET_FAKE(SDL_SetRenderDrawColor);
+    RESET_FAKE(SDL_GetRenderDrawColor);
     RESET_FAKE(SDL_RenderPresent);
   }
 
@@ -26,6 +30,20 @@ TEST_F(RendererTest, Clear)
 {
   m_renderer.clear();
   EXPECT_EQ(1, SDL_RenderClear_fake.call_count);
+}
+
+TEST_F(RendererTest, ClearWith)
+{
+  m_renderer.clear_with(cen::color{0xAA, 0xBB, 0xCC, 0xDD});
+
+  EXPECT_EQ(1, SDL_GetRenderDrawColor_fake.call_count);
+  EXPECT_EQ(2, SDL_SetRenderDrawColor_fake.call_count);
+  EXPECT_EQ(1, SDL_RenderClear_fake.call_count);
+
+  EXPECT_EQ(0xAA, SDL_SetRenderDrawColor_fake.arg1_history[0]);
+  EXPECT_EQ(0xBB, SDL_SetRenderDrawColor_fake.arg2_history[0]);
+  EXPECT_EQ(0xCC, SDL_SetRenderDrawColor_fake.arg3_history[0]);
+  EXPECT_EQ(0xDD, SDL_SetRenderDrawColor_fake.arg4_history[0]);
 }
 
 TEST_F(RendererTest, Present)

96193ee110c369bd7bd9e523c20e234375c019b4
Albin Johansson <albinoscar99@hotmail.com>
2021-01-01 01:12:39 +0100

    Reintroduce controller_button tests

--- test/unittests/input/controller_button_test.cpp
+++ test/unittests/input/controller_button_test.cpp
@@ -1,54 +1,52 @@
-#include <catch.hpp>
+#include <gtest/gtest.h>
 
 #include "controller.hpp"
 
-TEST_CASE("controller_button operator==", "[controller_button]")
+using button = cen::controller_button;
+
+TEST(ControllerButton, EqualityOperator)
 {
-  using button = cen::controller_button;
+  EXPECT_EQ(button::invalid, SDL_CONTROLLER_BUTTON_INVALID);
+  EXPECT_EQ(button::a, SDL_CONTROLLER_BUTTON_A);
+  EXPECT_EQ(button::b, SDL_CONTROLLER_BUTTON_B);
+  EXPECT_EQ(button::x, SDL_CONTROLLER_BUTTON_X);
+  EXPECT_EQ(button::y, SDL_CONTROLLER_BUTTON_Y);
+  EXPECT_EQ(button::back, SDL_CONTROLLER_BUTTON_BACK);
+  EXPECT_EQ(button::guide, SDL_CONTROLLER_BUTTON_GUIDE);
+  EXPECT_EQ(button::start, SDL_CONTROLLER_BUTTON_START);
+  EXPECT_EQ(button::left_stick, SDL_CONTROLLER_BUTTON_LEFTSTICK);
+  EXPECT_EQ(button::right_stick, SDL_CONTROLLER_BUTTON_RIGHTSTICK);
+  EXPECT_EQ(button::left_shoulder, SDL_CONTROLLER_BUTTON_LEFTSHOULDER);
+  EXPECT_EQ(button::right_shoulder, SDL_CONTROLLER_BUTTON_RIGHTSHOULDER);
+  EXPECT_EQ(button::dpad_up, SDL_CONTROLLER_BUTTON_DPAD_UP);
+  EXPECT_EQ(button::dpad_down, SDL_CONTROLLER_BUTTON_DPAD_DOWN);
+  EXPECT_EQ(button::dpad_left, SDL_CONTROLLER_BUTTON_DPAD_LEFT);
+  EXPECT_EQ(button::dpad_right, SDL_CONTROLLER_BUTTON_DPAD_RIGHT);
+  EXPECT_EQ(button::max, SDL_CONTROLLER_BUTTON_MAX);
 
-  CHECK(button::invalid == SDL_CONTROLLER_BUTTON_INVALID);
-  CHECK(button::a == SDL_CONTROLLER_BUTTON_A);
-  CHECK(button::b == SDL_CONTROLLER_BUTTON_B);
-  CHECK(button::x == SDL_CONTROLLER_BUTTON_X);
-  CHECK(button::y == SDL_CONTROLLER_BUTTON_Y);
-  CHECK(button::back == SDL_CONTROLLER_BUTTON_BACK);
-  CHECK(button::guide == SDL_CONTROLLER_BUTTON_GUIDE);
-  CHECK(button::start == SDL_CONTROLLER_BUTTON_START);
-  CHECK(button::left_stick == SDL_CONTROLLER_BUTTON_LEFTSTICK);
-  CHECK(button::right_stick == SDL_CONTROLLER_BUTTON_RIGHTSTICK);
-  CHECK(button::left_shoulder == SDL_CONTROLLER_BUTTON_LEFTSHOULDER);
-  CHECK(button::right_shoulder == SDL_CONTROLLER_BUTTON_RIGHTSHOULDER);
-  CHECK(button::dpad_up == SDL_CONTROLLER_BUTTON_DPAD_UP);
-  CHECK(button::dpad_down == SDL_CONTROLLER_BUTTON_DPAD_DOWN);
-  CHECK(button::dpad_left == SDL_CONTROLLER_BUTTON_DPAD_LEFT);
-  CHECK(button::dpad_right == SDL_CONTROLLER_BUTTON_DPAD_RIGHT);
-  CHECK(button::max == SDL_CONTROLLER_BUTTON_MAX);
-
-  CHECK(SDL_CONTROLLER_BUTTON_INVALID == button::invalid);
-  CHECK(SDL_CONTROLLER_BUTTON_A == button::a);
-  CHECK(SDL_CONTROLLER_BUTTON_B == button::b);
-  CHECK(SDL_CONTROLLER_BUTTON_X == button::x);
-  CHECK(SDL_CONTROLLER_BUTTON_Y == button::y);
-  CHECK(SDL_CONTROLLER_BUTTON_BACK == button::back);
-  CHECK(SDL_CONTROLLER_BUTTON_GUIDE == button::guide);
-  CHECK(SDL_CONTROLLER_BUTTON_START == button::start);
-  CHECK(SDL_CONTROLLER_BUTTON_LEFTSTICK == button::left_stick);
-  CHECK(SDL_CONTROLLER_BUTTON_RIGHTSTICK == button::right_stick);
-  CHECK(SDL_CONTROLLER_BUTTON_LEFTSHOULDER == button::left_shoulder);
-  CHECK(SDL_CONTROLLER_BUTTON_RIGHTSHOULDER == button::right_shoulder);
-  CHECK(SDL_CONTROLLER_BUTTON_DPAD_UP == button::dpad_up);
-  CHECK(SDL_CONTROLLER_BUTTON_DPAD_DOWN == button::dpad_down);
-  CHECK(SDL_CONTROLLER_BUTTON_DPAD_LEFT == button::dpad_left);
-  CHECK(SDL_CONTROLLER_BUTTON_DPAD_RIGHT == button::dpad_right);
-  CHECK(SDL_CONTROLLER_BUTTON_MAX == button::max);
+  EXPECT_EQ(SDL_CONTROLLER_BUTTON_INVALID, button::invalid);
+  EXPECT_EQ(SDL_CONTROLLER_BUTTON_A, button::a);
+  EXPECT_EQ(SDL_CONTROLLER_BUTTON_B, button::b);
+  EXPECT_EQ(SDL_CONTROLLER_BUTTON_X, button::x);
+  EXPECT_EQ(SDL_CONTROLLER_BUTTON_Y, button::y);
+  EXPECT_EQ(SDL_CONTROLLER_BUTTON_BACK, button::back);
+  EXPECT_EQ(SDL_CONTROLLER_BUTTON_GUIDE, button::guide);
+  EXPECT_EQ(SDL_CONTROLLER_BUTTON_START, button::start);
+  EXPECT_EQ(SDL_CONTROLLER_BUTTON_LEFTSTICK, button::left_stick);
+  EXPECT_EQ(SDL_CONTROLLER_BUTTON_RIGHTSTICK, button::right_stick);
+  EXPECT_EQ(SDL_CONTROLLER_BUTTON_LEFTSHOULDER, button::left_shoulder);
+  EXPECT_EQ(SDL_CONTROLLER_BUTTON_RIGHTSHOULDER, button::right_shoulder);
+  EXPECT_EQ(SDL_CONTROLLER_BUTTON_DPAD_UP, button::dpad_up);
+  EXPECT_EQ(SDL_CONTROLLER_BUTTON_DPAD_DOWN, button::dpad_down);
+  EXPECT_EQ(SDL_CONTROLLER_BUTTON_DPAD_LEFT, button::dpad_left);
+  EXPECT_EQ(SDL_CONTROLLER_BUTTON_DPAD_RIGHT, button::dpad_right);
+  EXPECT_EQ(SDL_CONTROLLER_BUTTON_MAX, button::max);
 }
 
-TEST_CASE("controller_button operator!=", "[controller_button]")
+TEST(ControllerButton, InequalityOperator)
 {
-  using button = cen::controller_button;
+  EXPECT_NE(button::dpad_right, SDL_CONTROLLER_BUTTON_LEFTSHOULDER);
+  EXPECT_NE(SDL_CONTROLLER_BUTTON_X, button::a);
 
-  CHECK(button::dpad_right != SDL_CONTROLLER_BUTTON_LEFTSHOULDER);
-  CHECK(SDL_CONTROLLER_BUTTON_X != button::a);
-
-  CHECK(!(SDL_CONTROLLER_BUTTON_INVALID != button::invalid));
+  EXPECT_FALSE(SDL_CONTROLLER_BUTTON_INVALID != button::invalid);
 }

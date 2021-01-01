5fdcc01ae7b5b6042635c92675ad4cb335b90fe9
Albin Johansson <albinoscar99@hotmail.com>
2021-01-01 02:35:03 +0100

    Update test_main.cpp

--- test/mocks/test_main.cpp
+++ test/mocks/test_main.cpp
@@ -1,7 +1,24 @@
+#include <SDL.h>
+#undef main  // SDL is unhappy without this (it wants to use its own main)
+
+#include <fff.h>
+
 #include <iostream>
+
+DEFINE_FFF_GLOBALS;
+
+FAKE_VOID_FUNC(hello)
+
+extern "C" {
+FAKE_VALUE_FUNC(SDL_Renderer*, SDL_CreateRenderer, SDL_Window*, int, Uint32)
+}
 
 int main(int argc, char* argv[])
 {
-  std::cout << "HelloWorld!\n";
+  SDL_CreateRenderer(nullptr, 0, 0);
+
+  std::cout << "Called SDL_CreateRenderer "
+            << SDL_CreateRenderer_fake.call_count << " times\n";
+
   return 0;
 }

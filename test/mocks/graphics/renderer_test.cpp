f1da8139af52639087d723262a909f5cc8c876be
Albin Johansson <albinoscar99@hotmail.com>
2021-01-01 16:27:43 +0100

    Add initial mocked renderer tests

---
+++ test/mocks/graphics/renderer_test.cpp
@@ -0,0 +1,34 @@
+#include "renderer.hpp"
+
+#include <fff.h>
+#include <gtest/gtest.h>
+
+// clang-format off
+extern "C" {
+FAKE_VALUE_FUNC(int, SDL_RenderClear, SDL_Renderer*)
+FAKE_VOID_FUNC(SDL_RenderPresent, SDL_Renderer*)
+}
+// clang-format on
+
+class RendererTest : public testing::Test
+{
+ protected:
+  void SetUp() override
+  {
+    RESET_FAKE(SDL_RenderClear);
+  }
+
+  cen::renderer_handle m_renderer{nullptr};
+};
+
+TEST_F(RendererTest, Clear)
+{
+  m_renderer.clear();
+  EXPECT_EQ(1, SDL_RenderClear_fake.call_count);
+}
+
+TEST_F(RendererTest, Present)
+{
+  m_renderer.present();
+  EXPECT_EQ(1, SDL_RenderPresent_fake.call_count);
+}

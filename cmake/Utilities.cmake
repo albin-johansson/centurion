99a30f263007f1c2639c0344a1d740e791f24466
Albin Johansson <albinoscar99@hotmail.com>
2021-01-01 02:14:41 +0100

    Tweak cmake scripts

--- cmake/Utilities.cmake
+++ cmake/Utilities.cmake
@@ -4,20 +4,20 @@
 #   includeDirectory: the path of the directory that contains the headers of the library.
 #   sources: the headers associated with the library.
 function(add_header_only_lib name includeDirectory sources)
-    add_library(${name} INTERFACE)
-    target_sources(${name} INTERFACE ${sources})
-    target_include_directories(${name} SYSTEM INTERFACE ${includeDirectory})
+  add_library(${name} INTERFACE)
+  target_sources(${name} INTERFACE ${sources})
+  target_include_directories(${name} SYSTEM INTERFACE ${includeDirectory})
 endfunction()
 
 # Checks if an environment variable is defined.
 #   envVar: the name of the actual environment variable.
 #   name: the name of the library associated with the environment variable.
 function(find_env_var envVar name)
-    if (DEFINED ENV{${envVar}})
-        message("${name} path: " $ENV{${envVar}})
-    else ()
-        message(WARNING "Couldn't find environment variable ${envVar}!")
-    endif ()
+  if (DEFINED ENV{${envVar}})
+    message("${name} path: " $ENV{${envVar}})
+  else ()
+    message(WARNING "Couldn't find environment variable ${envVar}!")
+  endif ()
 endfunction()
 
 # Copies a file.
@@ -25,12 +25,12 @@ endfunction()
 #   from: the file that will be copied.
 #   to: the target destination of the copied file.
 function(copy_file_post_build target from to)
-    add_custom_command(
-            TARGET ${target}
-            POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy
-            ${from}
-            ${to})
+  add_custom_command(
+      TARGET ${target}
+      POST_BUILD
+      COMMAND ${CMAKE_COMMAND} -E copy
+      ${from}
+      ${to})
 endfunction()
 
 # Copies a directory.
@@ -38,10 +38,28 @@ endfunction()
 #   from: the directory that will be copied.
 #   to: the target destination of the copied directory.
 function(copy_directory_post_build target from to)
-    add_custom_command(
-            TARGET ${target}
-            POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy_directory
-            ${from}
-            ${to})
+  add_custom_command(
+      TARGET ${target}
+      POST_BUILD
+      COMMAND ${CMAKE_COMMAND} -E copy_directory
+      ${from}
+      ${to})
+endfunction()
+
+function(cen_set_compiler_options target)
+  if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    target_compile_options(${target} PRIVATE
+        -Wno-unused-result)
+
+  elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    target_compile_options(${target} PRIVATE
+        /EHsc)
+
+  elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    target_compile_options(${target} PRIVATE
+        /EHsc
+        /MP
+        /W3)
+
+  endif ()
 endfunction()

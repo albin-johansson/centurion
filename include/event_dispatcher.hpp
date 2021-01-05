1cb588641883ed568e0634616d4638f887f9e8dd
Albin Johansson <albinoscar99@hotmail.com>
2021-01-05 01:26:31 +0100

    Tweak experimental event dispatcher

--- include/event_dispatcher.hpp
+++ include/event_dispatcher.hpp
@@ -5,48 +5,21 @@
 #include <functional>   // function
 #include <tuple>        // tuple
 #include <type_traits>  // is_same_v, is_invocable_v, is_reference_v, ...
-#include <utility>      // index_sequence_for, index_sequence
 
+#include "detail/tuple_type_index.hpp"
 #include "event.hpp"
 
 namespace cen::experimental {
 
-template <typename Target, typename Tuple>
-class index_for_type;
-
-template <typename Target, typename... T>
-class index_for_type<Target, std::tuple<T...>>
-{
-  template <std::size_t... index>
-  static constexpr int find(std::index_sequence<index...>)
-  {
-    return -1 + ((std::is_same_v<Target, T> ? index + 1 : 0) + ...);
-  }
-
- public:
-  inline static constexpr auto value = find(std::index_sequence_for<T...>{});
-};
-
-template <typename Target, typename... T>
-inline constexpr int index_for_type_v = index_for_type<Target, T...>::value;
-
 template <typename... E>
 class event_dispatcher final
 {
-  //  template <typename T, T... Index>
-  //  void visit(std::integer_sequence<T, Index...> sequence)
-  //  {
-  //    std::cout << "Visiting sequence: ";
-  //    ((std::cout << "{" << Index << ", " << typeid(T).name() << "}, "), ...);
-  //    std::cout << '\n';
-  //  }
-
   template <typename Event>
-  [[nodiscard]] constexpr static auto type_index()
+  [[nodiscard]] constexpr static auto index_of()
   {
     using func_t = std::function<void(const Event&)>;
 
-    constexpr auto index = index_for_type_v<func_t, functions_t>;
+    constexpr auto index = detail::tuple_type_index_v<func_t, function_tuple>;
     static_assert(index != -1);
 
     return index;
@@ -56,12 +29,7 @@ class event_dispatcher final
   auto check_for() -> bool
   {
     if (const auto* e = m_event.template try_get<Event>()) {
-      //      using func_t = std::function<void(const Event&)>;
-      //      using func_t = void(const Event&);
-      //      constexpr auto index = index_for_type<func_t, functions_t>::value;
-      //      static_assert(index != -1);
-
-      constexpr auto index = type_index<Event>();
+      constexpr auto index = index_of<Event>();
 
       if (auto func = std::get<index>(m_functions)) {
         func(*e);
@@ -112,18 +80,15 @@ class event_dispatcher final
     static_assert(std::is_invocable_v<T, const Event&>,
                   "Function object must be invocable with specified event!");
 
-    constexpr auto index = type_index<Event>();
-
-    auto& func = std::get<index>(m_functions);
-    func = std::function<void(const Event&)>(callable);
+    constexpr auto index = index_of<Event>();
+    std::get<index>(m_functions) = std::function<void(const Event&)>(callable);
   }
 
  private:
-//  inline constexpr static std::index_sequence_for<E...> m_indices;
-  using functions_t = std::tuple<std::function<void(const E&)>...>;
+  using function_tuple = std::tuple<std::function<void(const E&)>...>;
 
   cen::event m_event;
-  functions_t m_functions;
+  function_tuple m_functions;
 };
 
 }  // namespace cen::experimental

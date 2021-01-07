489df5b720ed621a1181e6917857a95864a773bd
Albin Johansson <albinoscar99@hotmail.com>
2021-01-07 23:59:55 +0100

    Update event_dispatcher.hpp

--- include/event_dispatcher.hpp
+++ include/event_dispatcher.hpp
@@ -1,4 +1,29 @@
-#pragma once
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019-2021 Albin Johansson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef CENTURION_EVENT_DISPATCHER_HEADER
+#define CENTURION_EVENT_DISPATCHER_HEADER
 
 #include <array>        // array
 #include <cstddef>      // size_t
@@ -8,9 +33,14 @@
 #include <tuple>        // tuple
 #include <type_traits>  // is_same_v, is_invocable_v, is_reference_v, ...
 
+#include "centurion_api.hpp"
 #include "detail/to_string.hpp"
 #include "detail/tuple_type_index.hpp"
 #include "event.hpp"
+
+#ifdef CENTURION_USE_PRAGMA_ONCE
+#pragma once
+#endif  // CENTURION_USE_PRAGMA_ONCE
 
 namespace cen {
 
@@ -347,3 +377,5 @@ inline auto operator<<(std::ostream& stream,
 /// \}
 
 }  // namespace cen
+
+#endif  // CENTURION_EVENT_DISPATCHER_HEADER

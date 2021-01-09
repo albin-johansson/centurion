1b392cb71c1afe2f66265072286e752261534a38
Albin Johansson <albinoscar99@hotmail.com>
2021-01-09 14:48:41 +0100

    Add mocked semaphore test

--- test/mocks/thread_mocks.cpp
+++ test/mocks/thread_mocks.cpp
@@ -6,6 +6,10 @@ DEFINE_FAKE_VOID_FUNC(SDL_DestroyMutex, SDL_mutex*)
 DEFINE_FAKE_VALUE_FUNC(SDL_mutex*, SDL_CreateMutex)
 DEFINE_FAKE_VALUE_FUNC(int, SDL_LockMutex, SDL_mutex*)
 DEFINE_FAKE_VALUE_FUNC(int, SDL_UnlockMutex, SDL_mutex*)
+
+// Semaphore
+DEFINE_FAKE_VOID_FUNC(SDL_DestroySemaphore, SDL_sem*)
+DEFINE_FAKE_VALUE_FUNC(SDL_sem*, SDL_CreateSemaphore, Uint32)
 }
 
 namespace mocks {
@@ -16,6 +20,9 @@ void reset_thread()
   RESET_FAKE(SDL_CreateMutex);
   RESET_FAKE(SDL_LockMutex);
   RESET_FAKE(SDL_UnlockMutex);
+
+  RESET_FAKE(SDL_DestroySemaphore);
+  RESET_FAKE(SDL_CreateSemaphore);
 }
 
 }  // namespace mocks
--- test/mocks/thread_mocks.hpp
+++ test/mocks/thread_mocks.hpp
@@ -9,6 +9,10 @@ DECLARE_FAKE_VOID_FUNC(SDL_DestroyMutex, SDL_mutex*)
 DECLARE_FAKE_VALUE_FUNC(SDL_mutex*, SDL_CreateMutex)
 DECLARE_FAKE_VALUE_FUNC(int, SDL_LockMutex, SDL_mutex*)
 DECLARE_FAKE_VALUE_FUNC(int, SDL_UnlockMutex, SDL_mutex*)
+
+// Semaphore
+DECLARE_FAKE_VOID_FUNC(SDL_DestroySemaphore, SDL_sem*)
+DECLARE_FAKE_VALUE_FUNC(SDL_sem*, SDL_CreateSemaphore, Uint32)
 }
 
 namespace mocks {

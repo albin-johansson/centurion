b31475c1e34c6769e9bf2ef8d3cdc0850d4b2fdf
Albin Johansson <albinoscar99@hotmail.com>
2021-01-09 16:22:35 +0100

    Remove thread mocks

--- test/mocks/thread_mocks.cpp
+++ test/mocks/thread_mocks.cpp
@@ -1,17 +1,6 @@
 #include "thread_mocks.hpp"
 
 extern "C" {
-// Thread
-DEFINE_FAKE_VALUE_FUNC(SDL_Thread*,
-                       SDL_CreateThread,
-                       SDL_ThreadFunction,
-                       const char*,
-                       void*,
-                       pfnSDL_CurrentBeginThread,
-                       pfnSDL_CurrentEndThread);
-DEFINE_FAKE_VOID_FUNC(SDL_DetachThread, SDL_Thread*)
-DEFINE_FAKE_VOID_FUNC(SDL_WaitThread, SDL_Thread*, int*)
-
 // Mutex
 DEFINE_FAKE_VOID_FUNC(SDL_DestroyMutex, SDL_mutex*)
 DEFINE_FAKE_VALUE_FUNC(SDL_mutex*, SDL_CreateMutex)
@@ -31,10 +20,6 @@ namespace mocks {
 
 void reset_thread()
 {
-  RESET_FAKE(SDL_CreateThread);
-  RESET_FAKE(SDL_DetachThread);
-  RESET_FAKE(SDL_WaitThread);
-
   RESET_FAKE(SDL_DestroyMutex);
   RESET_FAKE(SDL_CreateMutex);
   RESET_FAKE(SDL_LockMutex);
--- test/mocks/thread_mocks.hpp
+++ test/mocks/thread_mocks.hpp
@@ -5,18 +5,6 @@
 
 // clang-format off
 extern "C" {
-// Thread
-#undef SDL_CreateThread  // Don't do this at home
-DECLARE_FAKE_VALUE_FUNC(SDL_Thread*,
-                        SDL_CreateThread,
-                        SDL_ThreadFunction,
-                        const char*,
-                        void*,
-                        pfnSDL_CurrentBeginThread,
-                        pfnSDL_CurrentEndThread);
-DECLARE_FAKE_VOID_FUNC(SDL_DetachThread, SDL_Thread*)
-DECLARE_FAKE_VOID_FUNC(SDL_WaitThread, SDL_Thread*, int*)
-
 // Mutex
 DECLARE_FAKE_VOID_FUNC(SDL_DestroyMutex, SDL_mutex*)
 DECLARE_FAKE_VALUE_FUNC(SDL_mutex*, SDL_CreateMutex)

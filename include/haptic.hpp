9931db34b47a46a478a5e584e2c451e21f243b9d
Albin Johansson <albinoscar99@hotmail.com>
2021-01-20 00:34:27 +0100

    Update haptic.hpp

--- include/haptic.hpp
+++ include/haptic.hpp
@@ -67,6 +67,26 @@ using haptic = basic_haptic<std::true_type>;
  */
 using haptic_handle = basic_haptic<std::false_type>;
 
+enum class haptic_feature  // TODO verify that these are all of the "features"
+{
+  constant = SDL_HAPTIC_CONSTANT,
+  sine = SDL_HAPTIC_SINE,
+  left_right = SDL_HAPTIC_LEFTRIGHT,
+  triangle = SDL_HAPTIC_TRIANGLE,
+  sawtooth_up = SDL_HAPTIC_SAWTOOTHUP,
+  sawtooth_down = SDL_HAPTIC_SAWTOOTHDOWN,
+  ramp = SDL_HAPTIC_RAMP,
+  spring = SDL_HAPTIC_SPRING,
+  damper = SDL_HAPTIC_DAMPER,
+  inertia = SDL_HAPTIC_INERTIA,
+  friction = SDL_HAPTIC_FRICTION,
+  custom = SDL_HAPTIC_CUSTOM,
+  gain = SDL_HAPTIC_GAIN,
+  autocenter = SDL_HAPTIC_AUTOCENTER,
+  status = SDL_HAPTIC_STATUS,
+  pause = SDL_HAPTIC_PAUSE
+};
+
 /**
  * \class basic_haptic
  *

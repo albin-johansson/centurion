cb7882619001c4349d5f5637d382e6abf2bb7be1
Albin Johansson <albinoscar99@hotmail.com>
2021-03-10 23:39:43 +0100

    Update Utilities.cmake

--- cmake/Utilities.cmake
+++ cmake/Utilities.cmake
@@ -94,11 +94,11 @@ endfunction()
 #   target: the associated target.
 function(cen_include_centurion_headers target)
   target_include_directories(${target}
+      PUBLIC ${CEN_SOURCE_DIR}
       SYSTEM PUBLIC ${SDL2_INCLUDE_DIR}
       SYSTEM PUBLIC ${SDL2_IMAGE_INCLUDE_DIRS}
       SYSTEM PUBLIC ${SDL2_MIXER_INCLUDE_DIRS}
-      SYSTEM PUBLIC ${SDL2_TTF_INCLUDE_DIRS}
-      SYSTEM PUBLIC ${CEN_INCLUDE_DIR})
+      SYSTEM PUBLIC ${SDL2_TTF_INCLUDE_DIRS})
 endfunction()
 
 # Links against the SDL2 libraries.

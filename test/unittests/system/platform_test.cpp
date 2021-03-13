4d130c8857b618486bcf02f4e97dc41b2e050d67
Albin Johansson <albinoscar99@hotmail.com>
2021-03-13 16:36:09 +0100

    Update platform_test.cpp

--- test/unittests/system/platform_test.cpp
+++ test/unittests/system/platform_test.cpp
@@ -12,30 +12,46 @@ TEST(Platform, IsWindows)
   constexpr auto isWindows =
       cen::platform::ifdef_win32() || cen::platform::ifdef_win64();
   EXPECT_EQ(cen::platform::is_windows(), isWindows);
+
+  // Check that win64 -> win32
+  EXPECT_TRUE(!cen::platform::ifdef_win64() ||
+              (cen::platform::ifdef_win64() && cen::platform::ifdef_win32()));
 }
 
 TEST(Platform, IsMacOSX)
 {
   EXPECT_EQ(cen::platform::id() == cen::platform::platform_id::mac_osx,
             cen::platform::is_mac_osx());
+
+  constexpr auto isApple = cen::platform::ifdef_apple();
+  EXPECT_EQ(cen::platform::is_mac_osx(), isApple);
 }
 
 TEST(Platform, IsLinux)
 {
   EXPECT_EQ(cen::platform::id() == cen::platform::platform_id::linuxx,
             cen::platform::is_linux());
+
+  constexpr auto isLinux = cen::platform::ifdef_linux();
+  EXPECT_EQ(cen::platform::is_linux(), isLinux);
 }
 
 TEST(Platform, IsIOS)
 {
   EXPECT_EQ(cen::platform::id() == cen::platform::platform_id::ios,
             cen::platform::is_ios());
+
+  constexpr auto isApple = cen::platform::ifdef_apple();
+  EXPECT_EQ(cen::platform::is_ios(), isApple);
 }
 
 TEST(Platform, IsAndroid)
 {
   EXPECT_EQ(cen::platform::id() == cen::platform::platform_id::android,
             cen::platform::is_android());
+
+  constexpr auto isAndroid = cen::platform::ifdef_android();
+  EXPECT_EQ(cen::platform::is_android(), isAndroid);
 }
 
 TEST(Platform, Name)

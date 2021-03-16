f16eb3b6562966fb9b1370866e81d15d809dc1aa
Albin Johansson <albinoscar99@hotmail.com>
2021-03-16 19:52:33 +0100

    Update compiler_test.cpp

--- test/unittests/compiler/compiler_test.cpp
+++ test/unittests/compiler/compiler_test.cpp
@@ -19,3 +19,42 @@ TEST(Compiler, IsReleaseBuild)
   EXPECT_FALSE(cen::is_release_build());
 #endif
 }
+
+TEST(Compiler, OnMSVC)
+{
+#ifdef _MSC_VER
+  EXPECT_TRUE(cen::on_msvc());
+  EXPECT_FALSE(cen::on_clang());
+  EXPECT_FALSE(cen::on_gcc());
+  EXPECT_FALSE(cen::on_emscripten());
+  EXPECT_FALSE(cen::on_intel_cpp());
+#else
+  EXPECT_FALSE(cen::on_msvc());
+#endif
+}
+
+TEST(Compiler, OnClang)
+{
+#ifdef __clang__
+  EXPECT_TRUE(cen::on_clang());
+  EXPECT_FALSE(cen::on_msvc());
+  EXPECT_FALSE(cen::on_gcc());
+  EXPECT_FALSE(cen::on_emscripten());
+  EXPECT_FALSE(cen::on_intel_cpp());
+#else
+  EXPECT_FALSE(cen::on_clang());
+#endif
+}
+
+TEST(Compiler, OnGCC)
+{
+#ifdef __GNUC__
+  EXPECT_TRUE(cen::on_gcc());
+  EXPECT_FALSE(cen::on_clang());
+  EXPECT_FALSE(cen::on_msvc());
+  EXPECT_FALSE(cen::on_emscripten());
+  EXPECT_FALSE(cen::on_intel_cpp());
+#else
+  EXPECT_FALSE(cen::on_gcc());
+#endif
+}

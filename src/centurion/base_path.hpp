2359f355c256cb4f4ff4cf05e786c2702286d63e
Albin Johansson <albinoscar99@hotmail.com>
2021-03-17 21:16:20 +0100

    Remove base_path class

--- src/centurion/base_path.hpp
+++ src/centurion/base_path.hpp
@@ -3,9 +3,6 @@
 
 #include <SDL.h>
 
-#include <ostream>  // ostream
-#include <string>   // string
-
 #include "centurion_cfg.hpp"
 #include "sdl_string.hpp"
 
@@ -13,68 +10,6 @@ namespace cen {
 
 /// \addtogroup system
 /// \{
-
-/**
- * \class base_path
- *
- * \brief A wrapper for the application path obtained through `SDL_GetBasePath`.
- *
- * \details There is no guarantee that the application path is obtainable, so
- * you should always check for null when dealing with a base path instance.
- *
- * \see `SDL_GetBasePath`
- *
- * \since 3.0.0
- *
- * \deprecated Since 5.2.0, use ``get_base_path()`` instead.
- *
- * \headerfile base_path.hpp
- */
-class [[deprecated]] base_path final
-{
- public:
-  /**
-   * \brief Obtains the path of the application executable.
-   *
-   * \note This might be an expensive operation, so it's recommended to create
-   * only one instance of this class and cache it.
-   *
-   * \since 3.0.0
-   */
-  base_path() : m_path{SDL_GetBasePath()}
-  {}
-
-  /**
-   * \brief Indicates whether or not there is a non-null string in the base path
-   * instance.
-   *
-   * \return `true` if the internal string is non-null; `false` otherwise.
-   *
-   * \since 3.0.0
-   */
-  [[nodiscard]] explicit operator bool() const noexcept
-  {
-    return m_path.operator bool();
-  }
-
-  /**
-   * \brief Returns the path of the application executable.
-   *
-   * \note The returned pointer might be null! You should always check the
-   * returned pointer.
-   *
-   * \return the path of the application executable, might be `nullptr`.
-   *
-   * \since 3.0.0
-   */
-  [[nodiscard]] auto get() const noexcept -> czstring
-  {
-    return m_path.get();
-  }
-
- private:
-  sdl_string m_path;
-};
 
 /**
  * \brief Returns the "base" path associated with the application.
@@ -94,43 +29,6 @@ class [[deprecated]] base_path final
 [[nodiscard]] inline auto get_base_path() -> sdl_string
 {
   return sdl_string{SDL_GetBasePath()};
-}
-
-/**
- * \brief Returns a textual representation of a base path.
- *
- * \param path the base path that will be converted.
- *
- * \return a string that represents a base path.
- *
- * \deprecated Since 5.2.0.
- *
- * \since 5.0.0
- */
-[[nodiscard, deprecated]] inline auto to_string(const base_path& path)
-    -> std::string
-{
-  const std::string str = path ? path.get() : "N/A";
-  return "base_path{path: \"" + str + "\"}";
-}
-
-/**
- * \brief Prints a textual representation of a base path.
- *
- * \param stream the stream that will be used.
- * \param path the base path that will be printed.
- *
- * \return the used stream.
- *
- * \deprecated Since 5.2.0.
- *
- * \since 5.0.0
- */
-[[deprecated]] inline auto operator<<(std::ostream& stream,
-                                      const base_path& path) -> std::ostream&
-{
-  stream << to_string(path);
-  return stream;
 }
 
 /// \} End of group system

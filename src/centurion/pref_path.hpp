5dbdda81946a11936cad76c05a205cf5d5f8d1bc
Albin Johansson <albinoscar99@hotmail.com>
2021-03-17 21:18:57 +0100

    Remove pref_path class

--- src/centurion/pref_path.hpp
+++ src/centurion/pref_path.hpp
@@ -4,8 +4,6 @@
 #include <SDL.h>
 
 #include <cassert>  // assert
-#include <ostream>  // ostream
-#include <string>   // string
 
 #include "centurion_cfg.hpp"
 #include "czstring.hpp"
@@ -34,6 +32,8 @@ namespace cen {
  * \param org the name of the organization, cannot be null.
  * \param app the name of the application, cannot be null.
  *
+ * \todo std::string overload
+ *
  * \return an absolute path to the preferred path for storing application files;
  * a null string is returned if something goes wrong.
  *
@@ -46,107 +46,6 @@ namespace cen {
   assert(org);
   assert(app);
   return sdl_string{SDL_GetPrefPath(org, app)};
-}
-
-/**
- * \class pref_path
- *
- * \brief A wrapper for the preferred path for storing application related
- * files.
- *
- * \details This class is used to obtain the "pref dir". Where users are
- * meant to write personal files (such as preferences and save games, etc) that
- * are specific to your application. This directory is unique per user, per
- * application.
- *
- * \since 3.0.0
- *
- * \deprecated Since 5.2.0, use `get_pref_path()` instead.
- *
- * \see `SDL_GetPrefPath`
- *
- * \headerfile pref_path.hpp
- */
-class [[deprecated]] pref_path final
-{
- public:
-  /**
-   * \brief Constructs a `pref_path` instance.
-   *
-   * \note Only use letters, numbers, and spaces in the supplied strings!
-   *
-   * \param org the name of your organization, cannot be null.
-   * \param app the name of your application, cannot be null.
-   *
-   * \since 3.0.0
-   */
-  pref_path(const not_null<czstring> org, const not_null<czstring> app)
-      : m_path{get_pref_path(org, app)}
-  {}
-
-  /**
-   * \brief Indicates whether or not the instance holds a non-null path.
-   *
-   * \return `true` if the object holds a non-null path; `false` otherwise.
-   *
-   * \since 3.0.0
-   */
-  [[nodiscard]] explicit operator bool() const noexcept
-  {
-    return m_path.operator bool();
-  }
-
-  /**
-   * \brief Returns a string that represents the preferred path.
-   *
-   * \return a string that represents the preferred path.
-   *
-   * \since 3.0.0
-   */
-  [[nodiscard]] auto get() const noexcept -> czstring
-  {
-    return m_path.get();
-  }
-
- private:
-  sdl_string m_path;
-};
-
-/**
- * \brief Returns a textual representation of a pref path.
- *
- * \param path the pref path that will be converted.
- *
- * \return a string that represents a pref path.
- *
- * \deprecated Since 5.2.0.
- *
- * \since 5.0.0
- */
-[[nodiscard, deprecated]] inline auto to_string(const pref_path& path)
-    -> std::string
-{
-  const std::string str = path ? path.get() : "N/A";
-  return "pref_path{path: \"" + str + "\"}";
-}
-
-/**
- * \brief Prints a textual representation of a pref path.
- *
- * \param stream the stream that will be used.
- * \param path the pref path that will be printed.
- *
- * \return the used stream.
- *
- * \deprecated Since 5.2.0.
- *
- * \since 5.0.0
- */
-[[deprecated]] inline auto operator<<(std::ostream& stream,
-                                      const pref_path& path) -> std::ostream&
-{
-  stream << to_string(path);
-  return stream;
 }
 
 /// \}

f52cf55e1dcef887e2f34c99081ad2b1f4f6128e
Albin Johansson <albinoscar99@hotmail.com>
2021-04-08 13:41:00 +0200

    Update interactive_music.cpp

--- test/interactive/audio/music/interactive_music.cpp
+++ test/interactive/audio/music/interactive_music.cpp
@@ -1,21 +1,23 @@
+#include <cstddef>  // size_t
 #include <utility>  // move
 
 #include "centurion.hpp"
 
 namespace {
 
-inline constexpr cen::iarea windowSize{800, 600};
+inline constexpr std::size_t font_id = 7;
+inline constexpr cen::iarea window_size{800, 600};
 
-inline constexpr auto msgZero = "\"0\" to play the click one time.";
-inline constexpr auto msgOne = "\"1\" to play the click one time.";
-inline constexpr auto msgTwo = "\"2\" to play the click two times.";
-inline constexpr auto msgNine = "\"9\" to play the click forever.";
-inline constexpr auto msgFadeIn = "\"F\" to fade in the music over 5 seconds.";
-inline constexpr auto msgEsc = "\"ESC\" to halt the music.";
+inline constexpr auto msg_zero = "\"0\" to play the click one time.";
+inline constexpr auto msg_one = "\"1\" to play the click one time.";
+inline constexpr auto msg_two = "\"2\" to play the click two times.";
+inline constexpr auto msg_nine = "\"9\" to play the click forever.";
+inline constexpr auto msg_f = "\"F\" to fade in the music over 5 seconds.";
+inline constexpr auto msg_escape = "\"ESC\" to halt the music.";
 
-inline constexpr auto msgPlayingMusic = "Music is playing!";
-inline constexpr auto msgFadingMusic = "Music is fading!";
-inline constexpr auto msgNoMusic = "No music is playing";
+inline constexpr auto msg_playing_music = "Music is playing!";
+inline constexpr auto msg_fading_music = "Music is fading!";
+inline constexpr auto msg_no_music = "No music is playing";
 
 struct messages final
 {
@@ -29,31 +31,34 @@ struct messages final
   cen::texture fadingMusic;
   cen::texture noMusic;
 
-  [[nodiscard]] static auto make(cen::renderer& renderer, cen::font& font)
-      -> messages
+  [[nodiscard]] static auto make(cen::renderer& renderer) -> messages
   {
-    renderer.set_color(cen::colors::white);
+    auto& font = renderer.get_font(font_id);
 
     renderer.set_color(cen::colors::green);
-    auto playing = renderer.render_blended_latin1(msgPlayingMusic, font);
+    auto playing = renderer.render_blended_latin1(msg_playing_music, font);
 
     renderer.set_color(cen::colors::magenta);
-    auto fading = renderer.render_blended_latin1(msgFadingMusic, font);
+    auto fading = renderer.render_blended_latin1(msg_fading_music, font);
 
     renderer.set_color(cen::colors::red);
-    auto paused = renderer.render_blended_latin1(msgNoMusic, font);
+    auto paused = renderer.render_blended_latin1(msg_no_music, font);
 
-    return messages{renderer.render_blended_latin1(msgZero, font),
-                    renderer.render_blended_latin1(msgOne, font),
-                    renderer.render_blended_latin1(msgTwo, font),
-                    renderer.render_blended_latin1(msgNine, font),
-                    renderer.render_blended_latin1(msgFadeIn, font),
-                    renderer.render_blended_latin1(msgEsc, font),
+    renderer.set_color(cen::colors::white);
+    return messages{renderer.render_blended_latin1(msg_zero, font),
+                    renderer.render_blended_latin1(msg_one, font),
+                    renderer.render_blended_latin1(msg_two, font),
+                    renderer.render_blended_latin1(msg_nine, font),
+                    renderer.render_blended_latin1(msg_f, font),
+                    renderer.render_blended_latin1(msg_escape, font),
                     std::move(playing),
                     std::move(fading),
                     std::move(paused)};
   }
 };
+
+using event_dispatcher =
+    cen::event_dispatcher<cen::quit_event, cen::keyboard_event>;
 
 class interactive_music final
 {
@@ -63,83 +68,85 @@ class interactive_music final
       , m_renderer{m_window}
       , m_song{"resources/hiddenPond.mp3"}
       , m_click{"resources/click.wav"}
-      , m_font{"resources/fira_code.ttf", 14}
   {
-    m_window.set_size(windowSize);
+    m_renderer.emplace_font(font_id, "resources/fira_code.ttf", 14);
+
     m_window.set_title("Interactive music test");
+    m_window.set_size(window_size);
+
+    // clang-format off
+    m_dispatcher.bind<cen::quit_event>().to<&interactive_music::on_quit_event>(this);
+    m_dispatcher.bind<cen::keyboard_event>().to<&interactive_music::on_keyboard_event>(this);
+    // clang-format on
   }
 
-  void run()
+  auto run() -> int
   {
-    const auto msgs = messages::make(m_renderer, m_font);
+    const auto messages = messages::make(m_renderer);
     m_window.show();
 
-    bool running = true;
-    while (running)
+    while (m_running)
     {
-      running = handle_input();
-      render(msgs);
+      m_dispatcher.poll();
+      render(messages);
     }
 
     m_window.hide();
+    return 0;
   }
 
  private:
   cen::window m_window;
   cen::renderer m_renderer;
+  event_dispatcher m_dispatcher;
   cen::music m_song;
   cen::music m_click;
-  cen::font m_font;
+  bool m_running{true};
 
-  [[nodiscard]] auto handle_input() -> bool
+  void on_quit_event(const cen::quit_event&)
   {
-    cen::event event;
-    while (event.poll())
-    {
-      if (event.is<cen::quit_event>())
-      {
-        return false;
-      }
+    m_running = false;
+  }
 
-      if (const auto* key = event.try_get<cen::keyboard_event>();
-          key && key->released())
+  void on_keyboard_event(const cen::keyboard_event& event)
+  {
+    if (event.released())
+    {
+      if (event.is_active(cen::scancodes::zero))
       {
-        if (key->is_active(cen::scancodes::zero))
-        {
-          m_click.play(0);
-        }
-        else if (key->is_active(cen::scancodes::one))
-        {
-          m_click.play(1);
-        }
-        else if (key->is_active(cen::scancodes::two))
-        {
-          m_click.play(2);
-        }
-        else if (key->is_active(cen::scancodes::nine))
-        {
-          m_click.play(cen::music::forever);
-        }
-        else if (key->is_active(cen::scancodes::f))
-        {
-          m_song.fade_in(cen::seconds<int>{5});
-        }
-        else if (key->is_active(cen::scancodes::escape))
-        {
-          cen::music::halt();
-        }
+        m_click.play(0);
+      }
+      else if (event.is_active(cen::scancodes::one))
+      {
+        m_click.play(1);
+      }
+      else if (event.is_active(cen::scancodes::two))
+      {
+        m_click.play(2);
+      }
+      else if (event.is_active(cen::scancodes::nine))
+      {
+        m_click.play(cen::music::forever);
+      }
+      else if (event.is_active(cen::scancodes::f))
+      {
+        m_song.fade_in(cen::seconds<int>{5});
+      }
+      else if (event.is_active(cen::scancodes::escape))
+      {
+        cen::music::halt();
       }
     }
-
-    return true;
   }
 
   void render(const messages& msgs)
   {
     auto nextPos = [y = 50](const cen::texture& texture) mutable {
-      const auto x = (windowSize.width - texture.width()) / 2;
+      const auto x = (window_size.width - texture.width()) / 2;
+
       const auto tmpY = y;
       y += 25;
+
       return cen::ipoint{x, tmpY};
     };
 
@@ -172,12 +179,9 @@ class interactive_music final
 
 }  // namespace
 
-int main(int, char**)
+auto main(int, char**) -> int
 {
-  const cen::library lib;
-
-  interactive_music im;
-  im.run();
-
-  return 0;
+  const cen::library centurion;
+  interactive_music demo;
+  return demo.run();
 }

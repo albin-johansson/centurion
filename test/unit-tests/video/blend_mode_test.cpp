f9c906c79fc6a1b05b3735ef829544c23f936f64
Albin Johansson <albinoscar99@hotmail.com>
2021-04-20 12:32:47 +0200

    Ensure blend mode tests works with different SDL versions

--- test/unit-tests/video/blend_mode_test.cpp
+++ test/unit-tests/video/blend_mode_test.cpp
@@ -8,15 +8,19 @@ TEST(BlendMode, EqualityOperator)
   EXPECT_EQ(cen::blend_mode::blend, SDL_BLENDMODE_BLEND);
   EXPECT_EQ(cen::blend_mode::add, SDL_BLENDMODE_ADD);
   EXPECT_EQ(cen::blend_mode::mod, SDL_BLENDMODE_MOD);
-  EXPECT_EQ(cen::blend_mode::mul, SDL_BLENDMODE_MUL);
   EXPECT_EQ(cen::blend_mode::invalid, SDL_BLENDMODE_INVALID);
 
   EXPECT_EQ(SDL_BLENDMODE_NONE, cen::blend_mode::none);
   EXPECT_EQ(SDL_BLENDMODE_BLEND, cen::blend_mode::blend);
   EXPECT_EQ(SDL_BLENDMODE_ADD, cen::blend_mode::add);
   EXPECT_EQ(SDL_BLENDMODE_MOD, cen::blend_mode::mod);
-  EXPECT_EQ(SDL_BLENDMODE_MUL, cen::blend_mode::mul);
   EXPECT_EQ(SDL_BLENDMODE_INVALID, cen::blend_mode::invalid);
+
+#if SDL_VERSION_ATLEAST(2, 0, 12)
+  EXPECT_EQ(cen::blend_mode::mul, SDL_BLENDMODE_MUL);
+
+  EXPECT_EQ(SDL_BLENDMODE_MUL, cen::blend_mode::mul);
+#endif  // SDL_VERSION_ATLEAST(2, 0, 12)
 }
 
 TEST(BlendMode, InequalityOperator)

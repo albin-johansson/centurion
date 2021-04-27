fba8e0a824fe00a8caf7196276a3e490b42403a5
Albin Johansson <albinoscar99@hotmail.com>
2021-04-27 21:39:32 +0200

    Improve Vulkan API

--- src/centurion/video/vulkan/vk_core.hpp
+++ src/centurion/video/vulkan/vk_core.hpp
@@ -6,10 +6,14 @@
 #include <SDL.h>
 #include <SDL_vulkan.h>
 
-#include <cassert>  // assert
-#include <memory>   // unique_ptr
+#include <cassert>   // assert
+#include <memory>    // unique_ptr
+#include <optional>  // optional
+#include <vector>    // vector
 
 #include "../../core/czstring.hpp"
+#include "../../core/integers.hpp"
+#include "../../core/result.hpp"
 #include "../window.hpp"
 
 /// \addtogroup video
@@ -24,31 +28,81 @@
  */
 namespace cen::vk {
 
-// TODO Centurion 6: document and test
-
+/**
+ * \brief Returns the address of the `vkGetInstanceProcAddr` function.
+ *
+ * \return the address of the `vkGetInstanceProcAddr` function.
+ *
+ * \since 6.0.0
+ */
 [[nodiscard]] inline auto get_instance_proc_addr() noexcept -> void*
 {
   return SDL_Vulkan_GetVkGetInstanceProcAddr();
 }
 
+/**
+ * \brief Creates a rendering surface for a Vulkan window.
+ *
+ * \pre `window` must be a Vulkan window.
+ *
+ * \tparam T the ownership semantics of the window.
+ *
+ * \param window the Vulkan window.
+ * \param instance the current Vulkan instance.
+ * \param[out] outSurface the handle that will receive the handle of the created surface.
+ *
+ * \return `success` if the surface was succesfully created; `failure` otherwise.
+ *
+ * \since 6.0.0
+ */
 template <typename T>
 auto create_surface(basic_window<T>& window,
                     VkInstance instance,
-                    VkSurfaceKHR* outSurface) noexcept -> bool
+                    VkSurfaceKHR* outSurface) noexcept -> result
 {
   assert(window.is_vulkan());
   return SDL_Vulkan_CreateSurface(window.get(), instance, outSurface) == SDL_TRUE;
 }
 
-template <typename T>
-auto get_extensions(basic_window<T>& window,
-                    unsigned* outCount,
-                    czstring* outNames) noexcept -> bool
+/**
+ * \brief Returns the extensions required to create a Vulkan surface.
+ *
+ * \return the required Vulkan extensions; `std::nullopt` if something goes wrong.
+ *
+ * \since 6.0.0
+ */
+inline auto required_extensions() -> std::optional<std::vector<czstring>>
 {
-  assert(window.is_vulkan());
-  return SDL_Vulkan_GetInstanceExtensions(window.get(), outCount, outNames) == SDL_TRUE;
+  uint count{};
+  if (!SDL_Vulkan_GetInstanceExtensions(nullptr, &count, nullptr))
+  {
+    return std::nullopt;
+  }
+
+  std::vector<czstring> names;
+  names.reserve(count);
+
+  if (!SDL_Vulkan_GetInstanceExtensions(nullptr, &count, names.data()))
+  {
+    return std::nullopt;
+  }
+
+  return names;
 }
 
+/**
+ * \brief Returns the size of the drawable surface associated with the window.
+ *
+ * \pre `window` must be a Vulkan window.
+ *
+ * \tparam T the ownership semantics of the window.
+ *
+ * \param window the Vulkan window that will be queried.
+ *
+ * \return the size of the window drawable.
+ *
+ * \since 6.0.0
+ */
 template <typename T>
 [[nodiscard]] auto drawable_size(const basic_window<T>& window) noexcept -> iarea
 {

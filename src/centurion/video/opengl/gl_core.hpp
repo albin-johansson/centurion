cebf5dc38e92dcec8f3640e3f2aa4b3ed748bb1d
Albin Johansson <albinoscar99@hotmail.com>
2021-04-27 19:54:15 +0200

    Document and test core OpenGL API

--- src/centurion/video/opengl/gl_core.hpp
+++ src/centurion/video/opengl/gl_core.hpp
@@ -12,6 +12,7 @@
 
 #include "../../core/czstring.hpp"
 #include "../../core/not_null.hpp"
+#include "../../core/result.hpp"
 #include "../../math/area.hpp"
 #include "../window.hpp"
 #include "gl_attribute.hpp"
@@ -19,6 +20,24 @@
 
 /// \addtogroup video
 /// \{
+
+namespace cen {
+
+/**
+ * \enum gl_swap_interval
+ *
+ * \brief Provides identifiers that represent different swap interval modes.
+ *
+ * \since 6.0.0
+ */
+enum class gl_swap_interval
+{
+  immediate = 0,       ///< Immediate updates.
+  synchronized = 1,    ///< Updates synchronized with vertical retrace (VSync).
+  late_immediate = -1  ///< Allow immediate late swaps, instead of waiting for retrace.
+};
+
+}  // namespace cen
 
 /**
  * \namespace cen::gl
@@ -72,18 +91,41 @@ template <typename T>
   return {width, height};
 }
 
-// TODO Centurion 6: document and test
-
+/**
+ * \brief Resets all OpenGL context attributes to their default values.
+ *
+ * \since 6.0.0
+ */
 inline void reset_attributes() noexcept
 {
   SDL_GL_ResetAttributes();
 }
 
-inline auto set(const attribute attr, const int value) noexcept -> bool
+/**
+ * \brief Sets the value of an OpenGL context attribute.
+ *
+ * \param attr the attribute that will be set.
+ * \param value the new value of the attribute.
+ *
+ * \return `success` if the attribute was successfully set; `failure` otherwise.
+ *
+ * \since 6.0.0
+ */
+inline auto set(const attribute attr, const int value) noexcept -> result
 {
   return SDL_GL_SetAttribute(static_cast<SDL_GLattr>(attr), value) == 0;
 }
 
+/**
+ * \brief Returns the current value of an OpenGL context attribute.
+ *
+ * \param attr the OpenGL context attribute to check.
+ *
+ * \return the value of the specified attribute; `std::nullopt` if the value could not be
+ * obtained.
+ *
+ * \since 6.0.0
+ */
 inline auto get(const attribute attr) noexcept -> std::optional<int>
 {
   int value{};
@@ -97,37 +139,77 @@ inline auto get(const attribute attr) noexcept -> std::optional<int>
   }
 }
 
-inline auto set_swap_interval(const int interval) noexcept -> bool
+/**
+ * \brief Sets the swap interval strategy that will be used.
+ *
+ * \param interval the swap interval that will be used.
+ *
+ * \return `success` if the swap interval successfully set; `failure` if it isn't
+ * supported.
+ *
+ * \since 6.0.0
+ */
+inline auto set_swap_interval(const gl_swap_interval interval) noexcept -> result
 {
-  return SDL_GL_SetSwapInterval(interval) == 0;
+  return SDL_GL_SetSwapInterval(static_cast<int>(interval)) == 0;
 }
 
-[[nodiscard]] inline auto swap_interval() noexcept -> int
+/**
+ * \brief Returns the swap interval used by the current OpenGL context.
+ *
+ * \note `gl_swap_interval::immediate` is returned if the swap interval couldn't be
+ * determined.
+ *
+ * \return the current swap interval.
+ *
+ * \since 6.0.0
+ */
+[[nodiscard]] inline auto swap_interval() noexcept -> gl_swap_interval
 {
-  return SDL_GL_GetSwapInterval();
+  return gl_swap_interval{SDL_GL_GetSwapInterval()};
 }
 
+/**
+ * \brief Returns a handle to the currently active OpenGL window.
+ *
+ * \return a potentially invalid handle to the current OpenGL window.
+ *
+ * \since 6.0.0
+ */
 [[nodiscard]] inline auto get_window() noexcept -> window_handle
 {
   return window_handle{SDL_GL_GetCurrentWindow()};
 }
 
+/**
+ * \brief Returns a handle to the currently active OpenGL context.
+ *
+ * \return a potentially invalid handle to the current OpenGL context.
+ *
+ * \since 6.0.0
+ */
 [[nodiscard]] inline auto get_context() noexcept -> context_handle
 {
   return context_handle{SDL_GL_GetCurrentContext()};
 }
 
-// clang-format off
-
-[[nodiscard]] inline auto is_extension_supported(const not_null<czstring> extension) noexcept
-    -> bool
+/**
+ * \brief Indicates whether or not the specified extension is supported.
+ *
+ * \param extension the extension that will be checked.
+ *
+ * \return `true` if the specified extension is supported; `false` otherwise.
+ *
+ * \since 6.0.0
+ */
+[[nodiscard]] inline auto is_extension_supported(
+    const not_null<czstring> extension) noexcept -> bool
 {
   assert(extension);
   return SDL_GL_ExtensionSupported(extension) == SDL_TRUE;
 }
 
-// clang-format on
-
+/// \copydoc is_extension_supported()
 [[nodiscard]] inline auto is_extension_supported(const std::string& extension) noexcept
     -> bool
 {

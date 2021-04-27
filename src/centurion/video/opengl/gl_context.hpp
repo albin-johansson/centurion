72a25fead97712f089f40fb618fcdb752153d941
Albin Johansson <albinoscar99@hotmail.com>
2021-04-27 20:14:10 +0200

    Document and test OpenGL context API

--- src/centurion/video/opengl/gl_context.hpp
+++ src/centurion/video/opengl/gl_context.hpp
@@ -6,30 +6,53 @@
 #include <SDL.h>
 #include <SDL_opengl.h>
 
-#include <memory>  // unique_ptr
+#include <cassert>  // assert
+#include <memory>   // unique_ptr
 
 #include "../../core/exception.hpp"
 #include "../../core/result.hpp"
 #include "../../detail/owner_handle_api.hpp"
 #include "../window.hpp"
 
-namespace cen::gl {
-
 /// \addtogroup video
 /// \{
 
-// TODO Centurion 6: document and test
+namespace cen::gl {
 
 template <typename T>
 class basic_context;
 
+///< An owning OpenGL context.
 using context = basic_context<detail::owning_type>;
+
+///< A non-owning OpenGL context.
 using context_handle = basic_context<detail::handle_type>;
 
+/**
+ * \class basic_context
+ *
+ * \brief Represents an OpenGL context.
+ *
+ * \tparam T `owning_type` for owning semantics; `handle_type` for non-owning semantics.
+ *
+ * \since 6.0.0
+ */
 template <typename T>
 class basic_context final
 {
  public:
+  /// \name Construction
+  /// \{
+
+  /**
+   * \brief Creates a context instance from an existing OpenGL context.
+   *
+   * \param context the existing OpenGL context.
+   *
+   * \throws cen_error if the context is owning and the supplied pointer is null.
+   *
+   * \since 6.0.0
+   */
   explicit basic_context(SDL_GLContext context) noexcept(!detail::is_owning<T>())
       : m_context{context}
   {
@@ -42,6 +65,18 @@ class basic_context final
     }
   }
 
+  /**
+   * \brief Creates an OpenGL context based on the supplied window.
+   *
+   * \tparam U the ownership semantics of the window.
+   *
+   * \param window the OpenGL window.
+   *
+   * \throws sdl_error if the context has owning semantics and the OpenGL context couldn't
+   * be initialized.
+   *
+   * \since 6.0.0
+   */
   template <typename U>
   explicit basic_context(basic_window<U>& window) noexcept(!detail::is_owning<T>())
       : m_context{SDL_GL_CreateContext(window.get())}
@@ -55,12 +90,35 @@ class basic_context final
     }
   }
 
+  /// \} End of construction
+
+  /**
+   * \brief Makes the context the current OpenGL context for an OpenGL window.
+   *
+   * \pre `window` must be an OpenGL window.
+   *
+   * \tparam U the ownership semantics of the window.
+   *
+   * \param window the OpenGL window.
+   *
+   * \return `success` if the was operation was successful; `failure` otherwise.
+   *
+   * \since 6.0.0
+   */
   template <typename U>
   auto make_current(basic_window<U>& window) -> result
   {
+    assert(window.is_opengl());
     return SDL_GL_MakeCurrent(window.get(), m_context.get()) == 0;
   }
 
+  /**
+   * \brief Returns the associated OpenGL context.
+   *
+   * \return the handle to the associated OpenGL context.
+   *
+   * \since 6.0.0
+   */
   [[nodiscard]] auto get() const noexcept -> SDL_GLContext
   {
     return m_context.get();
@@ -78,9 +136,9 @@ class basic_context final
   std::unique_ptr<void, deleter> m_context;
 };
 
-/// \} End of group video
-
 }  // namespace cen::gl
+
+/// \} End of group video
 
 #endif  // CENTURION_NO_OPENGL
 #endif  // CENTURION_GL_CONTEXT_HEADER

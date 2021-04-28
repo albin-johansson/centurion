93b524c518809b9d9161fb31d452e05f4166df73
Albin Johansson <albinoscar99@hotmail.com>
2021-04-28 23:29:21 +0200

    Update channels_test.cpp

--- test/mocks/audio/channels_test.cpp
+++ test/mocks/audio/channels_test.cpp
@@ -18,6 +18,10 @@ FAKE_VALUE_FUNC(int, Mix_AllocateChannels, int)
 FAKE_VALUE_FUNC(int, Mix_ReserveChannels, int)
 FAKE_VALUE_FUNC(int, Mix_ExpireChannel, int, int)
 FAKE_VALUE_FUNC(int, Mix_GroupChannel, int, int)
+FAKE_VALUE_FUNC(int, Mix_GroupCount, int)
+FAKE_VALUE_FUNC(int, Mix_GroupAvailable, int)
+FAKE_VALUE_FUNC(int, Mix_GroupNewer, int)
+FAKE_VALUE_FUNC(int, Mix_GroupOldest, int)
 }
 
 class ChannelsTest : public testing::Test
@@ -27,11 +31,15 @@ class ChannelsTest : public testing::Test
   {
     mocks::reset_core();
 
+    RESET_FAKE(Mix_ChannelFinished)
     RESET_FAKE(Mix_AllocateChannels)
     RESET_FAKE(Mix_ReserveChannels)
     RESET_FAKE(Mix_ExpireChannel)
     RESET_FAKE(Mix_GroupChannel)
-    RESET_FAKE(Mix_ChannelFinished)
+    RESET_FAKE(Mix_GroupCount)
+    RESET_FAKE(Mix_GroupAvailable)
+    RESET_FAKE(Mix_GroupNewer)
+    RESET_FAKE(Mix_GroupOldest)
   }
 };
 
@@ -95,4 +103,44 @@ TEST_F(ChannelsTest, ResetGroup)
   ASSERT_EQ(cen::success, cen::channels::reset_group(7));
   ASSERT_EQ(-1, Mix_GroupChannel_fake.arg1_val);
   ASSERT_EQ(2, Mix_GroupChannel_fake.call_count);
+}
+
+TEST_F(ChannelsTest, GroupCount)
+{
+  const auto all [[maybe_unused]] = cen::channels::group_count();
+  ASSERT_EQ(-1, Mix_GroupCount_fake.arg0_val);
+
+  const auto count [[maybe_unused]] = cen::channels::group_count(123);
+  ASSERT_EQ(123, Mix_GroupCount_fake.arg0_val);
+  ASSERT_EQ(2, Mix_GroupCount_fake.call_count);
+}
+
+TEST_F(ChannelsTest, FirstAvailable)
+{
+  std::array values{-1, 0};
+  SET_RETURN_SEQ(Mix_GroupAvailable, values.data(), cen::isize(values));
+
+  ASSERT_FALSE(cen::channels::first_available(42));
+  ASSERT_TRUE(cen::channels::first_available(42));
+  ASSERT_EQ(2, Mix_GroupAvailable_fake.call_count);
+}
+
+TEST_F(ChannelsTest, MostRecent)
+{
+  std::array values{-1, 0};
+  SET_RETURN_SEQ(Mix_GroupNewer, values.data(), cen::isize(values));
+
+  ASSERT_FALSE(cen::channels::most_recent(42));
+  ASSERT_TRUE(cen::channels::most_recent(42));
+  ASSERT_EQ(2, Mix_GroupNewer_fake.call_count);
+}
+
+TEST_F(ChannelsTest, Oldest)
+{
+  std::array values{-1, 0};
+  SET_RETURN_SEQ(Mix_GroupOldest, values.data(), cen::isize(values));
+
+  ASSERT_FALSE(cen::channels::oldest(42));
+  ASSERT_TRUE(cen::channels::oldest(42));
+  ASSERT_EQ(2, Mix_GroupOldest_fake.call_count);
 }

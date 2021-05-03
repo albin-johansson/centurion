fb06a3a56f86fa50913fa2e409a2ab18ec6b4c4d
Albin Johansson <albinoscar99@hotmail.com>
2021-05-03 19:21:33 +0200

    Update preferred_path.hpp

--- src/centurion/filesystem/preferred_path.hpp
+++ src/centurion/filesystem/preferred_path.hpp
@@ -18,18 +18,18 @@ namespace cen {
 /**
  * \brief Returns the preferred path for storing application related files.
  *
- * \details This function returns the path to the directory to which
- * applications are meant to write files such as preferences and save data, etc.
- * This directory will be unique per user and application. The returned path
- * will end with a path separator (e.g. "\\" or "/").
+ * \details This function returns the path to the directory to which applications are
+ * meant to write files such as preferences and save data, etc. This directory will be
+ * unique per user and application. The returned path will end with a path separator (e.g.
+ * "\\" or "/").
  *
  * \note Only use letters, numbers, and spaces in the supplied names!
  *
  * \param org the name of the organization, cannot be null.
  * \param app the name of the application, cannot be null.
  *
- * \return an absolute path to the preferred path for storing application files;
- * a null string is returned if something goes wrong.
+ * \return an absolute path to the preferred path for storing application files; a null
+ * string is returned if something goes wrong.
  *
  * \since 5.2.0
  */
@@ -48,18 +48,18 @@ namespace cen {
 /**
  * \brief Returns the preferred path for storing application related files.
  *
- * \details This function returns the path to the directory to which
- * applications are meant to write files such as preferences and save data, etc.
- * This directory will be unique per user and application. The returned path
- * will end with a path separator (e.g. "\\" or "/").
+ * \details This function returns the path to the directory to which applications are
+ * meant to write files such as preferences and save data, etc. This directory will be
+ * unique per user and application. The returned path will end with a path separator (e.g.
+ * "\\" or "/").
  *
  * \note Only use letters, numbers, and spaces in the supplied names!
  *
  * \param org the name of the organization.
  * \param app the name of the application.
  *
- * \return an absolute path to the preferred path for storing application files;
- * a null string is returned if something goes wrong.
+ * \return an absolute path to the preferred path for storing application files; a null
+ * string is returned if something goes wrong.
  *
  * \since 6.0.0
  */

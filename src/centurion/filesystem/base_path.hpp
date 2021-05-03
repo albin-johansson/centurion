da53c871968001398d091806aa3869692f46d16f
Albin Johansson <albinoscar99@hotmail.com>
2021-05-03 19:21:23 +0200

    Update base_path.hpp

--- src/centurion/filesystem/base_path.hpp
+++ src/centurion/filesystem/base_path.hpp
@@ -14,14 +14,13 @@ namespace cen {
  * \brief Returns the "base" path associated with the application.
  *
  * \details The returned string represents the path to the directory from which
- * application was run from, and will end with a path separator (e.g. "\\" or
- * "/").
+ * application was run from, and will end with a path separator (e.g. "\\" or "/").
  *
- * \note This might be an expensive operation, so it's recommended to only call
- * this function once and cache the returned path.
+ * \note This might be an expensive operation, so it's recommended to only call this
+ * function once and cache the returned path.
  *
- * \return the base path of the application; a null string is returned if the
- * path cannot be obtained.
+ * \return the base path of the application; a null string is returned if the path cannot
+ * be obtained.
  *
  * \since 5.2.0
  */

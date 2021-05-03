918ca84212d7dcc82160725c8d69e34e84242932
Albin Johansson <albinoscar99@hotmail.com>
2021-05-03 19:23:27 +0200

    Update texture_access.hpp

--- src/centurion/video/texture_access.hpp
+++ src/centurion/video/texture_access.hpp
@@ -13,23 +13,18 @@ namespace cen {
  *
  * \brief Mirrors the `SDL_TextureAccess` enum.
  *
- * \note The `no_lock` enumerator is also referred to as "static" texture
- * access.
+ * \note The `no_lock` enumerator is also referred to as "static" texture access.
  *
  * \since 3.0.0
  *
  * \see `SDL_TextureAccess`
- *
- * \headerfile texture_access.hpp
  */
 enum class texture_access : int
 {
-  no_lock = SDL_TEXTUREACCESS_STATIC,       ///< Indicates that the texture changes
-                                            ///< rarely, and isn't lockable.
-  streaming = SDL_TEXTUREACCESS_STREAMING,  ///< Indicates that the texture changes
-                                            ///< frequently, and is lockable.
-  target = SDL_TEXTUREACCESS_TARGET         ///< Indicates that the texture can be used
-                                            ///< as a render target.
+  no_lock = SDL_TEXTUREACCESS_STATIC,  ///< Texture changes rarely, and isn't lockable.
+  streaming =
+      SDL_TEXTUREACCESS_STREAMING,   ///< Texture changes frequently, and is lockable.
+  target = SDL_TEXTUREACCESS_TARGET  ///< Texture can be used as a render target.
 };
 
 /**
@@ -56,8 +51,7 @@ enum class texture_access : int
 }
 
 /**
- * \brief Indicates whether or not the two texture access values aren't the
- * same.
+ * \brief Indicates whether or not the two texture access values aren't the same.
  *
  * \param lhs the lhs texture access value.
  * \param rhs the rhs texture access value.

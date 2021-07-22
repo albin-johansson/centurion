530d91c667e31f36fc1e818aa175fe9f411fc17a
Albin Johansson <albinoscar99@hotmail.com>
2021-07-22 23:29:58 +0200

    Add to_string and operator<< overloads for message_box_type

--- src/centurion/video/message_box_type.hpp
+++ src/centurion/video/message_box_type.hpp
@@ -3,6 +3,10 @@
 
 #include <SDL.h>
 
+#include <ostream>  // ostream
+#include <string>   // string
+
+#include "../core/exception.hpp"
 #include "../core/integers.hpp"
 
 namespace cen {
@@ -25,6 +29,55 @@ enum class message_box_type : u32
   warning = SDL_MESSAGEBOX_WARNING,
   information = SDL_MESSAGEBOX_INFORMATION
 };
+
+/**
+ * \brief Returns a textual version of the supplied message box type.
+ *
+ * \details This function returns a string that mirrors the name of the enumerator, e.g.
+ * `to_string(message_box_type::error) == "error"`.
+ *
+ * \param type the enumerator that will be converted.
+ *
+ * \return a string that mirrors the name of the enumerator.
+ *
+ * \throws cen_error if the enumerator is not recognized.
+ *
+ * \since 6.2.0
+ */
+[[nodiscard]] inline auto to_string(const message_box_type type) -> std::string
+{
+  switch (type)
+  {
+    case message_box_type::error:
+      return "error";
+
+    case message_box_type::warning:
+      return "warning";
+
+    case message_box_type::information:
+      return "information";
+
+    default:
+      throw cen_error{"Did not recognize message box type!"};
+  }
+}
+
+/**
+ * \brief Prints a textual representation of a message box type enumerator.
+ *
+ * \param stream the output stream that will be used.
+ * \param type the enumerator that will be printed.
+ *
+ * \see `to_string(message_box_type)`
+ *
+ * \return the used stream.
+ *
+ * \since 6.2.0
+ */
+inline auto operator<<(std::ostream& stream, const message_box_type type) -> std::ostream&
+{
+  return stream << to_string(type);
+}
 
 /// \} End of group video
 
--- test/unit-tests/video/message_box_type_test.cpp
+++ test/unit-tests/video/message_box_type_test.cpp
@@ -2,6 +2,8 @@
 
 #include <gtest/gtest.h>
 
+#include <iostream>  // cout
+
 #include "core/to_underlying.hpp"
 
 TEST(MessageBoxType, Values)
@@ -10,4 +12,13 @@ TEST(MessageBoxType, Values)
             cen::to_underlying(cen::message_box_type::information));
   ASSERT_EQ(SDL_MESSAGEBOX_WARNING, cen::to_underlying(cen::message_box_type::warning));
   ASSERT_EQ(SDL_MESSAGEBOX_ERROR, cen::to_underlying(cen::message_box_type::error));
+}
+
+TEST(MessageBoxType, ToString)
+{
+  ASSERT_EQ("information", cen::to_string(cen::message_box_type::information));
+  ASSERT_EQ("error", cen::to_string(cen::message_box_type::error));
+  ASSERT_EQ("warning", cen::to_string(cen::message_box_type::warning));
+
+  std::cout << "Message box type example: " << cen::message_box_type::information << '\n';
 }

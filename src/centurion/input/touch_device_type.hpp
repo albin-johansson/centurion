fe68ee29646f9e5809edd0795346f1d7acd8e83a
Albin Johansson <albinoscar99@hotmail.com>
2021-07-22 22:26:25 +0200

    Add to_string and operator<< overloads for touch_device_type

--- src/centurion/input/touch_device_type.hpp
+++ src/centurion/input/touch_device_type.hpp
@@ -3,7 +3,13 @@
 
 #include <SDL.h>
 
+#include <ostream>  // ostream
+#include <string>   // string
+
+#include "../core/exception.hpp"
+
 namespace cen {
+
 namespace touch {
 
 /// \addtogroup input
@@ -85,8 +91,72 @@ enum class device_type
 
 }  // namespace touch
 
+/// \addtogroup input
+/// \{
+
 // Added for consistency with rest of codebase (no classes in nested namespaces)
 using touch_device_type = touch::device_type;
+
+[[nodiscard]] auto to_string(touch_device_type type) -> std::string;
+
+namespace touch {
+
+/**
+ * \brief Prints a textual representation of a touch device type enumerator.
+ *
+ * \param stream the output stream that will be used.
+ * \param type the enumerator that will be printed.
+ *
+ * \see `to_string(touch_device_type)`
+ *
+ * \return the used stream.
+ *
+ * \since 6.2.0
+ */
+inline auto operator<<(std::ostream& stream, const touch_device_type type)
+    -> std::ostream&
+{
+  return stream << to_string(type);
+}
+
+}  // namespace touch
+
+/**
+ * \brief Returns a textual version of the supplied touch device type.
+ *
+ * \details This function returns a string that mirrors the name of the enumerator, e.g.
+ * `to_string(device_type::direct) == "direct"`.
+ *
+ * \param type the enumerator that will be converted.
+ *
+ * \return a string that mirrors the name of the enumerator.
+ *
+ * \throws cen_error if the enumerator is not recognized.
+ *
+ * \since 6.2.0
+ */
+[[nodiscard]] inline auto to_string(const touch_device_type type) -> std::string
+{
+  switch (type)
+  {
+    case touch_device_type::invalid:
+      return "invalid";
+
+    case touch_device_type::direct:
+      return "direct";
+
+    case touch_device_type::indirect_absolute:
+      return "indirect_absolute";
+
+    case touch_device_type::indirect_relative:
+      return "indirect_relative";
+
+    default:
+      throw cen_error{"Did not recognize touch device type!"};
+  }
+}
+
+/// \} End of group input
 
 }  // namespace cen
 

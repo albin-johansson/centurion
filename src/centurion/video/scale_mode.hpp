7697ae0a511c29d4ca56a3e3f2c8010a0596840d
Albin Johansson <albinoscar99@hotmail.com>
2021-07-22 23:54:17 +0200

    Add to_string and operator<< overloads for scale_mode

--- src/centurion/video/scale_mode.hpp
+++ src/centurion/video/scale_mode.hpp
@@ -3,6 +3,11 @@
 
 #include <SDL.h>
 
+#include <ostream>  // ostream
+#include <string>   // string
+
+#include "../core/exception.hpp"
+
 namespace cen {
 
 /// \addtogroup video
@@ -25,6 +30,58 @@ enum class scale_mode
   linear = SDL_ScaleModeLinear,    ///< Represents linear filtering.
   best = SDL_ScaleModeBest         ///< Represents anisotropic filtering.
 };
+
+/**
+ * \brief Returns a textual version of the supplied scale mode.
+ *
+ * \details This function returns a string that mirrors the name of the enumerator, e.g.
+ * `to_string(scale_mode::linear) == "linear"`.
+ *
+ * \param mode the enumerator that will be converted.
+ *
+ * \return a string that mirrors the name of the enumerator.
+ *
+ * \throws cen_error if the enumerator is not recognized.
+ *
+ * \since 6.2.0
+ */
+[[nodiscard]] inline auto to_string(const scale_mode mode) -> std::string
+{
+  switch (mode)
+  {
+    case scale_mode::nearest:
+      return "nearest";
+
+    case scale_mode::linear:
+      return "linear";
+
+    case scale_mode::best:
+      return "best";
+
+    default:
+      throw cen_error{"Did not recognize scale mode!"};
+  }
+}
+
+/**
+ * \brief Prints a textual representation of a scale mode enumerator.
+ *
+ * \param stream the output stream that will be used.
+ * \param mode the enumerator that will be printed.
+ *
+ * \see `to_string(scale_mode)`
+ *
+ * \return the used stream.
+ *
+ * \since 6.2.0
+ */
+inline auto operator<<(std::ostream& stream, const scale_mode mode) -> std::ostream&
+{
+  return stream << to_string(mode);
+}
+
+/// \name Scale mode comparison operators
+/// \{
 
 /**
  * \brief Indicates whether or not the two scale mode values are the same.
@@ -72,9 +129,11 @@ enum class scale_mode
   return !(lhs == rhs);
 }
 
+/// \} End of scale mode comparison operators
+
 #endif  // SDL_VERSION_ATLEAST(2, 0, 12)
 
-/// \}
+/// \} End of group video
 
 }  // namespace cen
 
--- test/unit-tests/video/scale_mode_test.cpp
+++ test/unit-tests/video/scale_mode_test.cpp
@@ -2,6 +2,8 @@
 
 #include <gtest/gtest.h>
 
+#include <iostream>  // cout
+
 #if SDL_VERSION_ATLEAST(2, 0, 12)
 
 TEST(ScaleMode, EnumValues)
@@ -16,6 +18,15 @@ TEST(ScaleMode, EnumValues)
 
   ASSERT_NE(cen::scale_mode::linear, SDL_ScaleModeNearest);
   ASSERT_NE(SDL_ScaleModeBest, cen::scale_mode::nearest);
+}
+
+TEST(ScaleMode, ToString)
+{
+  ASSERT_EQ("nearest", cen::to_string(cen::scale_mode::nearest));
+  ASSERT_EQ("linear", cen::to_string(cen::scale_mode::linear));
+  ASSERT_EQ("best", cen::to_string(cen::scale_mode::best));
+
+  std::cout << "Scale mode example: " << cen::scale_mode::linear << '\n';
 }
 
 #endif  // SDL_VERSION_ATLEAST(2, 0, 12)

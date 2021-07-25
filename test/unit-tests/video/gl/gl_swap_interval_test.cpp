d0d89137e5094388d75a4bc4dc6add082a200a22
Albin Johansson <albinoscar99@hotmail.com>
2021-07-25 14:28:34 +0200

    Update gl_swap_interval_test.cpp

--- test/unit-tests/video/gl/gl_swap_interval_test.cpp
+++ test/unit-tests/video/gl/gl_swap_interval_test.cpp
@@ -14,6 +14,8 @@ TEST(GLSwapInterval, Values)
 
 TEST(GLSwapInterval, ToString)
 {
+  ASSERT_THROW(cen::to_string(static_cast<cen::gl_swap_interval>(2)), cen::cen_error);
+
   ASSERT_EQ("immediate", cen::to_string(cen::gl_swap_interval::immediate));
   ASSERT_EQ("synchronized", cen::to_string(cen::gl_swap_interval::synchronized));
   ASSERT_EQ("late_immediate", cen::to_string(cen::gl_swap_interval::late_immediate));

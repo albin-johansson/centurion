5d4b800f7dc3ba9a3687198f76269ac5ad274f9b
Albin Johansson <albinoscar99@hotmail.com>
2021-09-14 13:23:02 +0200

    Update event.hpp

--- src/centurion/events/event.hpp
+++ src/centurion/events/event.hpp
@@ -29,6 +29,7 @@
 #include "mouse_wheel_event.hpp"
 #include "multi_gesture_event.hpp"
 #include "quit_event.hpp"
+#include "sensor_event.hpp"
 #include "text_editing_event.hpp"
 #include "text_input_event.hpp"
 #include "touch_finger_event.hpp"
@@ -387,9 +388,6 @@ class event final
                controller_axis_event,
                controller_button_event,
                controller_device_event,
-#if SDL_VERSION_ATLEAST(2, 0, 14)
-               display_event,
-#endif  // SDL_VERSION_ATLEAST(2, 0, 14)
                dollar_gesture_event,
                drop_event,
                joy_axis_event,
@@ -406,110 +404,174 @@ class event final
                text_editing_event,
                text_input_event,
                touch_finger_event,
+
+#if SDL_VERSION_ATLEAST(2, 0, 14)
+               display_event,
+               sensor_event,
+#endif  // SDL_VERSION_ATLEAST(2, 0, 14)
+
                window_event>
       m_data{};
 
-  // TODO 6.3.0: display_event, controller_sensor_event, sensor_event,
-  //  controller_touchpad_event, os_event, user_event
-  void update_data(const event_type event) noexcept
+  // TODO 6.3.0:
+  //   - display_event
+  //   - controller_sensor_event,
+  //   - controller_touchpad_event
+  //   - os_event
+  //   - user_event
+  void update_data(const event_type type) noexcept
   {
-    if (event == event_type::quit)
+    switch (type)
     {
-      m_data.emplace<quit_event>(m_event.quit);
-    }
-    else if (event == event_type::audio_device_added ||
-             event == event_type::audio_device_removed)
-    {
-      m_data.emplace<audio_device_event>(m_event.adevice);
-    }
-    else if (event == event_type::controller_axis_motion)
-    {
-      m_data.emplace<controller_axis_event>(m_event.caxis);
-    }
-    else if (event == event_type::controller_button_down ||
-             event == event_type::controller_button_up)
-    {
-      m_data.emplace<controller_button_event>(m_event.cbutton);
-    }
-    else if (event == event_type::controller_device_added ||
-             event == event_type::controller_device_removed ||
-             event == event_type::controller_device_remapped)
-    {
-      m_data.emplace<controller_device_event>(m_event.cdevice);
-    }
-    else if (event == event_type::dollar_gesture || event == event_type::dollar_record)
-    {
-      m_data.emplace<dollar_gesture_event>(m_event.dgesture);
-    }
-    else if (event == event_type::drop_begin || event == event_type::drop_complete ||
-             event == event_type::drop_file || event == event_type::drop_text)
-    {
-      m_data.emplace<drop_event>(m_event.drop);
-    }
-    else if (event == event_type::joystick_axis_motion)
-    {
-      m_data.emplace<joy_axis_event>(m_event.jaxis);
-    }
-    else if (event == event_type::joystick_ball_motion)
-    {
-      m_data.emplace<joy_ball_event>(m_event.jball);
-    }
-    else if (event == event_type::joystick_button_up ||
-             event == event_type::joystick_button_down)
-    {
-      m_data.emplace<joy_button_event>(m_event.jbutton);
-    }
-    else if (event == event_type::joystick_device_added ||
-             event == event_type::joystick_device_removed)
-    {
-      m_data.emplace<joy_device_event>(m_event.jdevice);
-    }
-    else if (event == event_type::joystick_hat_motion)
-    {
-      m_data.emplace<joy_hat_event>(m_event.jhat);
-    }
-    else if (event == event_type::key_down || event == event_type::key_up)
-    {
-      m_data.emplace<keyboard_event>(m_event.key);
-    }
-    else if (event == event_type::mouse_button_up ||
-             event == event_type::mouse_button_down)
-    {
-      m_data.emplace<mouse_button_event>(m_event.button);
-    }
-    else if (event == event_type::mouse_motion)
-    {
-      m_data.emplace<mouse_motion_event>(m_event.motion);
-    }
-    else if (event == event_type::mouse_wheel)
-    {
-      m_data.emplace<mouse_wheel_event>(m_event.wheel);
-    }
-    else if (event == event_type::multi_gesture)
-    {
-      m_data.emplace<multi_gesture_event>(m_event.mgesture);
-    }
-    else if (event == event_type::text_editing)
-    {
-      m_data.emplace<text_editing_event>(m_event.edit);
-    }
-    else if (event == event_type::text_input)
-    {
-      m_data.emplace<text_input_event>(m_event.text);
-    }
-    else if (event == event_type::touch_motion || event == event_type::touch_down ||
-             event == event_type::touch_up)
-    {
-      m_data.emplace<touch_finger_event>(m_event.tfinger);
-    }
-    else if (event == event_type::window)
-    {
-      m_data.emplace<window_event>(m_event.window);
-    }
+      case event_type::quit:
+        m_data.emplace<quit_event>(m_event.quit);
+        break;
+
+      case event_type::app_terminating:
+        break;
+      case event_type::app_low_memory:
+        break;
+      case event_type::app_will_enter_background:
+        break;
+      case event_type::app_did_enter_background:
+        break;
+      case event_type::app_will_enter_foreground:
+        break;
+      case event_type::app_did_enter_foreground:
+        break;
+
+      case event_type::locale_changed:
+        break;
+
 #if SDL_VERSION_ATLEAST(2, 0, 14)
-    else if (event == event_type::display)
-    {
-      m_data.emplace<display_event>(m_event.display);
+      case event_type::display:
+        m_data.emplace<display_event>(m_event.display);
+        break;
+#endif  // SDL_VERSION_ATLEAST(2, 0, 14)
+
+      case event_type::window:
+        m_data.emplace<window_event>(m_event.window);
+        break;
+
+      case event_type::system:
+        break;
+
+      case event_type::key_down:
+      case event_type::key_up:
+        m_data.emplace<keyboard_event>(m_event.key);
+        break;
+
+      case event_type::text_editing:
+        m_data.emplace<text_editing_event>(m_event.edit);
+        break;
+
+      case event_type::text_input:
+        m_data.emplace<text_input_event>(m_event.text);
+        break;
+
+      case event_type::keymap_changed:
+        break;
+
+      case event_type::mouse_motion:
+        m_data.emplace<mouse_motion_event>(m_event.motion);
+        break;
+
+      case event_type::mouse_button_down:
+      case event_type::mouse_button_up:
+        m_data.emplace<mouse_button_event>(m_event.button);
+        break;
+
+      case event_type::mouse_wheel:
+        m_data.emplace<mouse_wheel_event>(m_event.wheel);
+        break;
+
+      case event_type::joystick_axis_motion:
+        m_data.emplace<joy_axis_event>(m_event.jaxis);
+        break;
+
+      case event_type::joystick_ball_motion:
+        m_data.emplace<joy_ball_event>(m_event.jball);
+        break;
+
+      case event_type::joystick_hat_motion:
+        m_data.emplace<joy_hat_event>(m_event.jhat);
+        break;
+
+      case event_type::joystick_button_down:
+      case event_type::joystick_button_up:
+        m_data.emplace<joy_button_event>(m_event.jbutton);
+        break;
+
+      case event_type::joystick_device_added:
+      case event_type::joystick_device_removed:
+        m_data.emplace<joy_device_event>(m_event.jdevice);
+        break;
+
+      case event_type::controller_axis_motion:
+        m_data.emplace<controller_axis_event>(m_event.caxis);
+        break;
+
+      case event_type::controller_button_down:
+      case event_type::controller_button_up:
+        m_data.emplace<controller_button_event>(m_event.cbutton);
+        break;
+
+      case event_type::controller_device_added:
+      case event_type::controller_device_removed:
+      case event_type::controller_device_remapped:
+        m_data.emplace<controller_device_event>(m_event.cdevice);
+        break;
+
+      case event_type::controller_touchpad_down:
+        break;
+      case event_type::controller_touchpad_up:
+        break;
+      case event_type::controller_touchpad_motion:
+        break;
+      case event_type::controller_sensor_update:
+        break;
+
+      case event_type::touch_down:
+      case event_type::touch_up:
+      case event_type::touch_motion:
+        m_data.emplace<touch_finger_event>(m_event.tfinger);
+        break;
+
+      case event_type::dollar_gesture:
+      case event_type::dollar_record:
+        m_data.emplace<dollar_gesture_event>(m_event.dgesture);
+        break;
+
+      case event_type::multi_gesture:
+        m_data.emplace<multi_gesture_event>(m_event.mgesture);
+        break;
+
+      case event_type::clipboard_update:
+        break;
+
+      case event_type::drop_file:
+      case event_type::drop_text:
+      case event_type::drop_begin:
+      case event_type::drop_complete:
+        m_data.emplace<drop_event>(m_event.drop);
+        break;
+
+      case event_type::audio_device_added:
+      case event_type::audio_device_removed:
+        m_data.emplace<audio_device_event>(m_event.adevice);
+        break;
+
+      case event_type::sensor_update:
+        m_data.emplace<sensor_event>(m_event.sensor);
+        break;
+
+      case event_type::render_targets_reset:
+        break;
+      case event_type::render_device_reset:
+        break;
+
+      case event_type::user:
+        break;
     }
 #endif  // SDL_VERSION_ATLEAST(2, 0, 14)
   }

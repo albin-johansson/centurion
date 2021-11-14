021bfae32a9ccfcf2e787114d08b488bbe897c2c
Albin Johansson <albinoscar99@hotmail.com>
2021-11-14 21:01:28 +0100

    Remove aliases czstring and zstring

--- src/centurion/core/str.hpp
+++ src/centurion/core/str.hpp
@@ -15,21 +15,6 @@ namespace cen {
  */
 using str = const char*;
 
-/**
- * \typedef czstring
- *
- * \brief Alias for a const C-style null-terminated string.
- *
- * \deprecated Since 6.2.0, use `str` instead.
- */
-using czstring [[deprecated]] = const char*;
-
-/**
- * \typedef zstring
- * \deprecated Since 6.2.0.
- */
-using zstring [[deprecated]] = char*;
-
 /// \} End of group core
 
 }  // namespace cen

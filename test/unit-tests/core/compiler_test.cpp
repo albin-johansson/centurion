4b48cd9e159314287b31b597568a12eb82408e77
Albin Johansson <albinoscar99@hotmail.com>
2021-11-19 17:44:59 +0100

    Update compiler_test.cpp

--- test/unit-tests/core/compiler_test.cpp
+++ test/unit-tests/core/compiler_test.cpp
@@ -1,4 +1,4 @@
-#include "compiler/compiler.hpp"
+#include "core/compiler.hpp"
 
 #include <gtest/gtest.h>
 
@@ -8,7 +8,7 @@ TEST(Compiler, IsDebugBuild)
   ASSERT_FALSE(cen::is_debug_build());
 #else
   ASSERT_TRUE(cen::is_debug_build());
-#endif // NDEBUG
+#endif  // NDEBUG
 }
 
 TEST(Compiler, IsReleaseBuild)
@@ -17,7 +17,7 @@ TEST(Compiler, IsReleaseBuild)
   ASSERT_TRUE(cen::is_release_build());
 #else
   ASSERT_FALSE(cen::is_release_build());
-#endif // NDEBUG
+#endif  // NDEBUG
 }
 
 TEST(Compiler, OnMSVC)
@@ -26,7 +26,7 @@ TEST(Compiler, OnMSVC)
   ASSERT_TRUE(cen::on_msvc());
 #else
   ASSERT_FALSE(cen::on_msvc());
-#endif // _MSC_VER
+#endif  // _MSC_VER
 }
 
 TEST(Compiler, OnClang)
@@ -35,7 +35,7 @@ TEST(Compiler, OnClang)
   ASSERT_TRUE(cen::on_clang());
 #else
   ASSERT_FALSE(cen::on_clang());
-#endif // __clang__
+#endif  // __clang__
 }
 
 TEST(Compiler, OnGCC)
@@ -44,5 +44,5 @@ TEST(Compiler, OnGCC)
   ASSERT_TRUE(cen::on_gcc());
 #else
   ASSERT_FALSE(cen::on_gcc());
-#endif // __GNUC__
+#endif  // __GNUC__
 }

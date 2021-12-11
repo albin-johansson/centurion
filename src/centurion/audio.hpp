cfba8b9e9164284e080a4ed48ec91067c9953d31
Albin Johansson <albinoscar99@hotmail.com>
2021-12-12 00:48:03 +0100

    Remove channels.hpp

--- src/centurion/audio.hpp
+++ src/centurion/audio.hpp
@@ -1,7 +1,6 @@
 #ifndef CENTURION_AUDIO_HPP_
 #define CENTURION_AUDIO_HPP_
 
-#include "audio/channels.hpp"
 #include "audio/music.hpp"
 #include "audio/sound_effect.hpp"
 #include "audio/sound_fonts.hpp"

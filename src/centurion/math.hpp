56ab89803dd76fbe2fa3461c8b981ef3f0db22ff
Albin Johansson <albinoscar99@hotmail.com>
2021-12-12 02:57:54 +0100

    Remove math.hpp

--- src/centurion/math.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef CENTURION_MATH_HPP_
-#define CENTURION_MATH_HPP_
-
-#include "math/area.hpp"
-#include "math/point.hpp"
-#include "math/rect.hpp"
-#include "math/vector3.hpp"
-
-#endif  // CENTURION_MATH_HPP_

4e4ecfb2b05b145b03859716390bab21cbe280b5
Albin Johansson <albinoscar99@hotmail.com>
2021-12-12 02:48:04 +0100

    Update memory.hpp

--- src/centurion/core/memory.hpp
+++ src/centurion/core/memory.hpp
@@ -7,6 +7,10 @@
 #include <SDL_mixer.h>
 #endif  // CENTURION_NO_SDL_MIXER
 
+#ifndef CENTURION_NO_SDL_TTF
+#include <SDL_ttf.h>
+#endif  // CENTURION_NO_SDL_TTF
+
 #include <memory>  // unique_ptr
 
 namespace cen {
@@ -41,6 +45,114 @@ struct Deleter<SDL_RWops> final
   }
 };
 
+template <>
+struct Deleter<SDL_Surface> final
+{
+  void operator()(SDL_Surface* surface) noexcept
+  {
+    SDL_FreeSurface(surface);
+  }
+};
+
+template <>
+struct Deleter<SDL_Texture> final
+{
+  void operator()(SDL_Texture* texture) noexcept
+  {
+    SDL_DestroyTexture(texture);
+  }
+};
+
+template <>
+struct Deleter<SDL_PixelFormat> final
+{
+  void operator()(SDL_PixelFormat* format) noexcept
+  {
+    SDL_FreeFormat(format);
+  }
+};
+
+template <>
+struct Deleter<SDL_Palette> final
+{
+  void operator()(SDL_Palette* palette) noexcept
+  {
+    SDL_FreePalette(palette);
+  }
+};
+
+template <>
+struct Deleter<SDL_Cursor> final
+{
+  void operator()(SDL_Cursor* cursor) noexcept
+  {
+    SDL_FreeCursor(cursor);
+  }
+};
+
+template <>
+struct Deleter<SDL_Joystick> final
+{
+  void operator()(SDL_Joystick* joystick) noexcept
+  {
+    SDL_JoystickClose(joystick);
+  }
+};
+
+template <>
+struct Deleter<SDL_GameController> final
+{
+  void operator()(SDL_GameController* controller) noexcept
+  {
+    SDL_GameControllerClose(controller);
+  }
+};
+
+template <>
+struct Deleter<SDL_Haptic> final
+{
+  void operator()(SDL_Haptic* haptic) noexcept
+  {
+    SDL_HapticClose(haptic);
+  }
+};
+
+template <>
+struct Deleter<SDL_Sensor> final
+{
+  void operator()(SDL_Sensor* sensor) noexcept
+  {
+    SDL_SensorClose(sensor);
+  }
+};
+
+template <>
+struct Deleter<SDL_mutex> final
+{
+  void operator()(SDL_mutex* mutex) noexcept
+  {
+    SDL_DestroyMutex(mutex);
+  }
+};
+
+template <>
+struct Deleter<SDL_sem> final
+{
+  void operator()(SDL_sem* semaphore) noexcept
+  {
+    SDL_DestroySemaphore(semaphore);
+  }
+};
+
+template <>
+struct Deleter<SDL_cond> final
+{
+  void operator()(SDL_cond* cond) noexcept
+  {
+    SDL_DestroyCond(cond);
+  }
+};
+
 #ifndef CENTURION_NO_SDL_MIXER
 
 template <>
@@ -62,6 +174,19 @@ struct Deleter<Mix_Chunk> final
 };
 
 #endif  // CENTURION_NO_SDL_MIXER
+
+#ifndef CENTURION_NO_SDL_TTF
+
+template <>
+struct Deleter<TTF_Font> final
+{
+  void operator()(TTF_Font* font) noexcept
+  {
+    TTF_CloseFont(font);
+  }
+};
+
+#endif  // CENTURION_NO_SDL_TTF
 
 template <typename T>
 using Managed = std::unique_ptr<T, Deleter<T>>;

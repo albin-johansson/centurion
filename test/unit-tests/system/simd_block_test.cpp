5a3b62618f46449730ca109e21a9683593d43f43
Albin Johansson <albinoscar99@hotmail.com>
2021-12-13 22:08:40 +0100

    Update SIMD block class

--- test/unit-tests/system/simd_block_test.cpp
+++ test/unit-tests/system/simd_block_test.cpp
@@ -4,20 +4,17 @@
 
 TEST(SIMDBlock, Usage)
 {
-  cen::simd_block block{100};
+  cen::SIMDBlock block{100};
   ASSERT_TRUE(block);
   ASSERT_TRUE(block.data());
-  ASSERT_TRUE(block.cast_data<int>());
 
   const auto& cblock = block;
   ASSERT_TRUE(cblock);
   ASSERT_TRUE(cblock.data());
-  ASSERT_TRUE(cblock.cast_data<int>());
 
 #if SDL_VERSION_ATLEAST(2, 0, 14)
-  block.reallocate(50);
+  block.Reallocate(50);
   ASSERT_TRUE(block);
   ASSERT_TRUE(block.data());
-  ASSERT_TRUE(block.cast_data<int>());
 #endif  // SDL_VERSION_ATLEAST(2, 0, 14)
 }

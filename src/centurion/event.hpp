51e0f5fdc4b70404509c5c5f85cf233666356d8c
Albin Johansson <albinoscar99@hotmail.com>
2021-12-21 16:10:27 +0100

    Update event.hpp

--- src/centurion/event.hpp
+++ src/centurion/event.hpp
@@ -71,7 +71,7 @@ class Event final {
   }
 
   /// Polls the next available event, if there is one.
-  /// \return `true` if there are any pending events; `false` otherwise.
+  /// \return `true` if there are pending events; `false` otherwise.
   auto Poll() noexcept -> bool
   {
     const bool result = SDL_PollEvent(&mEvent);
@@ -86,13 +86,14 @@ class Event final {
     return result;
   }
 
+  /// Returns the type of the current event, if there is one.
   [[nodiscard]] auto GetType() const noexcept -> std::optional<EventType>
   {
     if (IsEmpty()) {
       return std::nullopt;
     }
     else {
-      return static_cast<EventType>(mEvent.type);
+      return EventType{mEvent.type};
     }
   }
 
@@ -382,10 +383,10 @@ class EventSink final {
   using signature_type = void(const event_type&);  ///< Signature of handler.
   using function_type = std::function<signature_type>;
 
-  /* Resets the event sink, removing any associated handler. */
+  /// Resets the event sink, removing any associated handler.
   void Reset() noexcept { mFunction = nullptr; }
 
-  /* Connects to a function object. */
+  /// Connects to a function object.
   template <typename T>
   void To(T&& callable)
   {
@@ -395,7 +396,7 @@ class EventSink final {
     mFunction = function_type{callable};
   }
 
-  /* Connects to a member function. */
+  /// Connects to a member function.
   template <auto MemberFunc, typename Self>
   void To(Self* self)
   {
@@ -406,7 +407,7 @@ class EventSink final {
     To(std::bind(MemberFunc, self, std::placeholders::_1));
   }
 
-  /* Connects to a free function. */
+  /// Connects to a free function.
   template <auto Function>
   void To()
   {
@@ -420,15 +421,15 @@ class EventSink final {
   function_type mFunction;
 };
 
-/// An event dispatcher wrapper around a `cen::event` instance.
+/// An event dispatcher, implemented as wrapper of an `Event` instance.
 ///
 /// This class is an attempt to simplify handling events in applications, usually you'd check
-/// for all of the events that you'd be interested in using `cen::event::try_get()` in a long
+/// for all of the events that you'd be interested in using `cen::Event::TryGet()` in a long
 /// `if-else if` statement. By using this class, it's possible to automatically generate
 /// equivalent checks by specifying the events that you want to subscribe to, and subsequently
 /// connect lambdas, free functions or member functions to handle the subscribed events. This
 /// can often lead to cleaner looking code, since the manual checks will be replaced by a
-/// single call to `event_dispatcher::poll()`.
+/// single call to `EventDispatcher::Poll()`.
 ///
 /// The runtime overhead of using this class compared to typical manual event dispatching is
 /// minimal. However, the function objects for the subscribed events are stored internally, so
@@ -441,16 +442,17 @@ class EventSink final {
 /// Note, it is advisable to always typedef the signature of this class with the events that
 /// you want to handle, since the class name quickly grows in size.
 ///
-/// \tparam E the list of events to "subscribe" to. Events that are not present in this list
-/// are ignored.
-template <typename... E>
+/// \tparam Events the list of events to "subscribe" to, all other events are ignored.
+template <typename... Events>
 class EventDispatcher final {
-  static_assert((!std::is_const_v<E> && ...));
-  static_assert((!std::is_volatile_v<E> && ...));
-  static_assert((!std::is_reference_v<E> && ...));
-  static_assert((!std::is_pointer_v<E> && ...));
+  static_assert((!std::is_const_v<Events> && ...));
+  static_assert((!std::is_volatile_v<Events> && ...));
+  static_assert((!std::is_reference_v<Events> && ...));
+  static_assert((!std::is_pointer_v<Events> && ...));
 
-  /* Returns the index of an event type in the function tuple. */
+  using sink_tuple = std::tuple<EventSink<Events>...>;
+
+  /// Returns the index of an event type in the function tuple.
   template <typename Event>
   [[nodiscard]] constexpr static auto IndexOf() -> std::size_t
   {
@@ -476,12 +478,14 @@ class EventDispatcher final {
     return std::get<index>(mSinks);
   }
 
-  /* Checks for the specified event type in the event handler, returns true upon a match. */
+  /// Checks for the specified event type in the event handler, returns true upon a match.
   template <typename Event>
   auto CheckFor() -> bool
   {
     if (const auto* event = mEvent.template TryGet<Event>()) {
-      if (auto& function = GetSink<Event>().GetFunction()) {
+      auto& function = GetSink<Event>().GetFunction();
+
+      if (function) {
         function(*event);
       }
 
@@ -493,42 +497,40 @@ class EventDispatcher final {
   }
 
  public:
-  /* Polls all events, checking for subscribed events. */
+  /// Polls all events, checking for subscribed events.
   void Poll()
   {
     while (mEvent.Poll()) {
-      (CheckFor<E>() || ...); /* Use OR to exploit short-circuiting */
+      (CheckFor<Events>() || ...); /* Use OR to exploit short-circuiting */
     }
   }
 
-  /* Returns the event sink associated with the specified event. */
+  /// Returns the event sink associated with the specified event.
   template <typename Event>
   auto Bind() -> EventSink<Event>&
   {
-    static_assert((std::is_same_v<std::decay_t<Event>, E> || ...),
+    static_assert((std::is_same_v<std::decay_t<Event>, Events> || ...),
                   "Cannot connect unsubscribed event! Make sure that the "
                   "event is provided as a class template parameter.");
     return GetSink<Event>();
   }
 
-  /* Removes all set handlers from all the subscribed events. */
-  void Reset() noexcept { (Bind<E>().Reset(), ...); }
+  /// Removes all set handlers from all the subscribed events.
+  void Reset() noexcept { (Bind<Events>().Reset(), ...); }
 
-  /* Returns the amount of set event handlers. */
+  /// Returns the amount of set event handlers.
   [[nodiscard]] auto GetActiveCount() const -> std::size_t
   {
-    return (0u + ... + (GetSink<E>().GetFunction() ? 1u : 0u));
+    return (0u + ... + (GetSink<Events>().GetFunction() ? 1u : 0u));
   }
 
-  /* Returns the number of subscribed events. */
+  /// Returns the number of subscribed events.
   [[nodiscard]] constexpr static auto GetSize() noexcept -> std::size_t
   {
-    return sizeof...(E);
+    return sizeof...(Events);
   }
 
  private:
-  using sink_tuple = std::tuple<EventSink<E>...>;
-
   cen::Event mEvent;
   sink_tuple mSinks;
 };

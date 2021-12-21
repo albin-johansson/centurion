895705e48aea48c337e339e90fdd9126d89c6399
Albin Johansson <albinoscar99@hotmail.com>
2021-12-21 16:03:00 +0100

    Move some tests

--- test/unit-tests/event/event_test.cpp
+++ test/unit-tests/event/event_test.cpp
@@ -6,29 +6,15 @@
 
 namespace {
 
-[[nodiscard]] auto create_event(const Uint32 type) -> cen::Event
+[[nodiscard]] auto CreateEvent(const Uint32 type) -> cen::Event
 {
-  SDL_Event sdlEvent;
-  sdlEvent.type = type;
-  return cen::Event{sdlEvent};
+  SDL_Event event;
+  event.type = type;
+  return cen::Event{event};
 }
 
-/**
- * \brief Indicates whether or not a Centurion event type fulfills the event
- * type specification.
- *
- * \ingroup event
- *
- * \tparam T the Centurion event type that will be checked.
- * \tparam E the SDL event type that the Centurion event is mirroring.
- *
- * \return `true` if the supplied event type passed the requirements; `false`
- * otherwise.
- *
- * \since 4.0.0
- */
 template <typename T, typename E>
-[[nodiscard]] constexpr auto validate_event() noexcept -> bool
+[[nodiscard]] constexpr auto ValidateEvent() noexcept -> bool
 {
   return !std::has_virtual_destructor_v<T> && std::is_nothrow_copy_constructible_v<T> &&
          std::is_nothrow_copy_assignable_v<T> && std::is_nothrow_move_constructible_v<T> &&
@@ -38,45 +24,42 @@ template <typename T, typename E>
 
 }  // namespace
 
-// clang-format off
-static_assert(validate_event<cen::AudioDeviceEvent, SDL_AudioDeviceEvent>());
-static_assert(validate_event<cen::ControllerAxisEvent, SDL_ControllerAxisEvent>());
-static_assert(validate_event<cen::ControllerButtonEvent, SDL_ControllerButtonEvent>());
-static_assert(validate_event<cen::ControllerDeviceEvent, SDL_ControllerDeviceEvent>());
-static_assert(validate_event<cen::dollar_gesture_event, SDL_DollarGestureEvent>());
-static_assert(validate_event<cen::drop_event, SDL_DropEvent>());
-static_assert(validate_event<cen::JoyAxisEvent, SDL_JoyAxisEvent>());
-static_assert(validate_event<cen::JoyBallEvent, SDL_JoyBallEvent>());
-static_assert(validate_event<cen::JoyButtonEvent, SDL_JoyButtonEvent>());
-static_assert(validate_event<cen::JoyDeviceEvent, SDL_JoyDeviceEvent>());
-static_assert(validate_event<cen::JoyHatEvent, SDL_JoyHatEvent>());
-static_assert(validate_event<cen::keyboard_event, SDL_KeyboardEvent>());
-static_assert(validate_event<cen::MouseButtonEvent, SDL_MouseButtonEvent>());
-static_assert(validate_event<cen::MouseMotionEvent, SDL_MouseMotionEvent>());
-static_assert(validate_event<cen::MouseWheelEvent, SDL_MouseWheelEvent>());
-static_assert(validate_event<cen::multi_gesture_event, SDL_MultiGestureEvent>());
-static_assert(validate_event<cen::quit_event, SDL_QuitEvent>());
-static_assert(validate_event<cen::text_editing_event, SDL_TextEditingEvent>());
-static_assert(validate_event<cen::text_input_event, SDL_TextInputEvent>());
-static_assert(validate_event<cen::touch_finger_event, SDL_TouchFingerEvent>());
-static_assert(validate_event<cen::WindowEvent, SDL_WindowEvent>());
-static_assert(validate_event<cen::sensor_event, SDL_SensorEvent>());
-static_assert(validate_event<cen::user_event, SDL_UserEvent>());
+static_assert(ValidateEvent<cen::AudioDeviceEvent, SDL_AudioDeviceEvent>());
+static_assert(ValidateEvent<cen::ControllerAxisEvent, SDL_ControllerAxisEvent>());
+static_assert(ValidateEvent<cen::ControllerButtonEvent, SDL_ControllerButtonEvent>());
+static_assert(ValidateEvent<cen::ControllerDeviceEvent, SDL_ControllerDeviceEvent>());
+static_assert(ValidateEvent<cen::dollar_gesture_event, SDL_DollarGestureEvent>());
+static_assert(ValidateEvent<cen::drop_event, SDL_DropEvent>());
+static_assert(ValidateEvent<cen::JoyAxisEvent, SDL_JoyAxisEvent>());
+static_assert(ValidateEvent<cen::JoyBallEvent, SDL_JoyBallEvent>());
+static_assert(ValidateEvent<cen::JoyButtonEvent, SDL_JoyButtonEvent>());
+static_assert(ValidateEvent<cen::JoyDeviceEvent, SDL_JoyDeviceEvent>());
+static_assert(ValidateEvent<cen::JoyHatEvent, SDL_JoyHatEvent>());
+static_assert(ValidateEvent<cen::keyboard_event, SDL_KeyboardEvent>());
+static_assert(ValidateEvent<cen::MouseButtonEvent, SDL_MouseButtonEvent>());
+static_assert(ValidateEvent<cen::MouseMotionEvent, SDL_MouseMotionEvent>());
+static_assert(ValidateEvent<cen::MouseWheelEvent, SDL_MouseWheelEvent>());
+static_assert(ValidateEvent<cen::multi_gesture_event, SDL_MultiGestureEvent>());
+static_assert(ValidateEvent<cen::quit_event, SDL_QuitEvent>());
+static_assert(ValidateEvent<cen::text_editing_event, SDL_TextEditingEvent>());
+static_assert(ValidateEvent<cen::text_input_event, SDL_TextInputEvent>());
+static_assert(ValidateEvent<cen::touch_finger_event, SDL_TouchFingerEvent>());
+static_assert(ValidateEvent<cen::WindowEvent, SDL_WindowEvent>());
+static_assert(ValidateEvent<cen::sensor_event, SDL_SensorEvent>());
+static_assert(ValidateEvent<cen::user_event, SDL_UserEvent>());
 
 #if SDL_VERSION_ATLEAST(2, 0, 14)
-static_assert(validate_event<cen::DisplayEvent, SDL_DisplayEvent>());
-static_assert(validate_event<cen::ControllerTouchpadEvent, SDL_ControllerTouchpadEvent>());
-static_assert(validate_event<cen::ControllerSensorEvent, SDL_ControllerSensorEvent>());
+static_assert(ValidateEvent<cen::DisplayEvent, SDL_DisplayEvent>());
+static_assert(ValidateEvent<cen::ControllerTouchpadEvent, SDL_ControllerTouchpadEvent>());
+static_assert(ValidateEvent<cen::ControllerSensorEvent, SDL_ControllerSensorEvent>());
 #endif  // SDL_VERSION_ATLEAST(2, 0, 14)
-
-// clang-format on
 
 TEST(Event, Push)
 {
   cen::Event::FlushAll();
 
   {
-    auto event = create_event(SDL_KEYDOWN);
+    auto event = CreateEvent(SDL_KEYDOWN);
     cen::Event::Push(event);
   }
 
@@ -139,7 +122,7 @@ TEST(Event, GetQueueSize)
   ASSERT_EQ(0, cen::Event::GetQueueSize());
   ASSERT_EQ(0, cen::Event::GetQueueSize(cen::EventType::Quit));
 
-  auto event = create_event(SDL_QUIT);
+  auto event = CreateEvent(SDL_QUIT);
   cen::Event::Push(event);
 
   ASSERT_EQ(1, cen::Event::GetQueueSize());
@@ -182,16 +165,16 @@ TEST(Event, IsEmpty)
   cen::Event empty;
   ASSERT_TRUE(empty.IsEmpty());
 
-  const auto notEmpty = create_event(SDL_AUDIODEVICEADDED);
+  const auto notEmpty = CreateEvent(SDL_AUDIODEVICEADDED);
   ASSERT_FALSE(notEmpty.IsEmpty());
 }
 
 TEST(Event, Is)
 {
   {  // audio_device_event
-    const auto added = create_event(SDL_AUDIODEVICEADDED);
-    const auto removed = create_event(SDL_AUDIODEVICEREMOVED);
-    const auto wrong = create_event(SDL_QUIT);
+    const auto added = CreateEvent(SDL_AUDIODEVICEADDED);
+    const auto removed = CreateEvent(SDL_AUDIODEVICEREMOVED);
+    const auto wrong = CreateEvent(SDL_QUIT);
 
     ASSERT_TRUE(added.Is<cen::AudioDeviceEvent>());
     ASSERT_TRUE(removed.Is<cen::AudioDeviceEvent>());
@@ -199,17 +182,17 @@ TEST(Event, Is)
   }
 
   {  // controller_axis_event
-    const auto event = create_event(SDL_CONTROLLERAXISMOTION);
-    const auto wrong = create_event(SDL_QUIT);
+    const auto event = CreateEvent(SDL_CONTROLLERAXISMOTION);
+    const auto wrong = CreateEvent(SDL_QUIT);
 
     ASSERT_TRUE(event.Is<cen::ControllerAxisEvent>());
     ASSERT_FALSE(wrong.Is<cen::ControllerAxisEvent>());
   }
 
   {  // controller_button_event
-    const auto up = create_event(SDL_CONTROLLERBUTTONUP);
-    const auto down = create_event(SDL_CONTROLLERBUTTONDOWN);
-    const auto wrong = create_event(SDL_QUIT);
+    const auto up = CreateEvent(SDL_CONTROLLERBUTTONUP);
+    const auto down = CreateEvent(SDL_CONTROLLERBUTTONDOWN);
+    const auto wrong = CreateEvent(SDL_QUIT);
 
     ASSERT_TRUE(up.Is<cen::ControllerButtonEvent>());
     ASSERT_TRUE(down.Is<cen::ControllerButtonEvent>());
@@ -217,10 +200,10 @@ TEST(Event, Is)
   }
 
   {  // controller_device_event
-    const auto added = create_event(SDL_CONTROLLERDEVICEADDED);
-    const auto removed = create_event(SDL_CONTROLLERDEVICEREMOVED);
-    const auto remapped = create_event(SDL_CONTROLLERDEVICEREMAPPED);
-    const auto wrong = create_event(SDL_QUIT);
+    const auto added = CreateEvent(SDL_CONTROLLERDEVICEADDED);
+    const auto removed = CreateEvent(SDL_CONTROLLERDEVICEREMOVED);
+    const auto remapped = CreateEvent(SDL_CONTROLLERDEVICEREMAPPED);
+    const auto wrong = CreateEvent(SDL_QUIT);
 
     ASSERT_TRUE(added.Is<cen::ControllerDeviceEvent>());
     ASSERT_TRUE(removed.Is<cen::ControllerDeviceEvent>());
@@ -229,9 +212,9 @@ TEST(Event, Is)
   }
 
   {  // dollar_gesture_event
-    const auto gesture = create_event(SDL_DOLLARGESTURE);
-    const auto record = create_event(SDL_DOLLARRECORD);
-    const auto wrong = create_event(SDL_QUIT);
+    const auto gesture = CreateEvent(SDL_DOLLARGESTURE);
+    const auto record = CreateEvent(SDL_DOLLARRECORD);
+    const auto wrong = CreateEvent(SDL_QUIT);
 
     ASSERT_TRUE(gesture.Is<cen::dollar_gesture_event>());
     ASSERT_TRUE(record.Is<cen::dollar_gesture_event>());
@@ -239,11 +222,11 @@ TEST(Event, Is)
   }
 
   {  // drop_event
-    const auto begin = create_event(SDL_DROPBEGIN);
-    const auto complete = create_event(SDL_DROPCOMPLETE);
-    const auto file = create_event(SDL_DROPFILE);
-    const auto text = create_event(SDL_DROPTEXT);
-    const auto wrong = create_event(SDL_QUIT);
+    const auto begin = CreateEvent(SDL_DROPBEGIN);
+    const auto complete = CreateEvent(SDL_DROPCOMPLETE);
+    const auto file = CreateEvent(SDL_DROPFILE);
+    const auto text = CreateEvent(SDL_DROPTEXT);
+    const auto wrong = CreateEvent(SDL_QUIT);
 
     ASSERT_TRUE(begin.Is<cen::drop_event>());
     ASSERT_TRUE(complete.Is<cen::drop_event>());
@@ -253,25 +236,25 @@ TEST(Event, Is)
   }
 
   {  // joy_axis_event
-    const auto motion = create_event(SDL_JOYAXISMOTION);
-    const auto wrong = create_event(SDL_QUIT);
+    const auto motion = CreateEvent(SDL_JOYAXISMOTION);
+    const auto wrong = CreateEvent(SDL_QUIT);
 
     ASSERT_TRUE(motion.Is<cen::JoyAxisEvent>());
     ASSERT_FALSE(wrong.Is<cen::JoyAxisEvent>());
   }
 
   {  // joy_ball_event
-    const auto motion = create_event(SDL_JOYBALLMOTION);
-    const auto wrong = create_event(SDL_QUIT);
+    const auto motion = CreateEvent(SDL_JOYBALLMOTION);
+    const auto wrong = CreateEvent(SDL_QUIT);
 
     ASSERT_TRUE(motion.Is<cen::JoyBallEvent>());
     ASSERT_FALSE(wrong.Is<cen::JoyBallEvent>());
   }
 
   {  // joy_button_event
-    const auto up = create_event(SDL_JOYBUTTONUP);
-    const auto down = create_event(SDL_JOYBUTTONDOWN);
-    const auto wrong = create_event(SDL_QUIT);
+    const auto up = CreateEvent(SDL_JOYBUTTONUP);
+    const auto down = CreateEvent(SDL_JOYBUTTONDOWN);
+    const auto wrong = CreateEvent(SDL_QUIT);
 
     ASSERT_TRUE(up.Is<cen::JoyButtonEvent>());
     ASSERT_TRUE(down.Is<cen::JoyButtonEvent>());
@@ -279,9 +262,9 @@ TEST(Event, Is)
   }
 
   {  // joy_device_event
-    const auto added = create_event(SDL_JOYDEVICEADDED);
-    const auto removed = create_event(SDL_JOYDEVICEREMOVED);
-    const auto wrong = create_event(SDL_QUIT);
+    const auto added = CreateEvent(SDL_JOYDEVICEADDED);
+    const auto removed = CreateEvent(SDL_JOYDEVICEREMOVED);
+    const auto wrong = CreateEvent(SDL_QUIT);
 
     ASSERT_TRUE(added.Is<cen::JoyDeviceEvent>());
     ASSERT_TRUE(removed.Is<cen::JoyDeviceEvent>());
@@ -289,17 +272,17 @@ TEST(Event, Is)
   }
 
   {  // joy_hat_event
-    const auto motion = create_event(SDL_JOYHATMOTION);
-    const auto wrong = create_event(SDL_QUIT);
+    const auto motion = CreateEvent(SDL_JOYHATMOTION);
+    const auto wrong = CreateEvent(SDL_QUIT);
 
     ASSERT_TRUE(motion.Is<cen::JoyHatEvent>());
     ASSERT_FALSE(wrong.Is<cen::JoyHatEvent>());
   }
 
   {  // keyboard_event
-    const auto up = create_event(SDL_KEYUP);
-    const auto down = create_event(SDL_KEYDOWN);
-    const auto wrong = create_event(SDL_QUIT);
+    const auto up = CreateEvent(SDL_KEYUP);
+    const auto down = CreateEvent(SDL_KEYDOWN);
+    const auto wrong = CreateEvent(SDL_QUIT);
 
     ASSERT_TRUE(up.Is<cen::keyboard_event>());
     ASSERT_TRUE(down.Is<cen::keyboard_event>());
@@ -307,9 +290,9 @@ TEST(Event, Is)
   }
 
   {  // mouse_button_event
-    const auto up = create_event(SDL_MOUSEBUTTONUP);
-    const auto down = create_event(SDL_MOUSEBUTTONDOWN);
-    const auto wrong = create_event(SDL_QUIT);
+    const auto up = CreateEvent(SDL_MOUSEBUTTONUP);
+    const auto down = CreateEvent(SDL_MOUSEBUTTONDOWN);
+    const auto wrong = CreateEvent(SDL_QUIT);
 
     ASSERT_TRUE(up.Is<cen::MouseButtonEvent>());
     ASSERT_TRUE(down.Is<cen::MouseButtonEvent>());
@@ -317,58 +300,58 @@ TEST(Event, Is)
   }
 
   {  // mouse_motion_event
-    const auto motion = create_event(SDL_MOUSEMOTION);
-    const auto wrong = create_event(SDL_QUIT);
+    const auto motion = CreateEvent(SDL_MOUSEMOTION);
+    const auto wrong = CreateEvent(SDL_QUIT);
 
     ASSERT_TRUE(motion.Is<cen::MouseMotionEvent>());
     ASSERT_FALSE(wrong.Is<cen::MouseButtonEvent>());
   }
 
   {  // mouse_wheel_event
-    const auto wheel = create_event(SDL_MOUSEWHEEL);
-    const auto wrong = create_event(SDL_QUIT);
+    const auto wheel = CreateEvent(SDL_MOUSEWHEEL);
+    const auto wrong = CreateEvent(SDL_QUIT);
 
     ASSERT_TRUE(wheel.Is<cen::MouseWheelEvent>());
     ASSERT_FALSE(wrong.Is<cen::MouseWheelEvent>());
   }
 
   {  // multi_gesture_event
-    const auto gesture = create_event(SDL_MULTIGESTURE);
-    const auto wrong = create_event(SDL_QUIT);
+    const auto gesture = CreateEvent(SDL_MULTIGESTURE);
+    const auto wrong = CreateEvent(SDL_QUIT);
 
     ASSERT_TRUE(gesture.Is<cen::multi_gesture_event>());
     ASSERT_FALSE(wrong.Is<cen::multi_gesture_event>());
   }
 
   {  // quit_event
-    const auto quit = create_event(SDL_QUIT);
-    const auto wrong = create_event(SDL_KEYUP);
+    const auto quit = CreateEvent(SDL_QUIT);
+    const auto wrong = CreateEvent(SDL_KEYUP);
 
     ASSERT_TRUE(quit.Is<cen::quit_event>());
     ASSERT_FALSE(wrong.Is<cen::quit_event>());
   }
 
   {  // text_editing_event
-    const auto editing = create_event(SDL_TEXTEDITING);
-    const auto wrong = create_event(SDL_QUIT);
+    const auto editing = CreateEvent(SDL_TEXTEDITING);
+    const auto wrong = CreateEvent(SDL_QUIT);
 
     ASSERT_TRUE(editing.Is<cen::text_editing_event>());
     ASSERT_FALSE(wrong.Is<cen::text_editing_event>());
   }
 
   {  // text_input_event
-    const auto input = create_event(SDL_TEXTINPUT);
-    const auto wrong = create_event(SDL_QUIT);
+    const auto input = CreateEvent(SDL_TEXTINPUT);
+    const auto wrong = CreateEvent(SDL_QUIT);
 
     ASSERT_TRUE(input.Is<cen::text_input_event>());
     ASSERT_FALSE(wrong.Is<cen::text_input_event>());
   }
 
   {  // touch_finger_event
-    const auto motion = create_event(SDL_FINGERMOTION);
-    const auto up = create_event(SDL_FINGERUP);
-    const auto down = create_event(SDL_FINGERDOWN);
-    const auto wrong = create_event(SDL_QUIT);
+    const auto motion = CreateEvent(SDL_FINGERMOTION);
+    const auto up = CreateEvent(SDL_FINGERUP);
+    const auto down = CreateEvent(SDL_FINGERDOWN);
+    const auto wrong = CreateEvent(SDL_QUIT);
 
     ASSERT_TRUE(motion.Is<cen::touch_finger_event>());
     ASSERT_TRUE(up.Is<cen::touch_finger_event>());
@@ -377,8 +360,8 @@ TEST(Event, Is)
   }
 
   {  // window_event
-    const auto window = create_event(SDL_WINDOWEVENT);
-    const auto wrong = create_event(SDL_QUIT);
+    const auto window = CreateEvent(SDL_WINDOWEVENT);
+    const auto wrong = CreateEvent(SDL_QUIT);
 
     ASSERT_TRUE(window.Is<cen::WindowEvent>());
     ASSERT_FALSE(wrong.Is<cen::WindowEvent>());
@@ -387,7 +370,7 @@ TEST(Event, Is)
 
 TEST(Event, Get)
 {
-  auto event = create_event(SDL_QUIT);
+  auto event = CreateEvent(SDL_QUIT);
   ASSERT_NO_THROW(event.Get<cen::quit_event>());
   ASSERT_ANY_THROW(event.Get<cen::WindowEvent>());
 
@@ -398,7 +381,7 @@ TEST(Event, Get)
 
 TEST(Event, TryGet)
 {
-  auto event = create_event(SDL_MOUSEMOTION);
+  auto event = CreateEvent(SDL_MOUSEMOTION);
   ASSERT_TRUE(event.TryGet<cen::MouseMotionEvent>());
   ASSERT_FALSE(event.TryGet<cen::WindowEvent>());
 

b5d8c33a0e58d8b86a36eb49ab96aa180c3b5bb1
Albin Johansson <albinoscar99@hotmail.com>
2022-01-16 17:15:26 +0100

    Overhaul endian header

--- src/centurion/endian.hpp
+++ src/centurion/endian.hpp
@@ -8,64 +8,64 @@
 namespace cen {
 
 /** Swaps the byte order of the specified value. */
-[[nodiscard]] inline auto SwapByteOrder(const uint16 value) noexcept -> uint16
+[[nodiscard]] inline auto swap_byte_order(const uint16 value) noexcept -> uint16
 {
   return SDL_Swap16(value);
 }
 
-[[nodiscard]] inline auto SwapByteOrder(const uint32 value) noexcept -> uint32
+[[nodiscard]] inline auto swap_byte_order(const uint32 value) noexcept -> uint32
 {
   return SDL_Swap32(value);
 }
 
-[[nodiscard]] inline auto SwapByteOrder(const uint64 value) noexcept -> uint64
+[[nodiscard]] inline auto swap_byte_order(const uint64 value) noexcept -> uint64
 {
   return SDL_Swap64(value);
 }
 
-[[nodiscard]] inline auto SwapByteOrder(const float value) noexcept -> float
+[[nodiscard]] inline auto swap_byte_order(const float value) noexcept -> float
 {
   return SDL_SwapFloat(value);
 }
 
 /* Swaps a big endian value to a little endian value. */
-[[nodiscard]] inline auto SwapBigEndian(const uint16 value) noexcept -> uint16
+[[nodiscard]] inline auto swap_big_endian(const uint16 value) noexcept -> uint16
 {
   return SDL_SwapBE16(value);
 }
 
-[[nodiscard]] inline auto SwapBigEndian(const uint32 value) noexcept -> uint32
+[[nodiscard]] inline auto swap_big_endian(const uint32 value) noexcept -> uint32
 {
   return SDL_SwapBE32(value);
 }
 
-[[nodiscard]] inline auto SwapBigEndian(const uint64 value) noexcept -> uint64
+[[nodiscard]] inline auto swap_big_endian(const uint64 value) noexcept -> uint64
 {
   return SDL_SwapBE64(value);
 }
 
-[[nodiscard]] inline auto SwapBigEndian(const float value) noexcept -> float
+[[nodiscard]] inline auto swap_big_endian(const float value) noexcept -> float
 {
   return SDL_SwapFloatBE(value);
 }
 
 /* Swaps a little endian value to a big endian value. */
-[[nodiscard]] inline auto SwapLittleEndian(const uint16 value) noexcept -> uint16
+[[nodiscard]] inline auto swap_little_endian(const uint16 value) noexcept -> uint16
 {
   return SDL_SwapLE16(value);
 }
 
-[[nodiscard]] inline auto SwapLittleEndian(const uint32 value) noexcept -> uint32
+[[nodiscard]] inline auto swap_little_endian(const uint32 value) noexcept -> uint32
 {
   return SDL_SwapLE32(value);
 }
 
-[[nodiscard]] inline auto SwapLittleEndian(const uint64 value) noexcept -> uint64
+[[nodiscard]] inline auto swap_little_endian(const uint64 value) noexcept -> uint64
 {
   return SDL_SwapLE64(value);
 }
 
-[[nodiscard]] inline auto SwapLittleEndian(const float value) noexcept -> float
+[[nodiscard]] inline auto swap_little_endian(const float value) noexcept -> float
 {
   return SDL_SwapFloatLE(value);
 }

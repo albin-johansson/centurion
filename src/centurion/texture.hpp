c1a35f708438abd5c36eed147a0ce87ee640719c
Albin Johansson <albinoscar99@hotmail.com>
2022-01-18 15:42:31 +0100

    Improve texture header

--- src/centurion/texture.hpp
+++ src/centurion/texture.hpp
@@ -33,6 +33,11 @@ namespace cen {
 /// \addtogroup texture
 /// \{
 
+/**
+ * \brief Represents different texture access modes.
+ *
+ * \remarks The `non_lockable` enumerator refers to "static" texture access.
+ */
 enum class texture_access {
   non_lockable = SDL_TEXTUREACCESS_STATIC,  ///< Texture changes rarely and isn't lockable.
   streaming = SDL_TEXTUREACCESS_STREAMING,  ///< Texture changes frequently and is lockable.
@@ -68,10 +73,13 @@ inline auto operator<<(std::ostream& stream, const texture_access access) -> std
 
 #if SDL_VERSION_ATLEAST(2, 0, 12)
 
+/**
+ * \brief Represents different texture scale modes.
+ */
 enum class scale_mode {
-  nearest = SDL_ScaleModeNearest,
-  linear = SDL_ScaleModeLinear,
-  best = SDL_ScaleModeBest
+  nearest = SDL_ScaleModeNearest,  ///< Nearest pixel sampling.
+  linear = SDL_ScaleModeLinear,    ///< Linear filtering.
+  best = SDL_ScaleModeBest         ///< Anisotropic filtering.
 };
 
 /// \name Scale mode functions
@@ -106,15 +114,30 @@ inline auto operator<<(std::ostream& stream, const scale_mode mode) -> std::ostr
 template <typename T>
 class basic_texture;
 
-using texture = basic_texture<detail::owner_tag>;
-using texture_handle = basic_texture<detail::handle_tag>;
+using texture = basic_texture<detail::owner_tag>;          ///< An owning texture.
+using texture_handle = basic_texture<detail::handle_tag>;  ///< A non-owning texture.
 
+/**
+ * \brief Represents a hardware-accelerated image.
+ *
+ * \ownerhandle `texture`/`texture_handle`
+ *
+ * \see `texture`
+ * \see `texture_handle`
+ */
 template <typename T>
 class basic_texture final {
  public:
   /// \name Construction
   /// \{
 
+  /**
+   * \brief Creates a texture based on an existing SDL texture.
+   *
+   * \details Ownership of the supplied texture is claimed if the texture is owning.
+   *
+   * \param source the source texture.
+   */
   explicit basic_texture(maybe_owner<SDL_Texture*> source) noexcept(detail::is_handle<T>)
       : mTexture{source}
   {
@@ -125,13 +148,16 @@ class basic_texture final {
     }
   }
 
-  template <typename TT = T, detail::enable_for_handle<TT> = 0>
-  explicit basic_texture(texture& owner) noexcept : mTexture{owner.get()}
-  {}
-
 #ifndef CENTURION_NO_SDL_IMAGE
 
-  /* Creates a texture based the image at the specified path. */
+  /**
+   * \brief Creates a texture based on an image file.
+   *
+   * \param renderer the renderer that will be used.
+   * \param path the file path to the source image.
+   *
+   * \throws img_error if the texture cannot be created.
+   */
   template <typename Renderer, typename TT = T, detail::enable_for_owner<TT> = 0>
   basic_texture(const Renderer& renderer, const char* path)
       : mTexture{IMG_LoadTexture(renderer.get(), path)}
@@ -141,6 +167,7 @@ class basic_texture final {
     }
   }
 
+  /// \copydoc basic_texture(const Renderer&, const char*)
   template <typename Renderer, typename TT = T, detail::enable_for_owner<TT> = 0>
   basic_texture(const Renderer& renderer, const std::string& path)
       : basic_texture{renderer, path.c_str()}
@@ -148,7 +175,14 @@ class basic_texture final {
 
 #endif  // CENTURION_NO_SDL_IMAGE
 
-  /* Creates a texture that is a copy of the supplied surface. */
+  /**
+   * \brief Creates a texture based on an existing surface.
+   *
+   * \param renderer the renderer that will be used.
+   * \param surface the surface that will be copied.
+   *
+   * \todo basic_renderer::create_texture?
+   */
   template <typename Renderer, typename S, typename TT = T, detail::enable_for_owner<TT> = 0>
   basic_texture(const Renderer& renderer, const basic_surface<S>& surface)
       : mTexture{SDL_CreateTextureFromSurface(renderer.get(), surface.get())}
@@ -158,7 +192,18 @@ class basic_texture final {
     }
   }
 
-  /* Creates a texture with the specified size, format, and access. */
+  /**
+   * \brief Creates a blank texture with the specified characteristics.
+   *
+   * \param renderer the renderer that will be used.
+   * \param format the pixel format that will be used.
+   * \param access the texture access that will be used.
+   * \param size the size of the texture.
+   *
+   * \todo basic_renderer::create_texture?
+   *
+   * \throws sdl_error if the texture cannot be created.
+   */
   template <typename Renderer, typename TT = T, detail::enable_for_owner<TT> = 0>
   basic_texture(const Renderer& renderer,
                 const PixelFormat format,
@@ -175,15 +220,39 @@ class basic_texture final {
     }
   }
 
+  template <typename TT = T, detail::enable_for_handle<TT> = 0>
+  explicit basic_texture(texture& owner) noexcept : mTexture{owner.get()}
+  {}
+
   /// \} End of construction
 
-  void set_alpha(const uint8 alpha) noexcept { SDL_SetTextureAlphaMod(mTexture, alpha); }
+  /// \name Setters
+  /// \{
 
+  /**
+   * \brief Sets the alpha modulation of the texture.
+   *
+   * \param alpha the new alpha modulation, in the range [0, 255].
+   */
+  void set_alpha_mod(const uint8 alpha) noexcept { SDL_SetTextureAlphaMod(mTexture, alpha); }
+
+  /**
+   * \brief Sets the color modulation of the texture.
+   *
+   * \note The alpha component of the color is ignored, see `set_alpha_mod()`.
+   *
+   * \param color the new color modulation.
+   */
   void set_color_mod(const color& color) noexcept
   {
     SDL_SetTextureColorMod(mTexture, color.red(), color.green(), color.blue());
   }
 
+  /**
+   * \brief Sets the blend mode used by the texture.
+   *
+   * \param mode the new blend mode.
+   */
   void set_blend_mode(const BlendMode mode) noexcept
   {
     SDL_SetTextureBlendMode(mTexture, static_cast<SDL_BlendMode>(mode));
@@ -191,6 +260,11 @@ class basic_texture final {
 
 #if SDL_VERSION_ATLEAST(2, 0, 12)
 
+  /**
+   * \brief Sets the scale mode that will be used by the texture.
+   *
+   * \param mode the new scale mode.
+   */
   void set_scale_mode(const scale_mode mode) noexcept
   {
     SDL_SetTextureScaleMode(mTexture, static_cast<SDL_ScaleMode>(mode));
@@ -198,6 +272,16 @@ class basic_texture final {
 
 #endif  // SDL_VERSION_ATLEAST(2, 0, 12)
 
+  /// \} End of setters
+
+  /// \name Getters
+  /// \{
+
+  /**
+   * \brief Returns the size of the texture.
+   *
+   * \return the texture size.
+   */
   [[nodiscard]] auto size() const noexcept -> iarea
   {
     int width{};
@@ -206,18 +290,37 @@ class basic_texture final {
     return {width, height};
   }
 
+  /**
+   * \brief Returns the width of the texture.
+   *
+   * \return the texture width.
+   *
+   * \see `size()`
+   */
   [[nodiscard]] auto width() const noexcept -> int
   {
     const auto [width, height] = size();
     return width;
   }
 
+  /**
+   * \brief Returns the height of the texture.
+   *
+   * \return the texture height.
+   *
+   * \see `size()`
+   */
   [[nodiscard]] auto height() const noexcept -> int
   {
     const auto [width, height] = size();
     return height;
   }
 
+  /**
+   * \brief Returns the pixel format used by the texture.
+   *
+   * \return the texture pixel format.
+   */
   [[nodiscard]] auto format() const noexcept -> PixelFormat
   {
     uint32 format{};
@@ -225,6 +328,11 @@ class basic_texture final {
     return static_cast<PixelFormat>(format);
   }
 
+  /**
+   * \brief Returns the texture access used by the texture.
+   *
+   * \return the texture access.
+   */
   [[nodiscard]] auto access() const noexcept -> texture_access
   {
     int access{};
@@ -232,28 +340,53 @@ class basic_texture final {
     return static_cast<texture_access>(access);
   }
 
-  [[nodiscard]] auto is_target() const noexcept -> bool
-  {
-    return access() == texture_access::target;
-  }
-
+  /**
+   * \brief Indicates whether the texture has static texture access.
+   *
+   * \return `true` if the texture has static texture access; `false` otherwise.
+   */
   [[nodiscard]] auto is_static() const noexcept -> bool
   {
     return access() == texture_access::non_lockable;
   }
 
+  /**
+   * \brief Indicates whether the texture has target texture access.
+   *
+   * \return `true` if the texture has target texture access; `false` otherwise.
+   */
+  [[nodiscard]] auto is_target() const noexcept -> bool
+  {
+    return access() == texture_access::target;
+  }
+
+  /**
+   * \brief Indicates whether the texture has streaming texture access.
+   *
+   * \return `true` if the texture has streaming texture access; `false` otherwise.
+   */
   [[nodiscard]] auto is_streaming() const noexcept -> bool
   {
     return access() == texture_access::streaming;
   }
 
-  [[nodiscard]] auto alpha() const noexcept -> uint8
+  /**
+   * \brief Returns the alpha modulation of the texture.
+   *
+   * \return the texture alpha modulation.
+   */
+  [[nodiscard]] auto alpha_mod() const noexcept -> uint8
   {
     uint8 alpha{};
     SDL_GetTextureAlphaMod(mTexture, &alpha);
     return alpha;
   }
 
+  /**
+   * \brief Returns the color modulation of the texture.
+   *
+   * \return the texture color modulation.
+   */
   [[nodiscard]] auto color_mod() const noexcept -> color
   {
     uint8 red{};
@@ -263,6 +396,11 @@ class basic_texture final {
     return {red, green, blue};
   }
 
+  /**
+   * \brief Returns the blend mode used by the texture.
+   *
+   * \return the texture blend mode.
+   */
   [[nodiscard]] auto get_blend_mode() const noexcept -> BlendMode
   {
     SDL_BlendMode mode{};
@@ -272,6 +410,11 @@ class basic_texture final {
 
 #if SDL_VERSION_ATLEAST(2, 0, 12)
 
+  /**
+   * \brief Returns the scale mode used by the texture.
+   *
+   * \return the texture scale mode.
+   */
   [[nodiscard]] auto get_scale_mode() const noexcept -> scale_mode
   {
     SDL_ScaleMode mode{};
@@ -281,7 +424,19 @@ class basic_texture final {
 
 #endif  // SDL_VERSION_ATLEAST(2, 0, 12)
 
-  /* Releases ownership of the associated SDL texture and returns a pointer to it. */
+  /// \} End of getters
+
+  /// \name Misc functions
+  /// \{
+
+  /**
+   * \brief Releases ownership of the associated SDL texture.
+   *
+   * \warning Think twice before using this function, and make sure you know what you are doing
+   * before using it.
+   *
+   * \return the released SDL texture.
+   */
   template <typename TT = T, detail::enable_for_owner<TT> = 0>
   [[nodiscard]] auto release() noexcept -> owner<SDL_Texture*>
   {
@@ -290,11 +445,18 @@ class basic_texture final {
 
   [[nodiscard]] auto get() const noexcept -> SDL_Texture* { return mTexture.get(); }
 
+  /**
+   * \brief Indicates whether a handle holds a non-null pointer.
+   *
+   * \return `true` if the handle holds a non-null pointer; `false` otherwise.
+   */
   template <typename TT = T, detail::enable_for_handle<TT> = 0>
   explicit operator bool() const noexcept
   {
     return mTexture != nullptr;
   }
+
+  /// \} End of misc functions
 
  private:
   detail::pointer<T, SDL_Texture> mTexture;

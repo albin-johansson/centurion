16a977539d1cb9eaac6777bd1fc5fb47d897c267
Albin Johansson <albinoscar99@hotmail.com>
2022-01-21 18:23:34 +0100

    Update examples.h

--- docs/examples.h
+++ docs/examples.h
@@ -4,15 +4,24 @@
  * This page lists all of the available examples that demonstrate various aspects of the
  * Centurion library. Some of these examples will be very simple if you have any previous
  * experience working with SDL, but they can still be useful to get a notion of how the
- * APIs are meant to be used. The examples are roughly organized in regards to increasing
- * difficulty, even if none of them are necessarily particularly difficult.
+ * APIs are intended to be used.
  *
- * | Example                                 | Synopsis |
- * | --------------------------------------- | -------- |
- * | \subpage page-minimal-centurion-program | Shows a minimal Centurion program |
- * | \subpage page-custom-configuration      | Shows how to use a custom configuration |
- * | \subpage page-message-box               | An example of the message box API |
- * | \subpage page-responsive-window         | Shows how to set up a responsive window |
- * | \subpage page-basic-rendering           | Shows the basics of the rendering API |
- * | \subpage page-event-dispatcher          | Shows how to use `event_dispatcher` |
+ * \section ex-section-basics Basics
+ *
+ * \li \subpage page-minimal-centurion-program "A minimal Centurion program"
+ * \li \subpage page-custom-configuration "How to use a custom configuration"
+ * \li \subpage page-responsive-window "How to set up a responsive window"
+ *
+ * \section ex-section-event-handling Event handling
+ *
+ * \li \subpage page-event-handler "event_handler"
+ * \li \subpage page-event-dispatcher "event_dispatcher"
+ *
+ * \section ex-section-video Video
+ *
+ * \li \subpage page-basic-rendering "Rendering basics"
+ *
+ * \section ex-section-misc Miscellaneous
+ *
+ * \li \subpage page-message-box "Message boxes"
  */

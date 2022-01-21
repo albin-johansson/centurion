0aa1759857064e3277e61f844312033511e9c2be
Albin Johansson <albinoscar99@hotmail.com>
2022-01-21 15:23:08 +0100

    Update demo.cpp

--- examples/event-dispatcher/demo.cpp
+++ examples/event-dispatcher/demo.cpp
@@ -2,7 +2,7 @@
 
 namespace {
 
-// Here we specify that we want to subscribe to four different kinds of events
+/* Here we specify that we want to subscribe to four different kinds of events */
 using event_dispatcher = cen::event_dispatcher<cen::quit_event,
                                                cen::window_event,
                                                cen::keyboard_event,
@@ -10,20 +10,21 @@ using event_dispatcher = cen::event_dispatcher<cen::quit_event,
 
 void on_mouse_button_event(const cen::mouse_button_event& event)
 {
-  cen::log_info("MouseButtonEvent");
+  cen::log_info("mouse_button_event");
 }
 
-class Game final {
+/* Our AAA game class */
+class aaa_game final {
  public:
-  Game()
+  aaa_game()
   {
-    // Member function handlers
-    m_dispatcher.bind<cen::quit_event>().to<&Game::on_quit_event>(this);
-    m_dispatcher.bind<cen::window_event>().to<&Game::on_window_event>(this);
+    /* Member function handlers */
+    m_dispatcher.bind<cen::quit_event>().to<&aaa_game::on_quit_event>(this);
+    m_dispatcher.bind<cen::window_event>().to<&aaa_game::on_window_event>(this);
 
-    // Lambda handler
+    /* Lambda handler */
     m_dispatcher.bind<cen::keyboard_event>().to(
-        [](const cen::keyboard_event& event) { cen::log_info("keyboard_event"); });
+        [](const cen::keyboard_event&) { cen::log_info("keyboard_event"); });
 
     // Free function handler
     m_dispatcher.bind<cen::mouse_button_event>().to<&on_mouse_button_event>();
@@ -34,10 +35,10 @@ class Game final {
     m_window.show();
 
     while (m_running) {
-      // All we need to do each frame to handle events is to poll the event dispatcher
+      /* All we need to do each frame to handle events is to poll the event dispatcher */
       m_dispatcher.poll();
 
-      // Game logic goes here...
+      /* Game logic goes here... */
     }
 
     m_window.hide();
@@ -49,15 +50,15 @@ class Game final {
   event_dispatcher m_dispatcher;
   bool m_running{true};
 
-  // Invoked for each quit event
-  void on_quit_event(const cen::quit_event& event)
+  /* Invoked for each quit event */
+  void on_quit_event(const cen::quit_event&)
   {
     cen::log_info("quit_event");
     m_running = false;
   }
 
-  // Invoked for each window event
-  void on_window_event(const cen::window_event& event) { cen::log_info("window_event"); }
+  /* Invoked for each window event */
+  void on_window_event(const cen::window_event&) { cen::log_info("window_event"); }
 };
 
 }  // namespace
@@ -69,6 +70,6 @@ int main(int, char**)
   const cen::mix mix;
   const cen::ttf ttf;
 
-  Game game;
+  aaa_game game;
   return game.run();
 }

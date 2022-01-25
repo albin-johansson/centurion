079557f8dc5430b1032afd9d562095d55e982a6b
Albin Johansson <albinoscar99@hotmail.com>
2022-01-26 00:35:18 +0100

    Update rendering demo with render_geo example

--- examples/basic-rendering/demo.cpp
+++ examples/basic-rendering/demo.cpp
@@ -56,6 +56,12 @@ int main(int, char**)
     renderer.render(image, cen::fpoint{600.0, 450.0});
     renderer.render(text, cen::fpoint{450.0, 230.0});
 
+    // Render a triangle, using the low level "geometry" rendering API
+    const SDL_Vertex vertices[] = {{{650, 400}, cen::colors::red.get(), {}},
+                                   {{410, 310}, cen::colors::lime.get(), {}},
+                                   {{450, 550}, cen::colors::blue.get(), {}}};
+    renderer.render_geo(vertices);
+
     // Apply our draw calls to the render target (the window)
     renderer.present();
   }

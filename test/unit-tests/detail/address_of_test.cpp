f36c269ceef99a5e8c12afcf1ca774bdf5cd84be
Albin Johansson <albinoscar99@hotmail.com>
2022-02-06 18:28:35 +0100

    Update address_of_test.cpp

--- test/unit-tests/detail/address_of_test.cpp
+++ test/unit-tests/detail/address_of_test.cpp
@@ -9,7 +9,13 @@ TEST(AddressOf, Correctness)
   {  // Non-null pointer
     int i = 42;
     const auto str = cen::detail::address_of(&i);
+
     ASSERT_FALSE(str.empty());
+
+#if CENTURION_HAS_FEATURE_CPP20
+    ASSERT_TRUE(str.starts_with("0x"));
+#endif  // CENTURION_HAS_FEATURE_CPP20
+
     std::cout << "detail::address_of output: " << str << '\n';
   }
 

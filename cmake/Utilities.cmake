73f0444deca44ca67def454a08c6fe8cd12295b7
Albin Johansson <albinoscar99@hotmail.com>
2023-01-22 18:15:09 +0100

    Minor CI changes

--- cmake/Utilities.cmake
+++ cmake/Utilities.cmake
@@ -3,41 +3,17 @@
 #   name: the name of the library target.
 #   includeDirectory: the path of the directory that contains the headers of the library.
 #   sources: the headers associated with the library.
-function(add_header_only_lib name includeDirectory sources)
+function(cen_add_header_only_lib name includeDirectory sources)
   add_library(${name} INTERFACE)
   target_sources(${name} INTERFACE ${sources})
   target_include_directories(${name} SYSTEM INTERFACE ${includeDirectory})
-endfunction()
-
-# Checks if an environment variable is defined.
-#   envVar: the name of the actual environment variable.
-#   name: the name of the library associated with the environment variable.
-function(find_env_var envVar name)
-  if (DEFINED ENV{${envVar}})
-    message("${name} path: " $ENV{${envVar}})
-  else ()
-    message(WARNING "Couldn't find environment variable ${envVar}!")
-  endif ()
-endfunction()
-
-# Copies a file.
-#   target: the associated target.
-#   from: the file that will be copied.
-#   to: the target destination of the copied file.
-function(copy_file_post_build target from to)
-  add_custom_command(
-      TARGET ${target}
-      POST_BUILD
-      COMMAND ${CMAKE_COMMAND} -E copy
-      ${from}
-      ${to})
 endfunction()
 
 # Copies a directory.
 #   target: the associated target.
 #   from: the directory that will be copied.
 #   to: the target destination of the copied directory.
-function(copy_directory_post_build target from to)
+function(cen_copy_directory_post_build target from to)
   add_custom_command(
       TARGET ${target}
       POST_BUILD

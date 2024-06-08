8c2d691fad1c1237ed459f5739e2c6daf9d06f83
Albin Johansson <albinoscar99@hotmail.com>
2024-06-08 16:07:21 +0200

    Make basic demo work without extensions

--- test/demo/basic/main.cpp
+++ test/demo/basic/main.cpp
@@ -11,13 +11,19 @@ auto main(int, char*[]) -> int
   const auto sdl_flags = SDL_INIT_TIMER | SDL_INIT_EVENTS;
   const auto sdl [[maybe_unused]] = cen::sdl_library::init(sdl_flags).value();
 
+#ifndef CENTURION_NO_SDL_IMAGE
   const auto img_flags = IMG_INIT_PNG;
   const auto img [[maybe_unused]] = cen::img_library::init(img_flags).value();
+#endif  // CENTURION_NO_SDL_IMAGE
 
+#ifndef CENTURION_NO_SDL_MIXER
   const auto mix_flags = MIX_INIT_MP3;
   const auto mix [[maybe_unused]] = cen::mix_library::init(mix_flags);
+#endif  // CENTURION_NO_SDL_MIXER
 
+#ifndef CENTURION_NO_SDL_TTF
   const auto ttf [[maybe_unused]] = cen::ttf_library::init().value();
+#endif  // CENTURION_NO_SDL_TTF
 
   std::cout << "Success!\n";
   return 0;
